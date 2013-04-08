e756a31949b8f1b45f164d772ef581599fc090e4
Robbie Hanson <robbiehanson@deusty.com>
2013-04-08 16:18:26 -0700

    Renaming file

--- YapDatabase/Collection-Key-Value/YapCollectionsDatabaseTransaction+Timestamp.m
+++ YapDatabase/Collection-Key-Value/YapCollectionsDatabaseTransaction+Timestamp.m
@@ -1,4 +1,4 @@
-#import "YapCollectionsDatabase+Timestamp.h"
+#import "YapCollectionsDatabaseTransaction+Timestamp.h"
 
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
