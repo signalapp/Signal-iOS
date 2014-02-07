56200bce7f613b587cd638ae8739e444b2fd92cc
Robbie Hanson <robbiehanson@deusty.com>
2014-02-07 15:14:20 -0800

    Adding unit tests for automatic unregistration of extension dependencies

--- Testing/UnitTesting/TestYapDatabaseFilteredView.m
+++ Testing/UnitTesting/TestYapDatabaseFilteredView.m
@@ -456,4 +456,248 @@
 	connection2 = nil;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)testUnregistration_persistent
+{
+	NSString *databasePath = [self databasePath:NSStringFromSelector(_cmd)];
+	
+	YapDatabaseViewOptions *options = [[YapDatabaseViewOptions alloc] init];
+	options.isPersistent = YES;
+	
+	[self _testUnregistration_withPath:databasePath options:options];
+}
+
+- (void)testUnregistration_nonPersistent
+{
+	NSString *databasePath = [self databasePath:NSStringFromSelector(_cmd)];
+	
+	YapDatabaseViewOptions *options = [[YapDatabaseViewOptions alloc] init];
+	options.isPersistent = NO;
+	
+	[self _testUnregistration_withPath:databasePath options:options];
+}
+
+- (void)_testUnregistration_withPath:(NSString *)databasePath options:(YapDatabaseViewOptions *)options
+{
+	[[NSFileManager defaultManager] removeItemAtPath:databasePath error:NULL];
+	YapDatabase *database = [[YapDatabase alloc] initWithPath:databasePath];
+	
+	STAssertNotNil(database, @"Oops");
+	
+	YapDatabaseConnection *connection = [database newConnection];
+	
+	YapDatabaseViewBlockType groupingBlockType;
+	YapDatabaseViewGroupingWithKeyBlock groupingBlock;
+	
+	YapDatabaseViewBlockType sortingBlockType;
+	YapDatabaseViewSortingWithObjectBlock sortingBlock;
+	
+	groupingBlockType = YapDatabaseViewBlockTypeWithKey;
+	groupingBlock = ^NSString *(NSString *collection, NSString *key)
+	{
+		if ([key isEqualToString:@"keyX"]) // Exclude keyX from view
+			return nil;
+		else
+			return @"";
+	};
+	
+	sortingBlockType = YapDatabaseViewBlockTypeWithObject;
+	sortingBlock = ^(NSString *group, NSString *collection1, NSString *key1, id obj1,
+	                                  NSString *collection2, NSString *key2, id obj2)
+	{
+		__unsafe_unretained NSNumber *number1 = (NSNumber *)obj1;
+		__unsafe_unretained NSNumber *number2 = (NSNumber *)obj2;
+		
+		return [number1 compare:number2];
+	};
+	
+	YapDatabaseView *view =
+	  [[YapDatabaseView alloc] initWithGroupingBlock:groupingBlock
+	                               groupingBlockType:groupingBlockType
+	                                    sortingBlock:sortingBlock
+	                                sortingBlockType:sortingBlockType
+	                                         version:0
+	                                         options:options];
+	
+	BOOL registerResult1 = [database registerExtension:view withName:@"order"];
+	STAssertTrue(registerResult1, @"Failure registering view extension");
+	
+	YapDatabaseViewBlockType filteringBlockType;
+	YapDatabaseViewFilteringBlock filteringBlock;
+	
+	filteringBlockType = YapDatabaseViewBlockTypeWithObject;
+	filteringBlock = ^BOOL (NSString *group, NSString *collection, NSString *key, id object)
+	{
+		__unsafe_unretained NSNumber *number = (NSNumber *)object;
+		
+		if ([number intValue] % 2 == 0)
+			return YES; // even
+		else
+			return NO;  // odd
+	};
+	
+	YapDatabaseFilteredView *filteredView =
+	  [[YapDatabaseFilteredView alloc] initWithParentViewName:@"order"
+	                                           filteringBlock:filteringBlock
+	                                       filteringBlockType:filteringBlockType];
+	
+	BOOL registerResult2 = [database registerExtension:filteredView withName:@"filter"];
+	STAssertTrue(registerResult2, @"Failure registering filteredView extension");
+	
+	// Make sure the extensions are visible
+	
+	[connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+		
+		STAssertNotNil([transaction ext:@"order"], @"Expected YapDatabaseViewTransaction");
+		STAssertNotNil([transaction ext:@"filter"], @"Expected YapDatabaseFilteredViewTransaction");
+	}];
+	
+	// Now unregister the view, and make sure it automatically unregisters the filteredView too.
+	
+	[database unregisterExtension:@"order"];
+	
+	[connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+		
+		STAssertNil([transaction ext:@"order"], @"Expected nil");
+		STAssertNil([transaction ext:@"filter"], @"Expected nil");
+	}];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)testDoubleUnregistration_persistent
+{
+	NSString *databasePath = [self databasePath:NSStringFromSelector(_cmd)];
+	
+	YapDatabaseViewOptions *options = [[YapDatabaseViewOptions alloc] init];
+	options.isPersistent = YES;
+	
+	[self _testDoubleUnregistration_withPath:databasePath options:options];
+}
+
+- (void)testDoubleUnregistration_nonPersistent
+{
+	NSString *databasePath = [self databasePath:NSStringFromSelector(_cmd)];
+	
+	YapDatabaseViewOptions *options = [[YapDatabaseViewOptions alloc] init];
+	options.isPersistent = NO;
+	
+	[self _testDoubleUnregistration_withPath:databasePath options:options];
+}
+
+- (void)_testDoubleUnregistration_withPath:(NSString *)databasePath options:(YapDatabaseViewOptions *)options
+{
+	[[NSFileManager defaultManager] removeItemAtPath:databasePath error:NULL];
+	YapDatabase *database = [[YapDatabase alloc] initWithPath:databasePath];
+	
+	STAssertNotNil(database, @"Oops");
+	
+	YapDatabaseConnection *connection = [database newConnection];
+	
+	YapDatabaseViewBlockType groupingBlockType;
+	YapDatabaseViewGroupingWithKeyBlock groupingBlock;
+	
+	YapDatabaseViewBlockType sortingBlockType;
+	YapDatabaseViewSortingWithObjectBlock sortingBlock;
+	
+	groupingBlockType = YapDatabaseViewBlockTypeWithKey;
+	groupingBlock = ^NSString *(NSString *collection, NSString *key)
+	{
+		if ([key isEqualToString:@"keyX"]) // Exclude keyX from view
+			return nil;
+		else
+			return @"";
+	};
+	
+	sortingBlockType = YapDatabaseViewBlockTypeWithObject;
+	sortingBlock = ^(NSString *group, NSString *collection1, NSString *key1, id obj1,
+	                                  NSString *collection2, NSString *key2, id obj2)
+	{
+		__unsafe_unretained NSNumber *number1 = (NSNumber *)obj1;
+		__unsafe_unretained NSNumber *number2 = (NSNumber *)obj2;
+		
+		return [number1 compare:number2];
+	};
+	
+	YapDatabaseView *view =
+	  [[YapDatabaseView alloc] initWithGroupingBlock:groupingBlock
+	                               groupingBlockType:groupingBlockType
+	                                    sortingBlock:sortingBlock
+	                                sortingBlockType:sortingBlockType
+	                                         version:0
+	                                         options:options];
+	
+	BOOL registerResult1 = [database registerExtension:view withName:@"order"];
+	STAssertTrue(registerResult1, @"Failure registering view extension");
+	
+	YapDatabaseViewBlockType filteringBlockType1;
+	YapDatabaseViewFilteringBlock filteringBlock1;
+	
+	filteringBlockType1 = YapDatabaseViewBlockTypeWithObject;
+	filteringBlock1 = ^BOOL (NSString *group, NSString *collection, NSString *key, id object)
+	{
+		__unsafe_unretained NSNumber *number = (NSNumber *)object;
+		
+		if ([number intValue] % 2 == 0)
+			return YES; // even
+		else
+			return NO;  // odd
+	};
+	
+	YapDatabaseFilteredView *filteredView1 =
+	  [[YapDatabaseFilteredView alloc] initWithParentViewName:@"order"
+	                                           filteringBlock:filteringBlock1
+	                                       filteringBlockType:filteringBlockType1];
+	
+	BOOL registerResult2 = [database registerExtension:filteredView1 withName:@"filter1"];
+	STAssertTrue(registerResult2, @"Failure registering filteredView1 extension");
+	
+	YapDatabaseViewBlockType filteringBlockType2;
+	YapDatabaseViewFilteringBlock filteringBlock2;
+	
+	filteringBlockType2 = YapDatabaseViewBlockTypeWithObject;
+	filteringBlock2 = ^BOOL (NSString *group, NSString *collection, NSString *key, id object)
+	{
+		__unsafe_unretained NSNumber *number = (NSNumber *)object;
+		
+		if ([number intValue] < 100)
+			return YES; // even && within range
+		else
+			return NO;  // odd || out of range
+	};
+	
+	YapDatabaseFilteredView *filteredView2 =
+	  [[YapDatabaseFilteredView alloc] initWithParentViewName:@"filter1"
+	                                           filteringBlock:filteringBlock2
+	                                       filteringBlockType:filteringBlockType2];
+	
+	BOOL registerResult3 = [database registerExtension:filteredView2 withName:@"filter2"];
+	STAssertTrue(registerResult3, @"Failure registering filteredView2 extension");
+	
+	// Make sure the extensions are visible
+	
+	[connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+		
+		STAssertNotNil([transaction ext:@"order"], @"Expected YapDatabaseViewTransaction");
+		STAssertNotNil([transaction ext:@"filter1"], @"Expected YapDatabaseFilteredViewTransaction");
+		STAssertNotNil([transaction ext:@"filter2"], @"Expected YapDatabaseFilteredViewTransaction");
+	}];
+	
+	// Now unregister the view, and make sure it automatically unregisters the filteredView too.
+	
+	[database unregisterExtension:@"order"];
+	
+	[connection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+		
+		STAssertNil([transaction ext:@"order"], @"Expected nil");
+		STAssertNil([transaction ext:@"filter1"], @"Expected nil");
+		STAssertNil([transaction ext:@"filter2"], @"Expected nil");
+	}];
+}
+
 @end
