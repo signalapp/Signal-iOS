8f47440ed0c3696fb06dba179e0c78fa69650ded
Andrew Mackenzie-Ross <andrew@mackross.net>
2014-10-07 23:45:50 -0700

    Imports required for dynamic framework.

--- Vendor/CocoaLumberjack/CLI/CLIColor.h
+++ Vendor/CocoaLumberjack/CLI/CLIColor.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
 
 /**
  Simple NSColor replacement for CLI projects that don't link with AppKit
--- YapDatabase/Extensions/Views/Utilities/YapDatabaseViewChange.m
+++ YapDatabase/Extensions/Views/Utilities/YapDatabaseViewChange.m
@@ -3,6 +3,10 @@
 #import "YapDatabaseViewMappingsPrivate.h"
 #import "YapDatabaseLogging.h"
 
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#endif
+
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
