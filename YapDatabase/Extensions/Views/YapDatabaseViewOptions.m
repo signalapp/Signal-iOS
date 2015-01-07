dec7e7f793e4e0cc0e7b194ad559eb0be92ab6a4
Nico Prananta <nicnocquee@yahoo.com>
2015-01-07 22:23:21 +0900

    synthesize skipInitialViewPopulation

--- YapDatabase/Extensions/Views/YapDatabaseViewOptions.m
+++ YapDatabase/Extensions/Views/YapDatabaseViewOptions.m
@@ -5,23 +5,24 @@
 
 @synthesize isPersistent = isPersistent;
 @synthesize allowedCollections = allowedCollections;
+@synthesize skipInitialViewPopulation = skipInitialViewPopulation;
 
 - (id)init
 {
-	if ((self = [super init]))
-	{
-		isPersistent = YES;
-	}
-	return self;
+    if ((self = [super init]))
+    {
+        isPersistent = YES;
+    }
+    return self;
 }
 
 - (id)copyWithZone:(NSZone *)zone
 {
-	YapDatabaseViewOptions *copy = [[[self class] alloc] init]; // [self class] required to support subclassing
-	copy->isPersistent = isPersistent;
-	copy->allowedCollections = allowedCollections;
-	
-	return copy;
+    YapDatabaseViewOptions *copy = [[[self class] alloc] init]; // [self class] required to support subclassing
+    copy->isPersistent = isPersistent;
+    copy->allowedCollections = allowedCollections;
+    copy->skipInitialViewPopulation = skipInitialViewPopulation;
+    return copy;
 }
 
 @end
