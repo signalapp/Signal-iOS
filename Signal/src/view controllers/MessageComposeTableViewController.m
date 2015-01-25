7d5154b10b625c4500f95522b12ad9ea25640d35
Christine Corbett <christine.corbett@gmail.com>
2015-01-24 19:19:09 -1000

    Dismiss search bar when new group is created.

--- Signal/src/view controllers/MessageComposeTableViewController.m
+++ Signal/src/view controllers/MessageComposeTableViewController.m
@@ -320,7 +320,7 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-  
+    self.searchController.active = NO;
 }
 
 -(IBAction)closeAction:(id)sender
