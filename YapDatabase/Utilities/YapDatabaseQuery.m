04baad32933928cc2f6e7ab22d77ace361989081
Caleb Davenport <calebmdavenport@gmail.com>
2015-02-03 17:51:01 -0800

    Restore documentation on method implementations.

--- YapDatabase/Utilities/YapDatabaseQuery.m
+++ YapDatabase/Utilities/YapDatabaseQuery.m
@@ -22,6 +22,32 @@
 	NSArray *queryParameters;
 }
 
+/**
+ * A YapDatabaseQuery is everything after the SELECT clause of a query.
+ * Methods that take YapDatabaseQuery parameters will prefix your query string similarly to:
+ *
+ * fullQuery = @"SELECT rowid FROM 'database' " + [yapDatabaseQuery queryString];
+ *
+ * Example 1:
+ *
+ * query = [YapDatabaseQuery queryWithFormat:@"WHERE jid = ?", message.jid];
+ * [secondaryIndex enumerateKeysAndObjectsMatchingQuery:query
+ *                                           usingBlock:^(NSString *key, id object, BOOL *stop){
+ *     ...
+ * }];
+ *
+ * Please note that you can ONLY pass objective-c objects as parameters.
+ * Primitive types such as int, float, double, etc are NOT supported.
+ * You MUST wrap these using NSNumber.
+ *
+ * Example 2:
+ *
+ * query = [YapDatabaseQuery queryWithFormat:@"WHERE department = ? AND salary >= ?", dept, @(minSalary)];
+ * [secondaryIndex enumerateKeysAndObjectsMatchingQuery:query
+ *                                           usingBlock:^(NSString *key, id object, BOOL *stop){
+ *     ...
+ * }];
+ **/
 + (instancetype)queryWithFormat:(NSString *)format, ...
 {
     va_list arguments;
@@ -118,6 +144,10 @@
 	}
 }
 
+/**
+ * Shorthand for a query with no 'WHERE' clause.
+ * Equivalent to [YapDatabaseQuery queryWithFormat:@""].
+ **/
 + (instancetype)queryMatchingAll
 {
 	return [[YapDatabaseQuery alloc] initWithQueryString:@"" queryParameters:nil];
@@ -138,6 +168,17 @@
 
 @end
 
+/**
+ * Shim that allows YapDatabaseQuery to be used from Swift.
+ *
+ * Define the following somewhere in your Swift code:
+ *
+ * extension YapDatabaseQuery {
+ *     class func queryWithFormat(format: String, _ arguments: CVarArgType...) -> YapDatabaseQuery? {
+ *         return withVaList(arguments, { __YapDatabaseQuerySwift(format, $0) })
+ *     }
+ * }
+ **/
 YapDatabaseQuery *__YapDatabaseQuerySwift(NSString *format, va_list arguments) {
     return [YapDatabaseQuery queryWithFormat:format arguments:arguments];
 }
