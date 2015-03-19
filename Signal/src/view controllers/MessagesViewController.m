8a012a5665ce97cc9c0172544a9fc37a0d11825e
Frederic Jacobs <github@fredericjacobs.com>
2015-03-19 10:05:13 +0100

    Fixes #698

--- Signal/src/view controllers/MessagesViewController.m
+++ Signal/src/view controllers/MessagesViewController.m
@@ -1134,8 +1134,9 @@ typedef enum : NSUInteger {
 
 -(void)chooseFromLibrary {
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-    picker.delegate = self;
-    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate                 = self;
+    picker.sourceType               = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.allowsEditing            = YES;
     
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
         
