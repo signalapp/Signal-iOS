0f57804eefcb837a83db96e35e2f2c70e7c80fe5
Brendan Ragan <lordmortis@gmail.com>
2015-05-01 09:35:37 +0800

    Enable data selectors.

--- Signal/src/view controllers/MessagesViewController.m
+++ Signal/src/view controllers/MessagesViewController.m
@@ -626,7 +626,6 @@ typedef enum : NSUInteger {
     JSQMessagesCollectionViewCell *cell = (JSQMessagesCollectionViewCell *)[super collectionView:self.collectionView cellForItemAtIndexPath:indexPath];
     if (!message.isMediaMessage) {
         cell.textView.textColor          = [UIColor ows_blackColor];
-        cell.textView.selectable         = NO;
         cell.textView.linkTextAttributes = @{ NSForegroundColorAttributeName : cell.textView.textColor,
                                               NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle | NSUnderlinePatternSolid) };
     }
@@ -640,7 +639,6 @@ typedef enum : NSUInteger {
     if (!message.isMediaMessage)
     {
         cell.textView.textColor          = [UIColor whiteColor];
-        cell.textView.selectable         = NO;
         cell.textView.linkTextAttributes = @{ NSForegroundColorAttributeName : cell.textView.textColor,
                                               NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle | NSUnderlinePatternSolid) };
     }
