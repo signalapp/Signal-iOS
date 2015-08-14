f6c25dd61b791006c12c8446d590b29d4cebb1ed
Robbie Hanson <robbiehanson@deusty.com>
2015-08-14 11:13:23 -0700

    Minor documentation fix.

--- YapDatabase/Extensions/Views/YapDatabaseViewTransaction.m
+++ YapDatabase/Extensions/Views/YapDatabaseViewTransaction.m
@@ -3999,7 +3999,7 @@
 
 - (NSUInteger)numberOfGroups
 {
-	// Note: We don't remove pages or groups until preCommitReadWriteTransaction.
+	// Note: We don't remove pages or groups until flushPendingChangesToExtensionTables.
 	// This allows us to recycle pages whenever possible, which reduces disk IO during the commit.
 	
 	__block NSUInteger count = 0;
@@ -4021,7 +4021,7 @@
 
 - (NSArray *)allGroups
 {
-	// Note: We don't remove pages or groups until preCommitReadWriteTransaction.
+	// Note: We don't remove pages or groups until flushPendingChangesToExtensionTables.
 	// This allows us to recycle pages whenever possible, which reduces disk IO during the commit.
 	
 	NSMutableArray *allGroups = [NSMutableArray arrayWithCapacity:[viewConnection->state numberOfGroups]];
@@ -4047,7 +4047,7 @@
 **/
 - (BOOL)hasGroup:(NSString *)group
 {
-	// Note: We don't remove pages or groups until preCommitReadWriteTransaction.
+	// Note: We don't remove pages or groups until flushPendingChangesToExtensionTables.
 	// This allows us to recycle pages whenever possible, which reduces disk IO during the commit.
 	
 	NSArray *pagesMetadataForGroup = [viewConnection->state pagesMetadataForGroup:group];
