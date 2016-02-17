59503381607ba7903f64f52db672e77d730de7b5
maelp <mael.primet@gmail.com>
2016-02-17 13:48:19 +0100

    Add comments

--- YapDatabase/Extensions/CrossProcessNotification/YapDatabaseCrossProcessNotification.h
+++ YapDatabase/Extensions/CrossProcessNotification/YapDatabaseCrossProcessNotification.h
@@ -14,7 +14,18 @@ NS_ASSUME_NONNULL_BEGIN
  * The project wiki has a wealth of documentation if you have any questions.
  * https://github.com/yapstudios/YapDatabase/wiki
  *
- * YapDatabaseCrossProcessNotification is an extension which allows you to be notified when database is updated in another process.
+ * `YapDatabaseCrossProcessNotification` is an extension which allows you to be notified when the
+ * database has been updated in another process.
+ *
+ * A `YapDatabaseModifiedExternallyNotification` notification is posted on the main thread.
+ *
+ * This is useful when the `enableMutiprocessSupport` option has been set, to be notified of external
+ * changes and having an opportunity to reload a view.
+ *
+ * All processes using the database should declare the extension.
+ *
+ * An identifier permits to distinguish each database, and all processes listening on the same database
+ * must use the same identifier.
  *
 **/
 @interface YapDatabaseCrossProcessNotification : YapDatabaseExtension
