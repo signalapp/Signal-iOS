7b1f186cff21643aea1d37ebfddfb6faed826975
maelp <mael.primet@gmail.com>
2016-02-17 23:51:04 +0100

    Test extension

--- Testing/Xcode-desktop/MultiprocessApp/MultiprocessTest.m
+++ Testing/Xcode-desktop/MultiprocessApp/MultiprocessTest.m
@@ -10,6 +10,7 @@
 
 #import "MultiprocessTest.h"
 #import <YapDatabase/YapDatabase.h>
+#import <YapDatabase/YapDatabaseRTreeIndex.h>
 #import <YapDatabase/YapDatabaseCrossProcessNotification.h>
 #import <CocoaLumberjack/CocoaLumberjack.h>
 
@@ -24,7 +25,26 @@ static int counter = 1;
     NSString* currentDir = [[NSFileManager defaultManager] currentDirectoryPath];
     NSString* filePath = [currentDir stringByAppendingPathComponent:@"db.yap"];
     
+    srandom((int)getpid());
+    
     YapDatabase* db = [[YapDatabase alloc] initWithPath:filePath options:options];
+    
+    YapDatabaseRTreeIndexSetup *setup = [[YapDatabaseRTreeIndexSetup alloc] init];
+    [setup setColumns:@[@"minLat",
+                        @"maxLat",
+                        @"minLon",
+                        @"maxLon"]];
+    YapDatabaseRTreeIndexHandler *handler = [YapDatabaseRTreeIndexHandler withObjectBlock:^(NSMutableDictionary *dict, NSString *collection, NSString *key, id object) {
+            dict[@"minLat"] = object[@"latitude"];
+            dict[@"maxLat"] = object[@"latitude"];
+            dict[@"minLon"] = object[@"longitude"];
+            dict[@"maxLon"] = object[@"longitude"];
+    }];
+    YapDatabaseRTreeIndexOptions *rTreeOptions = [[YapDatabaseRTreeIndexOptions alloc] init];
+    NSSet *allowedCollections = [NSSet setWithArray:@[@"coordinates"]];
+    rTreeOptions.allowedCollections = [[YapWhitelistBlacklist alloc] initWithWhitelist:allowedCollections];
+    YapDatabaseRTreeIndex *rTree = [[YapDatabaseRTreeIndex alloc] initWithSetup:setup handler:handler versionTag:@"1" options:rTreeOptions];
+    [db registerExtension:rTree withName:@"rTree"];
     
     YapDatabaseCrossProcessNotification* cpn = [[YapDatabaseCrossProcessNotification alloc] initWithIdentifier:@"mael"];
     [db registerExtension:cpn withName:@"cpn"];
@@ -33,6 +53,8 @@ static int counter = 1;
     
     YapDatabaseConnection* connection1 = [db newConnection];
     YapDatabaseConnection* connection2 = [db newConnection];
+    YapDatabaseConnection* connection3 = [db newConnection];
+    YapDatabaseConnection* connection4 = [db newConnection];
 
     dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
         while (true) {
@@ -57,7 +79,58 @@ static int counter = 1;
             usleep(n * 1000);
         }
     });
+
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
+        while (true) {
+            int lon = random() % 10;
+            int lat = random() % 10;
+            int id = counter++;
+            [connection3 readWriteWithBlock:^(YapDatabaseReadWriteTransaction * _Nonnull transaction) {
+                NSLog(@"%@: RTREE: Writing \"%i, %i\" (%i) to database (snapshot %llu)", name, lon, lat, id, connection1.database.snapshot);
+                NSDictionary* object = @{@"longitude": @(lon), @"latitude": @(lat), @"id": @(id)};
+                [transaction setObject:object forKey:@"key" inCollection:@"coordinates"];
+            }];
+            int n = random() % 5000;
+            usleep(n * 1000);
+        }
+    });
+    
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
+        while (true) {
+            [connection4 readWithBlock:^(YapDatabaseReadTransaction * _Nonnull transaction) {
+                NSMutableArray *results = [NSMutableArray array];
+
+                NSString *queryString = [NSString stringWithFormat:@"WHERE %@ >= ? AND %@ <= ? AND %@ >= ? AND %@ <= ?",
+                                         @"minLon",
+                                         @"maxLon",
+                                         @"minLat",
+                                         @"maxLat"];
+
+                NSArray *parameters = @[@(2),
+                                        @(8),
+                                        @(2),
+                                        @(8)];
+                YapDatabaseQuery *query = [YapDatabaseQuery queryWithString:queryString parameters:parameters];
+
+                YapDatabaseRTreeIndexTransaction *rTree = [transaction ext:@"rTree"];
+                [rTree enumerateKeysAndObjectsMatchingQuery:query usingBlock:^(NSString *collection, NSString *key, id object, BOOL *stop) {
+                    [results addObject:object];
+                }];
+                if (results.count == 0) {
+                    NSLog(@"%@: RTREE: Got no result in [2, 8] x [2, 8]", name);
+                }
+                for (id result in results) {
+                    NSLog(@"%@: RTREE: Got \"(%@, %@, %@)\" (snapshot %llu)", name, result[@"longitude"], result[@"latitude"], result[@"id"], connection4.database.snapshot);
+                }
+                
+            }];
+            int n = random() % 1000;
+            usleep(n * 1000);
+        }
+    });
+
 }
+
 
 - (void)run:(NSString *)name {
     NSRunLoop* runLoop = [NSRunLoop currentRunLoop];
