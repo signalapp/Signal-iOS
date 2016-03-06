3e1f6da5ea5be8e77f3943cb709458bb5d390518
Robbie Hanson <robbiehanson@deusty.com>
2016-03-06 11:34:33 -0800

    Fixing compiler warning: missing include statement

--- YapDatabase/Extensions/CrossProcessNotification/YapDatabaseCrossProcessNotification.m
+++ YapDatabase/Extensions/CrossProcessNotification/YapDatabaseCrossProcessNotification.m
@@ -6,6 +6,8 @@
 
 #import "YapDatabaseLogging.h"
 
+#include <notify.h>
+
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
