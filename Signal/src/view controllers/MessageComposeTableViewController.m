7d8292fd535bbd81aa3dc283268d66b20e56774d
Michael Kirk <michaelkirk@users.noreply.github.com>
2016-04-13 08:10:51 -0700

    show alert must happen in main thread (#1119)
    //freebie

--- Signal/src/view controllers/MessageComposeTableViewController.m
+++ Signal/src/view controllers/MessageComposeTableViewController.m
@@ -479,14 +479,14 @@
           });
         }
         failure:^(NSError *error) {
-          UIAlertView *alert = [[UIAlertView alloc] initWithTitle:TIMEOUT
-                                                          message:TIMEOUT_CONTACTS_DETAIL
-                                                         delegate:nil
-                                                cancelButtonTitle:NSLocalizedString(@"OK", @"")
-                                                otherButtonTitles:nil];
-          [alert show];
-          dispatch_async(dispatch_get_main_queue(), ^{
-            [self updateAfterRefreshTry];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:TIMEOUT
+                                                                message:TIMEOUT_CONTACTS_DETAIL
+                                                               delegate:nil
+                                                      cancelButtonTitle:NSLocalizedString(@"OK", @"")
+                                                      otherButtonTitles:nil];
+                [alert show];
+                [self updateAfterRefreshTry];
           });
         }];
 
