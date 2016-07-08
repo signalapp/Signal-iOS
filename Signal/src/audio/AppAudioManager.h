6a868c132f7c901e1309dd7a478e499d6cf6bcc7
Michael Kirk <michael.john.kirk@gmail.com>
2016-07-08 09:38:29 -0700

    IOS9 Fixes
    * Fix compiler warnings by explicitly casting to int.
    * Show a better permissions message when viewing attachments.
    * Fix comment.
    
    // FREEBIE

--- Signal/src/audio/AppAudioManager.h
+++ Signal/src/audio/AppAudioManager.h
@@ -8,10 +8,10 @@
 /**
  *  The AppAudioManager is a Singleton object used to control audio settings / updates
  *  for the entire application. This includes playing sounds appropriately, Initializing
- *  Audio Settings, and interfacing with the OS. The Call Audio Pipeline it self is delegated
+ *  Audio Settings, and interfacing with the OS. The Call Audio Pipeline itself is delegated
  *  to the RemoteIOAudio Class.
  *
- *  The Audio Profile determines which preset of logic to use for playing sounds, Such as
+ *  The AudioProfile determines which preset of logic to use for playing sounds, Such as
  *  which speaker to use or if all sounds should be muted.
  **/
 
