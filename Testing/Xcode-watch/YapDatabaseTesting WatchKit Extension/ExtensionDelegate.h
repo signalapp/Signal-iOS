fe04b8434fb5389154f4491f5fcc3c1639a4928a
Robbie Hanson <robbiehanson@deusty.com>
2016-07-21 11:16:09 -0700

    Adding Xcode-watch project for unit testing. Encountered a bug with CocoaPods. Awaiting CocoaPods pull request 5608 to be merged.

---
+++ Testing/Xcode-watch/YapDatabaseTesting WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  YapDatabaseTesting WatchKit Extension
+//
+//  Created by Robbie Hanson on 7/21/16.
+//  Copyright © 2016 Deusty LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ Testing/Xcode-watch/YapDatabaseTesting WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  YapDatabaseTesting WatchKit Extension
+//
+//  Created by Robbie Hanson on 7/21/16.
+//  Copyright © 2016 Deusty LLC. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
