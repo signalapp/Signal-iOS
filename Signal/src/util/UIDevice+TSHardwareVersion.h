baf0ea96d6099d39686401939c4149cac5794c86
Matthew Douglass <matthew+github@douglass.email>
2016-10-28 00:18:03 -0700

    Fixes [UIDevice isiPhoneVersionSixOrMore]
    Improves the detection logic for iPhone 6 and above (uses interface idiom and the pixel width of the screen). This fixes detection on the iPhone 7 which was previously broken because it’s model number wasn’t include.
    
    Also removes a number of category methods that weren’t being used in the codebase and were equally incomplete.
    
    // FREEBIE

--- Signal/src/util/UIDevice+TSHardwareVersion.h
+++ Signal/src/util/UIDevice+TSHardwareVersion.h
@@ -14,31 +14,8 @@
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSUInteger, UIDeviceFamily) {
-    UIDeviceFamilyiPhone,
-    UIDeviceFamilyiPod,
-    UIDeviceFamilyiPad,
-    UIDeviceFamilyAppleTV,
-    UIDeviceFamilyUnknown,
-};
-
 
 @interface UIDevice (TSHardwareVersion)
-
-/**
- Returns a machine-readable model name in the format of "iPhone4,1"
- */
-- (NSString *)modelIdentifier;
-
-/**
- Returns a human-readable model name in the format of "iPhone 4S". Fallback of the the `modelIdentifier` value.
- */
-- (NSString *)modelName;
-
-/**
- Returns the device family as a `UIDeviceFamily`
- */
-- (UIDeviceFamily)deviceFamily;
 
 /*
  * Returns true if device is iPhone 6 or 6+
