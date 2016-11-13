signalapp/Signal-iOS

commit 33f6a9552058747d016a788f7de29144e598788e

Explain send failures for text and media messages
fixes #1231

Motivation
----------
Previously when messages failed to send, there was no reason given.
Furthermore, when media messages failed to send there was no indication
that any attempt to send the message even occurred, nor a retry
dialog.

UX Changes
----------
- Show "uploading" status for media
- Show specific error message in retry-send dialog
- Only scroll to bottom when new message is inserted
- Show specific errors when group creation fails

Code Changes
-----------
- Updated incorrect references to TSMessageAdapters which were actually
  references to OWSMessageData
- MessageSender was extracted from SSK MessagesManager
- access MessagesManager as property
- idiomatic init/properties for Env
- log contact intersections
- Move scroll-to-bottom animation to main thread.

// FREEBIE

Signal/src/environment/Environment.m
@@ -1,7 +1,7 @@
+#import "Environment.h"
 #import "Constraints.h"
 #import "DH3KKeyAgreementProtocol.h"
 #import "DebugLogger.h"
-#import "Environment.h"
 #import "FunctionalUtil.h"
 #import "KeyAgreementProtocol.h"
 #import "MessagesViewController.h"
@@ -10,16 +10,13 @@
 #import "SignalsViewController.h"
 #import "TSContactThread.h"
 #import "TSGroupThread.h"
+#import <SignalServiceKit/ContactsUpdater.h>
 
 #define isRegisteredUserDefaultString @"isRegistered"
 
 static Environment *environment = nil;
 
 @implementation Environment
-
-@synthesize testingAndLegacyOptions, errorNoter, keyAgreementProtocolsInDescendingPriority, logging,
-    masterServerSecureEndPoint, defaultRelayName, relayServerHostNameSuffix, certificate, serverPort, zrtpClientId,
-    zrtpVersionId, phoneManager, recentCallManager, contactsManager;
 
 + (Environment *)getCurrent {
     NSAssert((environment != nil), @"Environment is not defined.");
@@ -54,20 +51,23 @@ static Environment *environment = nil;
     return [SecureEndPoint secureEndPointForHost:location identifiedByCertificate:env.certificate];
 }
 
-+ (Environment *)environmentWithLogging:(id<Logging>)logging
-                          andErrorNoter:(ErrorHandlerBlock)errorNoter
-                          andServerPort:(in_port_t)serverPort
-                andMasterServerHostName:(NSString *)masterServerHostName
-                    andDefaultRelayName:(NSString *)defaultRelayName
-           andRelayServerHostNameSuffix:(NSString *)relayServerHostNameSuffix
-                         andCertificate:(Certificate *)certificate
-      andSupportedKeyAgreementProtocols:(NSArray *)keyAgreementProtocolsInDescendingPriority
-                        andPhoneManager:(PhoneManager *)phoneManager
-                   andRecentCallManager:(RecentCallManager *)recentCallManager
-             andTestingAndLegacyOptions:(NSArray *)testingAndLegacyOptions
-                        andZrtpClientId:(NSData *)zrtpClientId
-                       andZrtpVersionId:(NSData *)zrtpVersionId
-                     andContactsManager:(OWSContactsManager *)contactsManager {
+- (instancetype)initWithLogging:(id<Logging>)logging
+                     errorNoter:(ErrorHandlerBlock)errorNoter
+                     serverPort:(in_port_t)serverPort
+           masterServerHostName:(NSString *)masterServerHostName
+               defaultRelayName:(NSString *)defaultRelayName
+      relayServerHostNameSuffix:(NSString *)relayServerHostNameSuffix
+                    certificate:(Certificate *)certificate
+ supportedKeyAgreementProtocols:(NSArray *)keyAgreementProtocolsInDescendingPriority
+                   phoneManager:(PhoneManager *)phoneManager
+              recentCallManager:(RecentCallManager *)recentCallManager
+        testingAndLegacyOptions:(NSArray *)testingAndLegacyOptions
+                   zrtpClientId:(NSData *)zrtpClientId
+                  zrtpVersionId:(NSData *)zrtpVersionId
+                contactsManager:(OWSContactsManager *)contactsManager
+                contactsUpdater:(ContactsUpdater *)contactsUpdater
+                 networkManager:(TSNetworkManager *)networkManager
+{
     ows_require(errorNoter != nil);
     ows_require(zrtpClientId != nil);
     ows_require(zrtpVersionId != nil);
@@ -82,23 +82,29 @@ static Environment *environment = nil;
       return [p isKindOfClass:DH3KKeyAgreementProtocol.class];
     }]);
 
-    Environment *e                = [Environment new];
-    e->errorNoter                 = errorNoter;
-    e->logging                    = logging;
-    e->testingAndLegacyOptions    = testingAndLegacyOptions;
-    e->serverPort                 = serverPort;
-    e->masterServerSecureEndPoint = [SecureEndPoint
+    self = [super init];
+    if (!self) {
+        return self;
+    }
+
+    _errorNoter = errorNoter;
+    _logging = logging;
+    _testingAndLegacyOptions = testingAndLegacyOptions;
+    _serverPort = serverPort;
+    _masterServerSecureEndPoint = [SecureEndPoint
           secureEndPointForHost:[HostNameEndPoint hostNameEndPointWithHostName:masterServerHostName andPort:serverPort]
         identifiedByCertificate:certificate];
-    e->defaultRelayName                          = defaultRelayName;
-    e->certificate                               = certificate;
-    e->relayServerHostNameSuffix                 = relayServerHostNameSuffix;
-    e->keyAgreementProtocolsInDescendingPriority = keyAgreementProtocolsInDescendingPriority;
-    e->phoneManager                              = phoneManager;
-    e->recentCallManager                         = recentCallManager;
-    e->zrtpClientId                              = zrtpClientId;
-    e->zrtpVersionId                             = zrtpVersionId;
-    e->contactsManager                           = contactsManager;
+
+    _defaultRelayName = defaultRelayName;
+    _certificate = certificate;
+    _relayServerHostNameSuffix = relayServerHostNameSuffix;
+    _keyAgreementProtocolsInDescendingPriority = keyAgreementProtocolsInDescendingPriority;
+    _phoneManager = phoneManager;
+    _recentCallManager = recentCallManager;
+    _zrtpClientId = zrtpClientId;
+    _zrtpVersionId = zrtpVersionId;
+    _contactsManager = contactsManager;
+    _networkManager = networkManager;
 
     if (recentCallManager != nil) {
         // recentCallManagers are nil in unit tests because they would require unnecessary allocations. Detailed
@@ -107,12 +113,13 @@ static Environment *environment = nil;
         [recentCallManager watchForCallsThrough:phoneManager untilCancelled:nil];
     }
 
-    return e;
+    return self;
 }
 
 + (PhoneManager *)phoneManager {
     return Environment.getCurrent.phoneManager;
 }
+
 + (id<Logging>)logging {
     // Many tests create objects that rely on Environment only for logging.
     // So we bypass the nil check in getCurrent and silently don't log during unit testing, instead of failing hard.

Podfile
@@ -3,7 +3,7 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 target 'Signal' do
     pod 'SocketRocket',               :git => 'https://github.com/facebook/SocketRocket.git'
-    pod 'SignalServiceKit',           git: 'https://github.com/WhisperSystems/SignalServiceKit.git'
+    pod 'SignalServiceKit',           git: 'https://github.com/WhisperSystems/SignalServiceKit.git', branch: 'mkirk/outgoing-media-status#1231'
     #pod 'SignalServiceKit',           path: '../SignalServiceKit'
     pod 'OpenSSL'
     pod 'PastelogKit',                '~> 1.3'

Signal/Signal-Info.plist
@@ -21,7 +21,7 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>2.6.2</string>
+	<string>2.6.3</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleURLTypes</key>
@@ -38,7 +38,7 @@
 		</dict>
 	</array>
 	<key>CFBundleVersion</key>
-	<string>2.6.2.0</string>
+	<string>2.6.3.4</string>
 	<key>ITSAppUsesNonExemptEncryption</key>
 	<false/>
 	<key>LOGS_EMAIL</key>

Signal/src/AppDelegate.m
@@ -6,6 +6,7 @@
 #import "Environment.h"
 #import "NotificationsManager.h"
 #import "OWSContactsManager.h"
+#import "OWSStaleNotificationObserver.h"
 #import "PreferencesUtil.h"
 #import "PushManager.h"
 #import "Release.h"
@@ -15,9 +16,9 @@
 #import "TSSocketManager.h"
 #import "TextSecureKitEnv.h"
 #import "VersionMigrations.h"
-#import "OWSStaleNotificationObserver.h"
 #import <SignalServiceKit/OWSDisappearingMessagesJob.h>
 #import <SignalServiceKit/OWSIncomingMessageReadObserver.h>
+#import <SignalServiceKit/OWSMessageSender.h>
 
 static NSString *const kStoryboardName                  = @"Storyboard";
 static NSString *const kInitialViewControllerIdentifier = @"UserInitialViewController";
@@ -128,8 +129,16 @@ static NSString *const kURLHostVerifyPrefix             = @"verify";
     [TextSecureKitEnv sharedEnv].contactsManager = [Environment getCurrent].contactsManager;
     [[TSStorageManager sharedManager] setupDatabase];
     [TextSecureKitEnv sharedEnv].notificationsManager = [[NotificationsManager alloc] init];
-    self.incomingMessageReadObserver = [[OWSIncomingMessageReadObserver alloc] initWithStorageManager:[TSStorageManager sharedManager]
-                                                                                      messagesManager:[TSMessagesManager sharedManager]];
+
+    OWSMessageSender *messageSender =
+        [[OWSMessageSender alloc] initWithNetworkManager:[Environment getCurrent].networkManager
+                                          storageManager:[TSStorageManager sharedManager]
+                                         contactsManager:[Environment getCurrent].contactsManager
+                                         contactsUpdater:[Environment getCurrent].contactsUpdater];
+
+    self.incomingMessageReadObserver =
+        [[OWSIncomingMessageReadObserver alloc] initWithStorageManager:[TSStorageManager sharedManager]
+                                                         messageSender:messageSender];
     [self.incomingMessageReadObserver startObserving];
 
     self.staleNotificationObserver = [OWSStaleNotificationObserver new];

Signal/src/Models/OWSCall.m
@@ -178,6 +178,11 @@ NS_ASSUME_NONNULL_BEGIN
     return NO;
 }
 
+- (BOOL)isOutgoingAndDelivered
+{
+    return NO;
+}
+
 - (NSUInteger)messageHash
 {
     return self.hash;

Signal/src/Models/TSMessageAdapaters/OWSMessageData.h
@@ -21,6 +21,7 @@ typedef NS_ENUM(NSInteger, TSMessageAdapterType) {
 @property (nonatomic, readonly) TSMessageAdapterType messageType;
 @property (nonatomic, readonly) TSInteraction *interaction;
 @property (nonatomic, readonly) BOOL isExpiringMessage;
+@property (nonatomic, readonly) BOOL isOutgoingAndDelivered;
 @property (nonatomic, readonly) BOOL shouldStartExpireTimer;
 @property (nonatomic, readonly) uint64_t expiresAtSeconds;
 @property (nonatomic, readonly) uint32_t expiresInSeconds;

Signal/src/Models/TSMessageAdapaters/TSMessageAdapter.h
@@ -23,6 +23,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic) TSInteraction *interaction;
 @property (readonly) TSInfoMessageType infoMessageType;
+@property (nonatomic, readonly) CGFloat mediaViewAlpha;
+@property (nonatomic, readonly) BOOL isOutgoingAndDelivered;
+@property (nonatomic, readonly) BOOL isMediaBeingSent;
 
 @end
 

Signal/src/Models/TSMessageAdapaters/TSMessageAdapter.m
@@ -305,4 +305,31 @@
     return self.outgoingMessageStatus;
 }
 
+- (CGFloat)mediaViewAlpha
+{
+    return (CGFloat)(self.isMediaBeingSent ? 0.75 : 1);
+}
+
+- (BOOL)isMediaBeingSent
+{
+    if ([self.interaction isKindOfClass:[TSOutgoingMessage class]]) {
+        TSOutgoingMessage *outgoingMessage = (TSOutgoingMessage *)self.interaction;
+        if (outgoingMessage.hasAttachments && outgoingMessage.messageState == TSOutgoingMessageStateAttemptingOut) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)isOutgoingAndDelivered
+{
+    if ([self.interaction isKindOfClass:[TSOutgoingMessage class]]) {
+        TSOutgoingMessage *outgoingMessage = (TSOutgoingMessage *)self.interaction;
+        if (outgoingMessage.messageState == TSOutgoingMessageStateDelivered) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
 @end

commit fbcf5fbf075a2b6fed30ee49c90bf3512fc59c14

Properly assign global contacts updater to environment
broken in 33f6a9552058747d016a788f7de29144e598788e

// FREEBIE

Signal/src/environment/Environment.m
@@ -105,6 +105,7 @@ static Environment *environment = nil;
     _zrtpClientId = zrtpClientId;
     _zrtpVersionId = zrtpVersionId;
     _contactsManager = contactsManager;
+    _contactsUpdater = contactsUpdater;
     _networkManager = networkManager;
     _messageSender = messageSender;
 