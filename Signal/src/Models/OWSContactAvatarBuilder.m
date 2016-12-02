d7b060dfb855d22756227791133451daa6f99efb
Russ Shanahan <russshanahan@gmail.com>
2016-12-01 22:19:38 -0500

    Use "#" instead of a placeholder avatar (#1512)
    For consistency with the Android and Desktop client behavior.
    
    // FREEBIE

--- Signal/src/Models/OWSContactAvatarBuilder.m
+++ Signal/src/Models/OWSContactAvatarBuilder.m
@@ -56,11 +56,10 @@ NS_ASSUME_NONNULL_BEGIN
         return cachedAvatar;
     }
 
-    UIImage *image;
+    NSMutableString *initials = [NSMutableString string];
     BOOL contactHasName = [self.contactsManager nameExistsForPhoneIdentifier:self.signalId];
     if (contactHasName) {
         // Make an image from the contact's initials
-        NSMutableString *initials = [NSMutableString string];
         NSArray *words =
             [self.contactName componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
         for (NSString *word in words) {
@@ -69,23 +68,20 @@ NS_ASSUME_NONNULL_BEGIN
                 [initials appendString:[firstLetter uppercaseString]];
             }
         }
-
+        
         NSRange stringRange = { 0, MIN([initials length], (NSUInteger)3) }; // Rendering max 3 letters.
         initials = [[initials substringWithRange:stringRange] mutableCopy];
-        
-        // This looks like initials, let's render an image that's has the user's initials
-        UIColor *backgroundColor = [UIColor backgroundColorForContact:self.signalId];
-        
-        image = [[JSQMessagesAvatarImageFactory avatarImageWithUserInitials:initials
-                                                            backgroundColor:backgroundColor
-                                                                  textColor:[UIColor whiteColor]
-                                                                       font:[UIFont ows_boldFontWithSize:36.0]
-                                                                   diameter:100] avatarImage];
     } else {
-        // This looks like a phone number, let's render an image for an unknown user
-        image = [JSQMessagesAvatarImageFactory circularAvatarHighlightedImage:[UIImage imageNamed:@"unknownContactAvatar"] withDiameter:100];
+        // We don't have a name for this contact, so we can't make an "initials" image
+        [initials appendString:@"#"];
     }
-
+    
+    UIColor *backgroundColor = [UIColor backgroundColorForContact:self.signalId];
+    UIImage *image = [[JSQMessagesAvatarImageFactory avatarImageWithUserInitials:initials
+                                                                 backgroundColor:backgroundColor
+                                                                       textColor:[UIColor whiteColor]
+                                                                            font:[UIFont ows_boldFontWithSize:36.0]
+                                                                        diameter:100] avatarImage];
     [self.contactsManager.avatarCache setObject:image forKey:self.signalId];
     return image;
 }
