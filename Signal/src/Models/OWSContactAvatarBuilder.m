306940abf457da918ca76f09080547d41d8a2ce8
Russ Shanahan <russshanahan@gmail.com>
2016-12-01 22:15:36 -0500

    Improve detection of whether good contact initials exist (#1512)
    Use the new `nameExistsForPhoneIdentifier` method to determine whether
    we're going to get back a phone number string or an actual name for a
    contact.
    
    // FREEBIE

--- Signal/src/Models/OWSContactAvatarBuilder.m
+++ Signal/src/Models/OWSContactAvatarBuilder.m
@@ -56,9 +56,11 @@ NS_ASSUME_NONNULL_BEGIN
         return cachedAvatar;
     }
 
-    NSMutableString *initials = [NSMutableString string];
-
-    if (self.contactName.length > 0) {
+    UIImage *image;
+    BOOL contactHasName = [self.contactsManager nameExistsForPhoneIdentifier:self.signalId];
+    if (contactHasName) {
+        // Make an image from the contact's initials
+        NSMutableString *initials = [NSMutableString string];
         NSArray *words =
             [self.contactName componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
         for (NSString *word in words) {
@@ -67,19 +69,10 @@ NS_ASSUME_NONNULL_BEGIN
                 [initials appendString:[firstLetter uppercaseString]];
             }
         }
-    }
 
-    NSRange stringRange = { 0, MIN([initials length], (NSUInteger)3) }; // Rendering max 3 letters.
-    initials = [[initials substringWithRange:stringRange] mutableCopy];
-
-    // Is this a phone number, or a set of initials?
-    NSCharacterSet *phoneNumberChars = [NSCharacterSet characterSetWithCharactersInString:@"0123456789+()-"];
-    BOOL phoneNumber = ([[initials componentsSeparatedByCharactersInSet:phoneNumberChars] componentsJoinedByString:@""].length == 0);
-    UIImage *image;
-    if (phoneNumber) {
-        // This looks like a phone number, let's render an image for an unknown user
-        image = [JSQMessagesAvatarImageFactory circularAvatarHighlightedImage:[UIImage imageNamed:@"unknownContactAvatar"] withDiameter:100];
-    } else {
+        NSRange stringRange = { 0, MIN([initials length], (NSUInteger)3) }; // Rendering max 3 letters.
+        initials = [[initials substringWithRange:stringRange] mutableCopy];
+        
         // This looks like initials, let's render an image that's has the user's initials
         UIColor *backgroundColor = [UIColor backgroundColorForContact:self.signalId];
         
@@ -88,6 +81,9 @@ NS_ASSUME_NONNULL_BEGIN
                                                                   textColor:[UIColor whiteColor]
                                                                        font:[UIFont ows_boldFontWithSize:36.0]
                                                                    diameter:100] avatarImage];
+    } else {
+        // This looks like a phone number, let's render an image for an unknown user
+        image = [JSQMessagesAvatarImageFactory circularAvatarHighlightedImage:[UIImage imageNamed:@"unknownContactAvatar"] withDiameter:100];
     }
 
     [self.contactsManager.avatarCache setObject:image forKey:self.signalId];
