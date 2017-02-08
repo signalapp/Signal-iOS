1ae670f2f93dde0d63b2d1e0afbae6e0f4f80014
Michael Kirk <michael.code@endoftheworl.de>
2017-02-08 15:49:17 -0500

    Prevent CallKit timeout when placing outgoing call
    More fallout from the outbound call timeout which was causing all
    CallKit calls not promptly answered to show "Call Failed"
    
    Inserting the timeout exacerbated an existing issue: We can't wait for
    long before choosing to fulfill/fail an action without CallKit falling
    over and assuming the call failed.
    
    We don't actually need to consider the case where we "fail to initiate"
    the outgoing call. Instead we say it started "successfully, and if there
    is an error, the existing promise error handling will fail the call at
    that time.
    
    // FREEBIE

--- Signal/src/call/Speakerbox/CallKitCallUIAdaptee.swift
+++ Signal/src/call/Speakerbox/CallKitCallUIAdaptee.swift
@@ -228,14 +228,9 @@ final class CallKitCallUIAdaptee: NSObject, CallUIAdaptee, CXProviderDelegate {
             return
         }
 
-        self.callService.handleOutgoingCall(call).then { () -> Void in
-            action.fulfill()
-            self.provider.reportOutgoingCall(with: call.localId, startedConnectingAt: nil)
-            }.catch { error in
-                Logger.error("\(self.TAG) error \(error) in \(#function)")
-                self.callManager.removeCall(call)
-                action.fail()
-        }
+        _ = self.callService.handleOutgoingCall(call)
+        action.fulfill()
+        self.provider.reportOutgoingCall(with: call.localId, startedConnectingAt: nil)
     }
 
     func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
