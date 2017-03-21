b9b81ca8ef682754e5848f9f033f847dbbc6bcad
Matthew Chen <charlesmchen@gmail.com>
2017-03-21 13:21:31 -0400

    Honor call privacy settings in call notifications.
    // FREEBIE

--- Signal/src/UserInterface/Strings.swift
+++ Signal/src/UserInterface/Strings.swift
@@ -10,6 +10,7 @@ import Foundation
 @objc class CallStrings: NSObject {
     static let callBackButtonTitle = NSLocalizedString("CALLBACK_BUTTON_TITLE", comment: "notification action")
     static let missedCallNotificationBody = NSLocalizedString("MISSED_CALL", comment: "notification title")
-    static let missedCallNotificationBodyWithCallerName = NSLocalizedString("MSGVIEW_MISSED_CALL", comment: "notification title. Embeds {{Caller's Name}}")
+    static let missedCallNotificationBodyWithCallerName = NSLocalizedString("MSGVIEW_MISSED_CALL_WITH_NAME", comment: "notification title. Embeds {{Caller's Name}}")
+    static let missedCallNotificationBodyWithoutCallerName = NSLocalizedString("MSGVIEW_MISSED_CALL_WITHOUT_NAME", comment: "notification title.")
     static let callStatusFormat = NSLocalizedString("CALL_STATUS_FORMAT", comment: "embeds {{Call Status}} in call screen label. For ongoing calls, {{Call Status}} is a seconds timer like 01:23, otherwise {{Call Status}} is a short text like 'Ringing', 'Busy', or 'Failed Call'")
 }
