0b815235976bfbe89755bbf6f55aa9ee09f4b1c7
Michael Kirk <michael.code@endoftheworl.de>
2017-03-22 15:03:04 -0400

    Clearer logging and added an assert per CR
    // FREEBIE

--- Signal/src/util/DisplayableTextFilter.swift
+++ Signal/src/util/DisplayableTextFilter.swift
@@ -15,7 +15,10 @@ import Foundation
         }
 
         if (self.hasExcessiveDiacriticals(text: text)) {
-            return text.folding(options: .diacriticInsensitive, locale: .current)
+            Logger.warn("\(TAG) filtering text for excessive diacriticals.")
+            let filteredText = text.folding(options: .diacriticInsensitive, locale: .current)
+            assert(!self.hasExcessiveDiacriticals(filteredText))
+            return filteredText
         }
 
         return text
@@ -26,7 +29,7 @@ import Foundation
         for char in text.characters.enumerated() {
             let scalarCount = String(char.element).unicodeScalars.count
             if scalarCount > 4 {
-                Logger.warn("\(TAG) filtering undisplayable text \(char.element) scalarCount: \(scalarCount)")
+                Logger.warn("\(TAG) detected excessive diacriticals at \(char.element) scalarCount: \(scalarCount)")
                 return true
             }
         }
