a7296db1fedba750500225152b27c0f7cec8a472
Matthew Chen <charlesmchen@gmail.com>
2017-03-31 17:43:42 -0400

    Add contacts list to “add blocked number” view.
    // FREEBIE

--- Signal/src/views/ContactTableViewCell.m
+++ Signal/src/views/ContactTableViewCell.m
@@ -6,7 +6,9 @@
 #import "Environment.h"
 #import "OWSContactAvatarBuilder.h"
 #import "OWSContactsManager.h"
+#import "UIFont+OWS.h"
 #import "UIUtil.h"
+#import "UIView+OWS.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -19,9 +21,49 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation ContactTableViewCell
 
+- (instancetype)init
+{
+    if (self = [super init]) {
+        [self configureProgrammatically];
+    }
+    return self;
+}
+
 - (nullable NSString *)reuseIdentifier
 {
     return NSStringFromClass(self.class);
+}
+
++ (CGFloat)rowHeight
+{
+    return 59.f;
+}
+
+- (void)configureProgrammatically
+{
+    _avatarView = [UIImageView new];
+    _avatarView.contentMode = UIViewContentModeScaleToFill;
+    _avatarView.image = [UIImage imageNamed:@"empty-group-avatar"];
+    [self.contentView addSubview:_avatarView];
+
+    _nameLabel = [UILabel new];
+    _nameLabel.contentMode = UIViewContentModeLeft;
+    _nameLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+    _nameLabel.font = [UIFont ows_dynamicTypeBodyFont];
+    [self.contentView addSubview:_nameLabel];
+
+    [_avatarView autoVCenterInSuperview];
+    [_avatarView autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:8.f];
+    [_avatarView autoSetDimension:ALDimensionWidth toSize:40.f];
+    [_avatarView autoSetDimension:ALDimensionHeight toSize:40.f];
+
+    [_nameLabel autoPinEdgeToSuperviewEdge:ALEdgeRight];
+    [_nameLabel autoPinEdgeToSuperviewEdge:ALEdgeTop];
+    [_nameLabel autoPinEdgeToSuperviewEdge:ALEdgeBottom];
+    [_nameLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:_avatarView withOffset:12.f];
+
+    // Force layout, since imageView isn't being initally rendered on App Store optimized build.
+    [self layoutSubviews];
 }
 
 - (void)configureWithContact:(Contact *)contact contactsManager:(OWSContactsManager *)contactsManager
