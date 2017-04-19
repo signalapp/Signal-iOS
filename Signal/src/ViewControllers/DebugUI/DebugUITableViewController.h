a37b194dc0217f05ed08a6a4b3dac44434a52b8a
Matthew Chen <charlesmchen@gmail.com>
2017-04-19 09:48:56 -0400

    Add debug functions for sending media messages.
    // FREEBIE

--- Signal/src/ViewControllers/DebugUI/DebugUITableViewController.h
+++ Signal/src/ViewControllers/DebugUI/DebugUITableViewController.h
@@ -10,8 +10,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DebugUITableViewController : OWSTableViewController
 
-+ (void)presentDebugUIForThread:(TSThread *)thread
-             fromViewController:(UIViewController *)fromViewController;
++ (void)presentDebugUIForThread:(TSThread *)thread fromViewController:(UIViewController *)fromViewController;
 
 @end
 
