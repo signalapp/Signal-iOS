d77addc01e7ae34841b48f5222aea2fc15645b97
Michael Kirk <michael.code@endoftheworl.de>
2017-06-07 10:56:06 -0400

    extract session state debug utils into section
    // FREEBIE

--- Signal/src/ViewControllers/DebugUI/DebugUITableViewController.m
+++ Signal/src/ViewControllers/DebugUI/DebugUITableViewController.m
@@ -5,9 +5,9 @@
 #import "DebugUITableViewController.h"
 #import "DebugUIContacts.h"
 #import "DebugUIMessages.h"
+#import "DebugUISessionState.h"
 #import "Signal-Swift.h"
 #import <SignalServiceKit/TSContactThread.h>
-#import <SignalServiceKit/TSStorageManager+SessionStore.h>
 #import <SignalServiceKit/TSThread.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -63,62 +63,19 @@ NS_ASSUME_NONNULL_BEGIN
 
     if ([thread isKindOfClass:[TSContactThread class]]) {
         TSContactThread *contactThread = (TSContactThread *)thread;
-
         [contents
             addSection:[OWSTableSection
-                           sectionWithTitle:@"Session State"
+                           sectionWithTitle:[DebugUISessionState sectionForContactThread:contactThread].headerTitle
                                       items:@[
-                                          [OWSTableItem itemWithTitle:@"Log All Recipient Identities"
-                                                          actionBlock:^{
-                                                              [OWSRecipientIdentity printAllIdentities];
-                                                          }],
-                                          [OWSTableItem itemWithTitle:@"Log All Sessions"
-                                                          actionBlock:^{
-                                                              dispatch_async([OWSDispatch sessionStoreQueue], ^{
-                                                                  [[TSStorageManager sharedManager] printAllSessions];
-                                                              });
-                                                          }],
-                                          [OWSTableItem itemWithTitle:@"Toggle Key Change (Contact Thread Only)"
-                                                          actionBlock:^{
-                                                              DDLogError(
-                                                                  @"Flipping identity Key. Flip again to return.");
-
-                                                              OWSIdentityManager *identityManager =
-                                                                  [OWSIdentityManager sharedManager];
-                                                              NSString *recipientId = [contactThread contactIdentifier];
-
-                                                              NSData *currentKey = [identityManager
-                                                                  identityKeyForRecipientId:recipientId];
-                                                              NSMutableData *flippedKey = [NSMutableData new];
-                                                              const char *currentKeyBytes = currentKey.bytes;
-                                                              for (NSUInteger i = 0; i < currentKey.length; i++) {
-                                                                  const char xorByte = currentKeyBytes[i] ^ 0xff;
-                                                                  [flippedKey appendBytes:&xorByte length:1];
-                                                              }
-
-                                                              OWSAssert(flippedKey.length == 32);
-
-
-                                                              [identityManager saveRemoteIdentity:flippedKey
-                                                                                      recipientId:recipientId];
-                                                          }],
                                           [OWSTableItem
-                                              itemWithTitle:@"Delete session (Contact Thread Only)"
-                                                actionBlock:^{
-                                                    dispatch_async([OWSDispatch sessionStoreQueue], ^{
-                                                        [[TSStorageManager sharedManager]
-                                                            deleteAllSessionsForContact:contactThread
-                                                                                            .contactIdentifier];
-                                                    });
-                                                }],
-                                          [OWSTableItem
-                                              itemWithTitle:@"Send session reset (Contact Thread Only)"
-                                                actionBlock:^{
-                                                    [OWSSessionResetJob
-                                                        runWithContactThread:contactThread
-                                                               messageSender:[Environment getCurrent].messageSender
-                                                              storageManager:[TSStorageManager sharedManager]];
-                                                }]
+                                              disclosureItemWithText:[DebugUISessionState
+                                                                         sectionForContactThread:contactThread]
+                                                                         .headerTitle
+                                                         actionBlock:^{
+                                                             [weakSelf pushPageWithSection:
+                                                                           [DebugUISessionState
+                                                                               sectionForContactThread:contactThread]];
+                                                         }],
                                       ]]];
 
         // After enqueing the notification you may want to background the app or lock the screen before it triggers, so
