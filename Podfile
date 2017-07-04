944cd7beefe69ab93eefe621a504f8c75fe57e5e
Matthew Chen <charlesmchen@gmail.com>
2017-07-04 13:24:25 -0400

    Show app update nag on launch if necessary.
    // FREEBIE

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'Signal' do
     pod 'SocketRocket',               :git => 'https://github.com/facebook/SocketRocket.git'
     pod 'AxolotlKit',                 git: 'https://github.com/WhisperSystems/SignalProtocolKit.git'
     #pod 'AxolotlKit',                 path: '../SignalProtocolKit'
+    pod 'ATAppUpdater'
     pod 'SignalServiceKit',           git: 'https://github.com/WhisperSystems/SignalServiceKit.git'
     #pod 'SignalServiceKit',           path: '../SignalServiceKit'
     pod 'JSQMessagesViewController',  git: 'https://github.com/WhisperSystems/JSQMessagesViewController.git', branch: 'mkirk/retain-keyboard-view'
---
+++ Signal/src/util/AppUpdateNag.m
@@ -0,0 +1,117 @@
+//
+//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//
+
+#import "AppUpdateNag.h"
+#import <ATAppUpdater/ATAppUpdater.h>
+#import <SignalServiceKit/TSStorageManager.h>
+
+NSString *const TSStorageManagerAppUpgradeNagCollection = @"TSStorageManagerAppUpgradeNagCollection";
+NSString *const TSStorageManagerAppUpgradeNagDate = @"TSStorageManagerAppUpgradeNagDate";
+
+@interface AppUpdateNag () <ATAppUpdaterDelegate>
+
+@property (nonatomic, readonly) TSStorageManager *storageManager;
+
+@end
+
+#pragma mark -
+
+@implementation AppUpdateNag
+
++ (instancetype)sharedInstance
+{
+    static AppUpdateNag *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] initDefault];
+    });
+    return sharedInstance;
+}
+
+- (instancetype)initDefault
+{
+    TSStorageManager *storageManager = [TSStorageManager sharedManager];
+
+    return [self initWithStorageManager:storageManager];
+}
+
+- (instancetype)initWithStorageManager:(TSStorageManager *)storageManager
+{
+    self = [super init];
+
+    if (!self) {
+        return self;
+    }
+
+    OWSAssert(storageManager);
+
+    _storageManager = storageManager;
+
+    OWSSingletonAssert();
+
+    return self;
+}
+
+- (void)showAppUpgradeNagIfNecessary
+{
+    NSDate *lastNagDate = [[TSStorageManager sharedManager] dateForKey:TSStorageManagerAppUpgradeNagDate
+                                                          inCollection:TSStorageManagerAppUpgradeNagCollection];
+    const NSTimeInterval kMinute = 60.f;
+    const NSTimeInterval kHour = 60 * kMinute;
+    const NSTimeInterval kDay = 24 * kHour;
+    const NSTimeInterval kNagFrequency = kDay * 14;
+    BOOL canNag = (!lastNagDate || fabs(lastNagDate.timeIntervalSinceNow) > kNagFrequency);
+    if (!canNag) {
+        return;
+    }
+
+    // NOTE: The iTunes app store API exposes "short" version numbers, so
+    // it isn't possible to nag about hotfix releases.
+    ATAppUpdater *updater = [ATAppUpdater sharedUpdater];
+    [updater setAlertTitle:NSLocalizedString(
+                               @"APP_UPDATE_NAG_ALERT_TITLE", @"Title for the 'new app version available' alert.")];
+    [updater setAlertMessage:NSLocalizedString(@"APP_UPDATE_NAG_ALERT_MESSAGE_FORMAT",
+                                 @"Message format for the 'new app version available' alert. Embeds: {{The latest app "
+                                 @"version number.}}.")];
+    [updater setAlertUpdateButtonTitle:NSLocalizedString(@"APP_UPDATE_NAG_ALERT_UPDATE_BUTTON",
+                                           @"Label for the 'update' button in the 'new app version available' alert.")];
+    [updater setAlertCancelButtonTitle:NSLocalizedString(@"TXT_CANCEL_TITLE", @"")];
+    [updater setDelegate:self];
+    [updater showUpdateWithConfirmation];
+}
+
+#pragma mark - ATAppUpdaterDelegate
+
+- (void)appUpdaterDidShowUpdateDialog
+{
+    DDLogInfo(@"%@ %s", self.tag, __PRETTY_FUNCTION__);
+
+    [[TSStorageManager sharedManager] setDate:[NSDate new]
+                                       forKey:TSStorageManagerAppUpgradeNagDate
+                                 inCollection:TSStorageManagerAppUpgradeNagCollection];
+}
+
+- (void)appUpdaterUserDidLaunchAppStore
+{
+    DDLogInfo(@"%@ %s", self.tag, __PRETTY_FUNCTION__);
+}
+
+- (void)appUpdaterUserDidCancel
+{
+    DDLogInfo(@"%@ %s", self.tag, __PRETTY_FUNCTION__);
+}
+
+#pragma mark - Logging
+
++ (NSString *)tag
+{
+    return [NSString stringWithFormat:@"[%@]", self.class];
+}
+
+- (NSString *)tag
+{
+    return self.class.tag;
+}
+
+@end
