ffd8d692c611970bce41ec6289fa7589f18eb5a3
Fabian Kreiser <fabian@fabian-kreiser.com>
2017-07-10 20:38:42 +0200

    -initWithReachabilityRef: now retains the ref argument.
    Without this Clang shows an analyzer warning that after calling -initWithReachabilityRef: the ref should be released. Because -initWithReachabilityRef: doesn’t retain the argument, this would result in a dangling pointer.
    
    Warning: This change fixes the issue in YapDatabase internally, but it introduces a memory leak for consumers that are currently using -initWithReachabilityRef without releasing the ref.

--- YapDatabase/Extensions/ActionManager/Utilities/YapReachability.m
+++ YapDatabase/Extensions/ActionManager/Utilities/YapReachability.m
@@ -116,6 +116,7 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     if (ref) 
     {
         id reachability = [[self alloc] initWithReachabilityRef:ref];
+        CFRelease(ref);
 
         return reachability;
     }
@@ -129,6 +130,7 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     if (ref) 
     {
         id reachability = [[self alloc] initWithReachabilityRef:ref];
+        CFRelease(ref);
         
         return reachability;
     }
@@ -167,7 +169,7 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     if (self != nil) 
     {
         self.reachableOnWWAN = YES;
-        self.reachabilityRef = ref;
+        self.reachabilityRef = CFRetain(ref);
 
         // We need to create a serial queue.
         // We allocate this once for the lifetime of the notifier.
