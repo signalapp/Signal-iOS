b1f240440213a21fd6b52d27d1652db4d0b6d9d3
Matthew Chen <charlesmchen@gmail.com>
2017-07-27 11:53:29 -0400

    Hide empty conversations in home view.
    // FREEBIE

--- Signal/src/AppDelegate.m
+++ Signal/src/AppDelegate.m
@@ -172,6 +172,8 @@ static NSString *const kURLHostVerifyPrefix             = @"verify";
     BOOL shouldShowUpgradeLabel = NO;
     NSString *lastAppVersion = AppVersion.instance.lastAppVersion;
     NSString *lastCompletedLaunchAppVersion = AppVersion.instance.lastCompletedLaunchAppVersion;
+    // Every time we change a database view in such a way that might cause a delay on launch,
+    // we need to bump this constant.
     NSString *lastVersionWithDatabaseViewChange = @"2.15.0";
     BOOL mayNeedUpgrade = ([TSAccountManager isRegistered] &&
         [VersionMigrations isVersion:lastAppVersion atLeast:@"2.0.0" andLessThan:lastVersionWithDatabaseViewChange]);
