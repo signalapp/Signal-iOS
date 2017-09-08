e00111e32e63f55d332a9a383ed4ee802dfee822
Levi Brown <levigroker@gmail.com>
2017-09-08 17:43:54 -0600

    Adding validity check to script

--- _master_build.sh
+++ _master_build.sh
@@ -16,6 +16,8 @@
 
 OPENSSL_VERSION="1.0.2l"
 
+FRAMEWORK_BIN="openssl.framework/openssl"
+
 # macOS configuration
 MAC_HEADER_DEST="OpenSSL-macOS/OpenSSL-macOS/openssl.h"
 MAC_HEADER_TEMPLATE="OpenSSL-macOS/OpenSSL-macOS/openssl_umbrella_template.h"
@@ -44,14 +46,14 @@ function usage()
 {
 	[[ "$@" = "" ]] || echo "$@" >&2
 	echo "Usage:" >&2
-	echo "$0 build|clean" >&2
+	echo "$0 build|valid|clean" >&2
 	echo "    build  Builds OpenSSL libraries from source." >&2
+	echo "    valid  Validates the frameworks." >&2
 	echo "    clean  Removes all build artifacts." >&2
 	echo "" >&2
 	echo "    ex.: $0 build" >&2
 	echo "    ex.: $0 clean" >&2
 	echo "" >&2
-	echo "    All commands will default to 'dev' environment, and 'latest' tag." >&2
     exit 1
 }
 
@@ -77,6 +79,47 @@ function build()
 	echo "Created $HEADER_DEST"
 	
 	echo "Build complete. Please follow the steps under \"Building\" in the README.md file to create the macOS and iOS frameworks."
+}
+
+function valid()
+{
+	local LIB_BIN="${IOS_BUILD_DIR}/${FRAMEWORK_BIN}"
+	
+	if [ -r "${LIB_BIN}" ]; then
+		# Check expected architectures
+		local REZ=$($LIPO_B -info "${LIB_BIN}")
+		if [ "$REZ" != "Architectures in the fat file: OpenSSL-iOS/bin/openssl.framework/openssl are: i386 x86_64 armv7 armv7s arm64 " ]; then
+			echo "ERROR: Unexpected result from $LIPO_B: \"${REZ}\""
+		else
+			echo " GOOD: ${REZ}"
+		fi
+
+		# Check for bitcode where expected
+		local ARCHS=("arm64" "armv7" "armv7s")
+		for ARCH in ${ARCHS[*]}
+		do
+			local REZ=$($OTOOL_B -arch ${ARCH} -l "${LIB_BIN}" | $GREP_B LLVM)
+			if [ "$REZ" == "" ]; then
+				echo "ERROR: Did not find bitcode slice for ${ARCH}."
+			else
+				echo " GOOD: Found bitcode slice for ${ARCH}."
+			fi
+		done
+	
+		# Check for bitcode where not expected
+		local ARCHS=("i386")
+		for ARCH in ${ARCHS[*]}
+		do
+			local REZ=$($OTOOL_B -arch ${ARCH} -l "${LIB_BIN}" | $GREP_B LLVM)
+			if [ "$REZ" != "" ]; then
+				echo "ERROR: Found bitcode slice for ${ARCH}."
+			else
+				echo " GOOD: Did not find bitcode slice for ${ARCH}."
+			fi
+		done
+	else
+		echo "ERROR: \"${LIB_BIN}\" not found. Please be sure it has been built (see README.md)"
+	fi
 }
 
 function clean()
@@ -109,6 +152,9 @@ set -eu
 
 # Fully qualified binaries (_B suffix to prevent collisions)
 RM_B="/bin/rm"
+GREP_B="/usr/bin/grep"
+LIPO_B="/usr/bin/lipo"
+OTOOL_B="/usr/bin/otool"
 
 if [[ $# -eq 0 ]]; then
 	usage
@@ -120,6 +166,13 @@ case $command in
     build)
 		if [[ $# -le 0 ]]; then
 			build
+		else
+			usage
+		fi
+    ;;
+    valid)
+		if [[ $# -le 0 ]]; then
+			valid
 		else
 			usage
 		fi
