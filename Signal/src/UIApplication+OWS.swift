43370ffc3fb1d18ba81d10da560d9424f49becfe
Matthew Chen <charlesmchen@gmail.com>
2017-09-12 17:14:43 -0400

    Fix assert during calls without camera permission.
    // FREEBIE

--- Signal/src/UIApplication+OWS.swift
+++ Signal/src/UIApplication+OWS.swift
@@ -6,12 +6,25 @@ import Foundation
 
 extension UIApplication {
 
+    var frontmostViewControllerIgnoringAlerts: UIViewController? {
+        return findFrontmostViewController(ignoringAlerts:true)
+    }
+
     var frontmostViewController: UIViewController? {
+        return findFrontmostViewController(ignoringAlerts:false)
+    }
+
+    internal func findFrontmostViewController(ignoringAlerts: Bool) -> UIViewController? {
         let window = UIApplication.shared.keyWindow
         var viewController = window!.rootViewController
-        
+
         while true {
             if let nextViewController = viewController?.presentedViewController {
+                if ignoringAlerts {
+                    if nextViewController as? UIAlertController != nil {
+                        break
+                    }
+                }
                 viewController = nextViewController
             } else if let navigationController = viewController as? UINavigationController {
                 if let nextViewController = navigationController.topViewController {
