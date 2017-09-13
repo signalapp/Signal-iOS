023c804a619fbe89d79ed891d4ed4cf467eb2ea8
Matthew Chen <charlesmchen@gmail.com>
2017-09-13 18:03:02 -0400

    Decrypt and process messages in batches.
    // FREEBIE

--- SignalServiceKit/src/Messages/OWSBatchMessageProcessor.h
+++ SignalServiceKit/src/Messages/OWSBatchMessageProcessor.h
@@ -7,6 +7,9 @@ NS_ASSUME_NONNULL_BEGIN
 @class OWSSignalServiceProtosEnvelope;
 @class YapDatabase;
 
+// This class is used to write incoming (decrypted, unprocessed)
+// messages to a durable queue and then process them in batches,
+// in the order in which they were received.
 @interface OWSBatchMessageProcessor : NSObject
 
 + (instancetype)sharedInstance;
