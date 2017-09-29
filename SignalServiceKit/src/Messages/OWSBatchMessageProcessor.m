08e560f9695dc0654b39855ef85b38099e3ae210
Matthew Chen <charlesmchen@gmail.com>
2017-09-29 14:38:32 -0400

    Respond to CR.
    // FREEBIE

--- SignalServiceKit/src/Messages/OWSBatchMessageProcessor.m
+++ SignalServiceKit/src/Messages/OWSBatchMessageProcessor.m
@@ -318,7 +318,7 @@ NSString *const OWSMessageContentJobFinderExtensionGroup = @"OWSMessageContentJo
     AssertOnDispatchQueue(self.serialQueue);
 
     // We want a value that is just high enough to yield perf benefits.
-    const NSUInteger kIncomingMessageBatchSize = 16;
+    const NSUInteger kIncomingMessageBatchSize = 32;
 
     NSArray<OWSMessageContentJob *> *jobs = [self.finder nextJobsForBatchSize:kIncomingMessageBatchSize];
     OWSAssert(jobs);
@@ -341,7 +341,7 @@ NSString *const OWSMessageContentJobFinderExtensionGroup = @"OWSMessageContentJo
     // This delay won't affect the first message to arrive when this queue is idle,
     // so by definition we're receiving more than one message and can benefit from
     // batching.
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.f * NSEC_PER_SEC)), self.serialQueue, ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5f * NSEC_PER_SEC)), self.serialQueue, ^{
         [self drainQueueWorkStep];
     });
 }
