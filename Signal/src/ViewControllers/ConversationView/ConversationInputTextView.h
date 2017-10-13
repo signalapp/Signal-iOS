2ec8522355e6e02c5a3029a78ec6375ab4440feb
Matthew Chen <charlesmchen@gmail.com>
2017-10-13 11:17:55 -0400

    Fix the input toolbar.
    // FREEBIE

--- Signal/src/ViewControllers/ConversationView/ConversationInputTextView.h
+++ Signal/src/ViewControllers/ConversationView/ConversationInputTextView.h
@@ -2,8 +2,6 @@
 //  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
 //
 
-//#import <JSQMessagesViewController/JSQMessagesViewController.h>
-
 NS_ASSUME_NONNULL_BEGIN
 
 @class SignalAttachment;
