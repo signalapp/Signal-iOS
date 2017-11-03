de0aabd37ce8fffa707065898038b2c2d3335df6
Michael Kirk <michael.code@endoftheworl.de>
2017-11-03 09:52:04 -0400

    Add GIAG3 to censorship circumvention trust store
    openssl x509 -outform der -in GIAG3.pem -out GIAG3.crt
    
    // FREEBIE

--- SignalServiceKit/src/Network/OWSSignalService.m
+++ SignalServiceKit/src/Network/OWSSignalService.m
@@ -5,6 +5,7 @@
 #import "OWSSignalService.h"
 #import "NSNotificationCenter+OWS.h"
 #import "OWSCensorshipConfiguration.h"
+#import "OWSError.h"
 #import "OWSHTTPSecurityPolicy.h"
 #import "TSAccountManager.h"
 #import "TSConstants.h"
@@ -259,35 +260,63 @@ NSString *const kNSNotificationName_IsCensorshipCircumventionActiveDidChange =
 
 #pragma mark - Google Pinning Policy
 
++ (nullable NSData *)certificateDataWithName:(NSString *)name error:(NSError **)error
+{
+    if (!name.length) {
+        OWSFail(@"%@ expected name with length > 0", self.tag);
+        *error = OWSErrorMakeAssertionError();
+        return nil;
+    }
+
+    NSString *path = [NSBundle.mainBundle pathForResource:name ofType:@"crt"];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        OWSFail(@"%@ Missing certificate for name: %@", self.tag, name);
+        *error = OWSErrorMakeAssertionError();
+        return nil;
+    }
+
+    NSData *_Nullable certData = [NSData dataWithContentsOfFile:path options:0 error:error];
+
+    if (*error != nil) {
+        OWSFail(@"%@ Failed to read cert file with path: %@", self.tag, path);
+        return nil;
+    }
+
+    if (certData.length == 0) {
+        OWSFail(@"%@ empty certData for name: %@", self.tag, name);
+        return nil;
+    }
+
+    DDLogVerbose(@"%@ read cert data with name: %@ length: %lu", self.tag, name, certData.length);
+    return certData;
+}
+
 /**
  * We use the Google Pinning Policy when connecting to our censorship circumventing reflector,
  * which is hosted on Google.
  */
-+ (AFSecurityPolicy *)googlePinningPolicy {
++ (AFSecurityPolicy *)googlePinningPolicy
+{
     static AFSecurityPolicy *securityPolicy = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSError *error;
-        NSString *path = [NSBundle.mainBundle pathForResource:@"GIAG2" ofType:@"crt"];
+        NSData *giag2CertData = [self certificateDataWithName:@"GIAG2" error:&error];
+        if (error) {
+            DDLogError(@"%@ Failed to get GIAG2 certificate data with error: %@", self.tag, error);
+            @throw [NSException exceptionWithName:@"OWSSignalService_UnableToReadCertificate"
+                                           reason:error.description
+                                         userInfo:nil];
+        }
+        NSData *giag3CertData = [self certificateDataWithName:@"GIAG3" error:&error];
+        if (error) {
+            DDLogError(@"%@ Failed to get GIAG3 certificate data with error: %@", self.tag, error);
+            @throw [NSException exceptionWithName:@"OWSSignalService_UnableToReadCertificate"
+                                           reason:error.description
+                                         userInfo:nil];
+        }
 
-        if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
-            @throw [NSException
-                    exceptionWithName:@"Missing server certificate"
-                    reason:[NSString stringWithFormat:@"Missing signing certificate for service googlePinningPolicy"]
-                    userInfo:nil];
-        }
-        
-        NSData *googleCertData = [NSData dataWithContentsOfFile:path options:0 error:&error];
-        if (!googleCertData) {
-            if (error) {
-                @throw [NSException exceptionWithName:@"OWSSignalServiceHTTPSecurityPolicy" reason:@"Couln't read google pinning cert" userInfo:nil];
-            } else {
-                NSString *reason = [NSString stringWithFormat:@"Reading google pinning cert faile with error: %@", error];
-                @throw [NSException exceptionWithName:@"OWSSignalServiceHTTPSecurityPolicy" reason:reason userInfo:nil];
-            }
-        }
-        
-        NSSet<NSData *> *certificates = [NSSet setWithObject:googleCertData];
+        NSSet<NSData *> *certificates = [NSSet setWithArray:@[ giag2CertData, giag3CertData ]];
         securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate withPinnedCertificates:certificates];
     });
     return securityPolicy;
