36703d3bbde72f5afc9a8d8f7c7caa73247ea8bc
Matthew Chen <charlesmchen@gmail.com>
2017-12-07 10:35:47 -0500

    Add asserts around Swift singletons.

--- SignalMessaging/utils/OWSSwiftUtils.swift
+++ SignalMessaging/utils/OWSSwiftUtils.swift
@@ -20,30 +20,3 @@ public func owsFail(_ message: String) {
     Logger.flush()
     assertionFailure(message)
 }
-
-public class SwiftSingletons: NSObject {
-    public static let shared = SwiftSingletons()
-
-    private var classSet = Set<String>()
-
-    private override init() {
-        super.init()
-    }
-
-    public func register(_ singleton: AnyObject) {
-        guard _isDebugAssertConfiguration() else {
-            return
-        }
-        let singletonClassName = String(describing:type(of:singleton))
-        guard !classSet.contains(singletonClassName) else {
-            owsFail("\(self.logTag()) in \(#function) Duplicate singleton: \(singletonClassName).")
-            return
-        }
-        Logger.verbose("\(self.logTag()) in \(#function) Registering singleton: \(singletonClassName).")
-        classSet.insert(singletonClassName)
-    }
-
-    public static func register(_ singleton: AnyObject) {
-        shared.register(singleton)
-    }
-}
