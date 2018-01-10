41e6eaeafcad41d17d446f3e50736980acdafa24
Matthew Chen <charlesmchen@gmail.com>
2018-01-10 12:51:29 -0500

    Skip redundant sync messages.

--- SignalServiceKit/src/Devices/OWSDevice.m
+++ SignalServiceKit/src/Devices/OWSDevice.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
 #import "OWSDevice.h"
@@ -52,7 +52,7 @@ NSString *const kTSStorageManager_MayHaveLinkedDevices = @"kTSStorageManager_May
         if (!self.mayHaveLinkedDevicesCached) {
             self.mayHaveLinkedDevicesCached = @([dbConnection boolForKey:kTSStorageManager_MayHaveLinkedDevices
                                                             inCollection:kTSStorageManager_OWSDeviceCollection
-                                                            defaultValue:YES]);
+                                                            defaultValue:NO]);
         }
 
         return [self.mayHaveLinkedDevicesCached boolValue];
