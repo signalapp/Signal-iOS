6eddfae214f38f4ee4e179a65f21495dfaa59c83
Matthew Chen <charlesmchen@gmail.com>
2018-01-29 10:58:38 -0500

    Improve post-migration testing tools.

--- Signal/src/AppDelegate.m
+++ Signal/src/AppDelegate.m
@@ -220,6 +220,16 @@ static NSString *const kURLHostVerifyPrefix             = @"verify";
         return;
     }
 
+    DDLogInfo(@"%@ Database file size: %@",
+        self.logTag,
+        [OWSFileSystem fileSizeOfPath:TSStorageManager.legacyDatabaseFilePath]);
+    DDLogInfo(@"%@ \t SHM file size: %@",
+        self.logTag,
+        [OWSFileSystem fileSizeOfPath:TSStorageManager.legacyDatabaseFilePath_SHM]);
+    DDLogInfo(@"%@ \t WAL file size: %@",
+        self.logTag,
+        [OWSFileSystem fileSizeOfPath:TSStorageManager.legacyDatabaseFilePath_WAL]);
+
     NSError *_Nullable error = [self convertDatabaseIfNecessary];
     // TODO: Handle this error.
     OWSAssert(!error);
