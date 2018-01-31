43ed8d9a5dfd21fbd6667efd0f7e22ae82313976
Matthew Chen <charlesmchen@gmail.com>
2018-01-31 14:11:40 -0500

    Send "disappearing messages" state for groups.

--- SignalServiceKit/src/Devices/OWSGroupsOutputStream.m
+++ SignalServiceKit/src/Devices/OWSGroupsOutputStream.m
@@ -1,9 +1,13 @@
-//  Copyright © 2016 Open Whisper Systems. All rights reserved.
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+//
 
 #import "OWSGroupsOutputStream.h"
 #import "MIMETypeUtil.h"
+#import "OWSDisappearingMessagesConfiguration.h"
 #import "OWSSignalServiceProtos.pb.h"
 #import "TSGroupModel.h"
+#import "TSGroupThread.h"
 #import <ProtocolBuffers/CodedOutputStream.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -32,6 +36,16 @@ NS_ASSUME_NONNULL_BEGIN
     uint32_t groupDataLength = (uint32_t)groupData.length;
     [self.delegateStream writeRawVarint32:groupDataLength];
     [self.delegateStream writeRawData:groupData];
+
+    TSGroupThread *_Nullable groupThread = [TSGroupThread threadWithGroupId:group.groupId];
+    if (groupThread) {
+        OWSDisappearingMessagesConfiguration *_Nullable disappearingMessagesConfiguration =
+            [OWSDisappearingMessagesConfiguration fetchObjectWithUniqueID:groupThread.uniqueId];
+
+        if (disappearingMessagesConfiguration && disappearingMessagesConfiguration.isEnabled) {
+            [groupBuilder setExpireTimer:disappearingMessagesConfiguration.durationSeconds];
+        }
+    }
 
     if (avatarPng) {
         [self.delegateStream writeRawData:avatarPng];
