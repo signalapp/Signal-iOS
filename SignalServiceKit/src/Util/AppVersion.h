9c8178653d3c4fbe82bf303943956b7c175dad6b
Matthew Chen <charlesmchen@gmail.com>
2018-02-12 10:00:20 -0500

    Revisit TODOs in the SAE work.

--- SignalServiceKit/src/Util/AppVersion.h
+++ SignalServiceKit/src/Util/AppVersion.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
 @interface AppVersion : NSObject
@@ -11,10 +11,13 @@
 // Unlike lastAppVersion, this property isn't updated until
 // appLaunchDidComplete is called.
 @property (nonatomic, readonly) NSString *lastCompletedLaunchAppVersion;
+@property (nonatomic, readonly) NSString *lastCompletedLaunchMainAppVersion;
+@property (nonatomic, readonly) NSString *lastCompletedLaunchSAEAppVersion;
 
 + (instancetype)instance;
 
-- (void)appLaunchDidComplete;
+- (void)mainAppLaunchDidComplete;
+- (void)saeLaunchDidComplete;
 
 - (BOOL)isFirstLaunch;
 
