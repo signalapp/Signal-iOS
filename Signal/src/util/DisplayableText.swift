6075f98197cbdd5f270a9bc28e80908c13b4dda2
Matthew Chen <charlesmchen@gmail.com>
2018-02-15 22:02:12 -0500

    Start work on Indic script crash.

--- Signal/src/util/DisplayableText.swift
+++ Signal/src/util/DisplayableText.swift
@@ -185,7 +185,7 @@ extension String {
         if string == "" {
             return 0
         }
-        if string.characters.count > Int(kMaxJumbomojiCount * kMaxCharactersPerEmojiCount) {
+        if string.count > Int(kMaxJumbomojiCount * kMaxCharactersPerEmojiCount) {
             return 0
         }
         guard string.containsOnlyEmoji else {
@@ -202,29 +202,10 @@ extension String {
 
     @objc
     class func displayableText(_ text: String?) -> String? {
-        guard let text = text?.ows_stripped().filterStringForDisplay() else {
+        guard let text = text else {
             return nil
         }
 
-        if (self.hasExcessiveDiacriticals(text: text)) {
-            Logger.warn("\(TAG) filtering text for excessive diacriticals.")
-            let filteredText = text.folding(options: .diacriticInsensitive, locale: .current)
-            return filteredText.ows_stripped()
-        }
-
-        return text.ows_stripped()
-    }
-
-    private class func hasExcessiveDiacriticals(text: String) -> Bool {
-        // discard any zalgo style text, by detecting maximum number of glyphs per character
-        for char in text.characters.enumerated() {
-            let scalarCount = String(char.element).unicodeScalars.count
-            if scalarCount > 4 {
-                Logger.warn("\(TAG) detected excessive diacriticals at \(char.element) scalarCount: \(scalarCount)")
-                return true
-            }
-        }
-
-        return false
+        return text.filterStringForDisplay()
     }
 }
--- SignalServiceKit/src/Util/NSString+SSK.m
+++ SignalServiceKit/src/Util/NSString+SSK.m
@@ -24,22 +24,68 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 // See: https://manishearth.github.io/blog/2018/02/15/picking-apart-the-crashing-ios-string/
-- (NSString *)filterStringForDisplay
+- (NSString *)filterForIndicScripts
 {
-    NSString *stripped = self.ows_stripped;
-
     if (!NSString.shouldFilterIndic) {
-        return stripped;
+        return self;
     }
-    NSMutableString *result = [NSMutableString new];
-    for (NSUInteger i = 0; i < stripped.length; i++) {
-        unichar c = [stripped characterAtIndex:i];
+
+    NSMutableString *filteredForIndic = [NSMutableString new];
+    for (NSUInteger i = 0; i < self.length; i++) {
+        unichar c = [self characterAtIndex:i];
         if (c == 0x200C) {
             continue;
         }
-        [result appendFormat:@"%C", c];
+        [filteredForIndic appendFormat:@"%C", c];
     }
-    return [result copy];
+    return [filteredForIndic copy];
+}
+
+- (NSString *)filterStringForDisplay
+{
+    return self.ows_stripped.filterForIndicScripts.filterForExcessiveDiacriticals;
+}
+
+- (NSString *)filterForExcessiveDiacriticals
+{
+    if (!self.hasExcessiveDiacriticals) {
+        return self;
+    }
+    return [self stringByFoldingWithOptions:NSDiacriticInsensitiveSearch locale:[NSLocale currentLocale]];
+}
+
+- (BOOL)hasExcessiveDiacriticals
+{
+    // discard any zalgo style text, by detecting maximum number of glyphs per character
+    NSUInteger index = 0;
+    while (index < self.length) {
+        // Walk the grapheme clusters in the string.
+        NSRange range = [self rangeOfComposedCharacterSequenceAtIndex:index];
+        if (range.length > 4) {
+            // There are too many characters in this grapheme cluster.
+            return YES;
+        } else if (range.location != index || range.length < 1) {
+            // This should never happen.
+            OWSFail(
+                @"%@ unexpected composed character sequence: %zd, %@", self.logTag, index, NSStringFromRange(range));
+            return YES;
+        }
+        index = range.location + range.length;
+    }
+    return NO;
+}
+
+- (BOOL)isValidE164
+{
+    NSError *error = nil;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\+\\d+$"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
+    if (error || !regex) {
+        OWSFail(@"%@ could not compile regex: %@", self.logTag, error);
+        return NO;
+    }
+    return [regex rangeOfFirstMatchInString:self options:0 range:NSMakeRange(0, self.length)].location != NSNotFound;
 }
 
 @end
