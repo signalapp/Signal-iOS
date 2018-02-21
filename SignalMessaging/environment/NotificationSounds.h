60d839d7a4a34dd90a91863d6c28d72e3047f84f
Matthew Chen <charlesmchen@gmail.com>
2018-02-20 21:59:15 -0500

    Custom notification sounds.

--- SignalMessaging/environment/NotificationSounds.h
+++ SignalMessaging/environment/NotificationSounds.h
@@ -4,7 +4,18 @@
 
 typedef NS_ENUM(NSUInteger, NotificationSound) {
     NotificationSound_Aurora = 0,
-    NotificationSound_Default = NotificationSound_Aurora
+    NotificationSound_Bamboo,
+    NotificationSound_Chord,
+    NotificationSound_Circles,
+    NotificationSound_Complete,
+    NotificationSound_Hello,
+    NotificationSound_Input,
+    NotificationSound_Keys,
+    NotificationSound_Note,
+    NotificationSound_Popcorn,
+    NotificationSound_Pulse,
+    NotificationSound_Synth,
+    NotificationSound_Default = NotificationSound_Note,
 };
 
 NS_ASSUME_NONNULL_BEGIN
