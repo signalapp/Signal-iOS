e0800b9610278bfbbe0af6a888553e18884242b4
Fredrik Lillejordet <fretholil@gmail.com>
2018-03-04 12:05:03 +0100

    forget target reference to new class in PR

--- Signal/src/ViewControllers/Utils/NeverClearView.swift
+++ Signal/src/ViewControllers/Utils/NeverClearView.swift
@@ -1,7 +1,7 @@
 // Created by Fredrik Lillejordet on 04.03.2018.
 // Copyright © 2018 Open Whisper Systems. All rights reserved.
 
-@objc class NeverClearUIView: UIView {
+@objc class NeverClearView: UIView {
     override var backgroundColor: UIColor? {
         didSet {
             if backgroundColor != nil && backgroundColor!.cgColor.alpha == 0 {
