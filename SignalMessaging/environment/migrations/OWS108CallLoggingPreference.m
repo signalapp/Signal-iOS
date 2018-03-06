79ee5ed216d80be8b1163c76fa1beddb7981187c
Michael Kirk <michael.code@endoftheworl.de>
2018-03-05 21:32:35 -0500

    Be more conservative about logging legacy users into "Recents"
    // FREEBIE

---
+++ SignalMessaging/environment/migrations/OWS108CallLoggingPreference.m
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+//
+
+#import "OWS108CallLoggingPreference.h"
+#import "Environment.h"
+#import "OWSPreferences.h"
+#import <YapDatabase/YapDatabaseTransaction.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+// Increment a similar constant for every future DBMigration
+static NSString *const OWS108CallLoggingPreferenceId = @"108";
+
+@implementation OWS108CallLoggingPreference
+
++ (NSString *)migrationId
+{
+    return OWS108CallLoggingPreferenceId;
+}
+
+- (void)runUpWithTransaction:(YapDatabaseReadWriteTransaction *)transaction
+{
+    OWSAssert(transaction);
+
+    [[Environment preferences] applyCallLoggingSettingsForLegacyUsersWithTransaction:transaction];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
