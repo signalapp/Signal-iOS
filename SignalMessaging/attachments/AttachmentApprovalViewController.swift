0025661a8d13028e05ce774f5c4b37c90141928d
Michael Kirk <michael@signal.org>
2018-03-20 17:29:35 -0400

    Extract GradientView
    // FREEBIE

--- SignalMessaging/attachments/AttachmentApprovalViewController.swift
+++ SignalMessaging/attachments/AttachmentApprovalViewController.swift
@@ -472,28 +472,6 @@ extension AttachmentApprovalViewController: UIScrollViewDelegate {
     }
 }
 
-// TODO extract into it's own file
-public class GradientView: UIView {
-
-    let gradientLayer = CAGradientLayer()
-
-    public required init(from fromColor: UIColor, to toColor: UIColor) {
-        gradientLayer.colors = [fromColor.cgColor, toColor.cgColor]
-        super.init(frame: CGRect.zero)
-
-        self.layer.addSublayer(gradientLayer)
-    }
-
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-        gradientLayer.frame = self.bounds
-    }
-}
-
 protocol CaptioningToolbarDelegate: class {
     func captioningToolbarDidTapSend(_ captioningToolbar: CaptioningToolbar, captionText: String?)
     func captioningToolbarDidBeginEditing(_ captioningToolbar: CaptioningToolbar)
