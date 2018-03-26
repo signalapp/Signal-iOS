3a5ba15d21333394274e8d7613a490819b38a275
Matthew Chen <matthew@signal.org>
2018-03-26 10:54:43 -0400

    Elaborate debug UI for messages.

--- Signal/src/ViewControllers/AppSettings/AppSettingsViewController.m
+++ Signal/src/ViewControllers/AppSettings/AppSettingsViewController.m
@@ -91,10 +91,6 @@
     self.title = NSLocalizedString(@"SETTINGS_NAV_BAR_TITLE", @"Title for settings activity");
 
     [self updateTableContents];
-
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self showDebugUI];
-    });
 }
 
 - (void)viewWillAppear:(BOOL)animated
