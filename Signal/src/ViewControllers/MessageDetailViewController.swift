eb400114c221c0872bbe8d0680fdd7e81465da8a
Matthew Chen <matthew@signal.org>
2018-04-09 10:06:46 -0400

    Fix message details layout in iPhone X.

--- Signal/src/ViewControllers/MessageDetailViewController.swift
+++ Signal/src/ViewControllers/MessageDetailViewController.swift
@@ -146,7 +146,7 @@ class MessageDetailViewController: OWSViewController, MediaGalleryDataSourceDele
                 UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
             ]
         } else {
-            scrollView.autoPin(toBottomLayoutGuideOf: self, withInset: 0)
+            scrollView.autoPinEdge(toSuperviewEdge: .bottom)
         }
 
         updateContent()
