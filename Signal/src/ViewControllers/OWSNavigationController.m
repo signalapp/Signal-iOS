12c98f434007928360060364a3ab7b23873cf981
Michael Kirk <michael@signal.org>
2018-05-16 21:37:04 -0400

    cleanup and move to OWSNavigationController
    // FREEBIE

--- Signal/src/ViewControllers/OWSNavigationController.m
+++ Signal/src/ViewControllers/OWSNavigationController.m
@@ -1,8 +1,9 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
 #import "OWSNavigationController.h"
+#import "Signal-Swift.h"
 
 // We use a category to expose UINavigationController's private
 // UINavigationBarDelegate methods.
@@ -19,6 +20,62 @@
 #pragma mark -
 
 @implementation OWSNavigationController
+
+- (instancetype)initWithRootViewController:(UIViewController *)rootViewController
+{
+    //  Attempt 1: negative additionalSafeArea
+    // Failure: additionalSafeArea insets cannot be negative
+    //    UIEdgeInsets newSafeArea = UIEdgeInsetsMake(-50, 30, 20, 30);
+    //    rootViewController.additionalSafeAreaInsets = newSafeArea;
+    
+    // Attempt 2: safeAreaInsets on vc.view
+    // failure. they're already 0
+    //    UIEdgeInsets existingInsets = rootViewController.view.safeAreaInsets;
+    
+    // Attempt 3: override topLayoutGuide?
+    // Failure - not called.
+    // overriding it does no good - it's not called by default layout code.
+    // presumably it just existing if you want to use it as an anchor.
+    
+    // Attemp 4: sizeForChildContentConainer?
+    // Failure - not called.
+    
+    // Attempt 5: autoSetDimension on navbar
+    // Failure: no effect on rendered size
+    
+    // Attempt 6: manually set child frames in will/didLayoutSubviews
+    // glitchy, and viewcontrollers re-layout themselves afterwards anyway
+    
+    // Attempt 7: Since we can't seem to *shrink* the navbar, maybe we can grow it.
+    // make additionalSafeAreaInsets
+    
+    [self updateAdditionalSafeAreaInsets];
+    
+    self = [self initWithNavigationBarClass:[SignalNavigationBar class] toolbarClass:nil];
+    [self pushViewController:rootViewController animated:NO];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(windowManagerCallDidChange:)
+                                                 name:OWSWindowManagerCallDidChangeNotification
+                                               object:nil];
+    
+    return self;
+}
+
+- (void)windowManagerCallDidChange:(NSNotification *)notification
+{
+    DDLogDebug(@"%@ in %s", self.logTag, __PRETTY_FUNCTION__);
+    [self updateAdditionalSafeAreaInsets];
+}
+
+- (void)updateAdditionalSafeAreaInsets
+{
+    if (OWSWindowManager.sharedManager.hasCall) {
+        self.additionalSafeAreaInsets = UIEdgeInsetsMake(64, 0, 0, 0);
+    } else {
+        self.additionalSafeAreaInsets = UIEdgeInsetsZero;
+    }
+}
 
 - (void)viewDidLoad
 {
