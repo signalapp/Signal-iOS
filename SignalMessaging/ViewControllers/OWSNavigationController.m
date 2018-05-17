7ad5f154483640c0cab9da30d153ce1f7c8299fd
Michael Kirk <michael@signal.org>
2018-05-17 16:22:14 -0400

    Restore cancleable back gesture for those VC's that use it
    // FREEBIE

--- SignalMessaging/ViewControllers/OWSNavigationController.m
+++ SignalMessaging/ViewControllers/OWSNavigationController.m
@@ -7,7 +7,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface OWSNavigationController (OWSNavigationController) <UINavigationBarDelegate, NavBarLayoutDelegate>
+@interface UINavigationController (OWSNavigationController) <UINavigationBarDelegate, NavBarLayoutDelegate>
 
 @end
 
@@ -106,7 +106,7 @@ NS_ASSUME_NONNULL_BEGIN
 {
     [super viewDidLoad];
 
-    //    self.interactivePopGestureRecognizer.delegate = self;
+    self.interactivePopGestureRecognizer.delegate = self;
 }
 
 #pragma mark - UINavigationBarDelegate
@@ -114,51 +114,48 @@ NS_ASSUME_NONNULL_BEGIN
 // All OWSNavigationController serve as the UINavigationBarDelegate for their navbar.
 // We override shouldPopItem: in order to cancel some back button presses - for example,
 // if a view has unsaved changes.
-//- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPopItem:(UINavigationItem *)item
-//{
-//    OWSAssert(self.interactivePopGestureRecognizer.delegate == self);
-//    UIViewController *topViewController = self.topViewController;
-//
-//    // wasBackButtonClicked is YES if the back button was pressed but not
-//    // if a back gesture was performed or if the view is popped programmatically.
-//    BOOL wasBackButtonClicked = topViewController.navigationItem == item;
-//    BOOL result = YES;
-//    if (wasBackButtonClicked) {
-//        if ([topViewController conformsToProtocol:@protocol(OWSNavigationView)]) {
-//            id<OWSNavigationView> navigationView = (id<OWSNavigationView>)topViewController;
-//            result = ![navigationView shouldCancelNavigationBack];
-//        }
-//    }
-//
-//    // If we're not going to cancel the pop/back, we need to call the super
-//    // implementation since it has important side effects.
-//    if (result) {
-//        // NOTE: result might end up NO if the super implementation cancels the
-//        //       the pop/back.
-//
-//        // MJK WTF?? This seems super broken. It won't compile now, but how could it ever
-//        // have? This is a delegate method, so how are we calling it on super?
-//        // [super navigationBar:navigationBar shouldPopItem:item];
-//        result =  YES;
-//    }
-//    return result;
-//}
+- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPopItem:(UINavigationItem *)item
+{
+    OWSAssert(self.interactivePopGestureRecognizer.delegate == self);
+    UIViewController *topViewController = self.topViewController;
+
+    // wasBackButtonClicked is YES if the back button was pressed but not
+    // if a back gesture was performed or if the view is popped programmatically.
+    BOOL wasBackButtonClicked = topViewController.navigationItem == item;
+    BOOL result = YES;
+    if (wasBackButtonClicked) {
+        if ([topViewController conformsToProtocol:@protocol(OWSNavigationView)]) {
+            id<OWSNavigationView> navigationView = (id<OWSNavigationView>)topViewController;
+            result = ![navigationView shouldCancelNavigationBack];
+        }
+    }
+
+    // If we're not going to cancel the pop/back, we need to call the super
+    // implementation since it has important side effects.
+    if (result) {
+        // NOTE: result might end up NO if the super implementation cancels the
+        //       the pop/back.
+        [super navigationBar:navigationBar shouldPopItem:item];
+        result =  YES;
+    }
+    return result;
+}
 
 #pragma mark - UIGestureRecognizerDelegate
 
 // We serve as the UIGestureRecognizerDelegate of the interactivePopGestureRecognizer
 // in order to cancel some "back" gestures - for example,
 // if a view has unsaved changes.
-//- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
-//{
-//    UIViewController *topViewController = self.topViewController;
-//    if ([topViewController conformsToProtocol:@protocol(OWSNavigationView)]) {
-//        id<OWSNavigationView> navigationView = (id<OWSNavigationView>)topViewController;
-//        return ![navigationView shouldCancelNavigationBack];
-//    } else {
-//        return YES;
-//    }
-//}
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    UIViewController *topViewController = self.topViewController;
+    if ([topViewController conformsToProtocol:@protocol(OWSNavigationView)]) {
+        id<OWSNavigationView> navigationView = (id<OWSNavigationView>)topViewController;
+        return ![navigationView shouldCancelNavigationBack];
+    } else {
+        return YES;
+    }
+}
 
 @end
 
