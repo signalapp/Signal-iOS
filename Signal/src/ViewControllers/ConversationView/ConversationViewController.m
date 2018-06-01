5c8969ddcb568cb8a150080b68bc0bc39e74c781
Michael Kirk <michael@signal.org>
2018-06-01 11:09:16 -0400

    Fix: input toolbar disappears when tapping media while edit menu is present
    // FREEBIE

--- Signal/src/ViewControllers/ConversationView/ConversationViewController.m
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController.m
@@ -2059,6 +2059,12 @@ typedef enum : NSUInteger {
 
     [self dismissKeyBoard];
 
+    // In case we were presenting edit menu, we need to become first responder before presenting another VC
+    // else UIKit won't restore first responder status to us when the presented VC is dismissed.
+    if (!self.isFirstResponder) {
+        [self becomeFirstResponder];
+    }
+
     if (![viewItem.interaction isKindOfClass:[TSMessage class]]) {
         OWSFail(@"Unexpected viewItem.interaction");
         return;
@@ -2082,6 +2088,11 @@ typedef enum : NSUInteger {
     OWSAssert(attachmentStream);
 
     [self dismissKeyBoard];
+    // In case we were presenting edit menu, we need to become first responder before presenting another VC
+    // else UIKit won't restore first responder status to us when the presented VC is dismissed.
+    if (!self.isFirstResponder) {
+        [self becomeFirstResponder];
+    }
 
     if (![viewItem.interaction isKindOfClass:[TSMessage class]]) {
         OWSFail(@"Unexpected viewItem.interaction");
