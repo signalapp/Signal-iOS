41e505fb6fb1b9535239f86e887c7d76d73cab6b
Matthew Chen <matthew@signal.org>
2018-07-05 12:36:50 -0400

    Tweak relative timestamps.

--- SignalServiceKit/src/Messages/Attachments/TSAttachmentStream.m
+++ SignalServiceKit/src/Messages/Attachments/TSAttachmentStream.m
@@ -469,7 +469,7 @@ NS_ASSUME_NONNULL_BEGIN
     }
 
     if (result == nil) {
-        OWSFail(@"%@ Unable to build thumbnail for attachmentId: %@", self.logTag, self.uniqueId);
+        DDLogError(@"%@ Unable to build thumbnail for attachmentId: %@", self.logTag, self.uniqueId);
         return;
     }
 
