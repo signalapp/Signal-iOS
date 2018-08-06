90002459c40673ca702c7b89cd398eb6afd3547f
Matthew Chen <matthew@signal.org>
2018-08-06 13:02:01 -0400

    Add unit tests around proto wrappers.

--- Signal/test/call/PeerConnectionClientTest.swift
+++ Signal/test/call/PeerConnectionClientTest.swift
@@ -115,8 +115,16 @@ class PeerConnectionClientTest: XCTestCase {
     func testDataChannelMessage() {
         XCTAssertEqual(0, clientDelegate.dataChannelMessages.count)
 
-        let hangup = DataChannelMessage.forHangup(callId: 123)
-        let hangupBuffer = RTCDataBuffer(data: hangup.asData(), isBinary: false)
+        var hangupData: Data
+        do {
+            let hangupBuilder = WebRTCProtoHangup.WebRTCProtoHangupBuilder()
+            hangupBuilder.setId(123)
+            hangupData = try hangupBuilder.buildSerializedData()
+        } catch {
+            XCTFail("Couldn't build proto.")
+            return
+        }
+        let hangupBuffer = RTCDataBuffer(data: hangupData, isBinary: false)
         client.dataChannel(dataChannel, didReceiveMessageWith: hangupBuffer)
 
         waitForPeerConnectionClient()
@@ -124,7 +132,7 @@ class PeerConnectionClientTest: XCTestCase {
         XCTAssertEqual(1, clientDelegate.dataChannelMessages.count)
 
         let dataChannelMessageProto = clientDelegate.dataChannelMessages[0]
-        XCTAssert(dataChannelMessageProto.hasHangup())
+        XCTAssert(dataChannelMessageProto.hasHangup)
 
         let hangupProto = dataChannelMessageProto.hangup!
         XCTAssertEqual(123, hangupProto.id)
