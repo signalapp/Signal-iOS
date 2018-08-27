fe485b644ca85118b370743d5fd75b7b2c976726
Matthew Chen <matthew@signal.org>
2018-08-27 10:51:46 -0400

    cherry-pick Merge branch 'charlesmchen/logSdp'

--- Signal/src/call/PeerConnectionClient.swift
+++ Signal/src/call/PeerConnectionClient.swift
@@ -1109,6 +1109,58 @@ class HardenedRTCSessionDescription {
 
         return RTCSessionDescription.init(type: rtcSessionDescription.type, sdp: description)
     }
+
+    var logSafeDescription: String {
+        #if DEBUG
+        return sdp
+        #else
+        return redactIPV6(sdp: redactIcePwd(sdp: sdp))
+        #endif
+    }
+
+    private func redactIcePwd(sdp: String) -> String {
+        #if DEBUG
+        return sdp
+        #else
+        var text = sdp
+        text = text.replacingOccurrences(of: "\r", with: "\n")
+        text = text.replacingOccurrences(of: "\n\n", with: "\n")
+        let lines = text.components(separatedBy: "\n")
+        let filteredLines: [String] = lines.map { line in
+            guard !line.contains("ice-pwd") else {
+                return "[ REDACTED ice-pwd ]"
+            }
+            return line
+        }
+        let filteredText = filteredLines.joined(separator: "\n")
+        return filteredText
+        #endif
+    }
+
+    private func redactIPV6(sdp: String) -> String {
+        #if DEBUG
+        return sdp
+        #else
+
+        // Example values to match:
+        //
+        // * 2001:0db8:85a3:0000:0000:8a2e:0370:7334
+        // * 2001:db8:85a3::8a2e:370:7334
+        // * ::1
+        // * ::
+        // * ::ffff:192.0.2.128
+        //
+        // See: https://en.wikipedia.org/wiki/IPv6_addresshttps://en.wikipedia.org/wiki/IPv6_address
+        do {
+            let regex = try NSRegularExpression(pattern: "[\\da-f]*:[\\da-f]*:[\\da-f:\\.]*",
+                options: .caseInsensitive)
+            return regex.stringByReplacingMatches(in: sdp, options: [], range: NSRange(location: 0, length: sdp.count), withTemplate: "[ REDACTED_IPV6_ADDRESS ]")
+        } catch {
+            owsFail("Could not redact IPv6 addresses.")
+            return "[Could not redact IPv6 addresses.]"
+        }
+        #endif
+    }
 }
 
 protocol VideoCaptureSettingsDelegate: class {
--- SignalMessaging/utils/OWSScrubbingLogFormatter.m
+++ SignalMessaging/utils/OWSScrubbingLogFormatter.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2017 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
 #import "OWSScrubbingLogFormatter.h"
@@ -8,14 +8,60 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation OWSScrubbingLogFormatter
 
+- (NSRegularExpression *)phoneRegex
+{
+    static NSRegularExpression *regex = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSError *error;
+        regex = [NSRegularExpression regularExpressionWithPattern:@"\\+\\d{7,12}(\\d{3})"
+                                                          options:NSRegularExpressionCaseInsensitive
+                                                            error:&error];
+        if (error || !regex) {
+            OWSFail(@"%@ could not compile regular expression: %@", self.logTag, error);
+        }
+    });
+    return regex;
+}
+
+- (NSRegularExpression *)dataRegex
+{
+    static NSRegularExpression *regex = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSError *error;
+        regex = [NSRegularExpression regularExpressionWithPattern:@"<([\\da-f]{2})[\\da-f]{6}( [\\da-f]{8})*>"
+                                                          options:NSRegularExpressionCaseInsensitive
+                                                            error:&error];
+        if (error || !regex) {
+            OWSFail(@"%@ could not compile regular expression: %@", self.logTag, error);
+        }
+    });
+    return regex;
+}
+
+- (NSRegularExpression *)ipV4AddressRegex
+{
+    static NSRegularExpression *regex = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // NOTE: The group matches the last quad of the IPv4 address.
+        NSError *error;
+        regex = [NSRegularExpression regularExpressionWithPattern:@"\\d+\\.\\d+\\.\\d+\\.(\\d+)"
+                                                          options:NSRegularExpressionCaseInsensitive
+                                                            error:&error];
+        if (error || !regex) {
+            OWSFail(@"%@ could not compile regular expression: %@", self.logTag, error);
+        }
+    });
+    return regex;
+}
+
 - (NSString *__nullable)formatLogMessage:(DDLogMessage *)logMessage
 {
     NSString *logString = [super formatLogMessage:logMessage];
-    NSRegularExpression *phoneRegex =
-        [NSRegularExpression regularExpressionWithPattern:@"\\+\\d{7,12}(\\d{3})"
-                                                  options:NSRegularExpressionCaseInsensitive
-                                                    error:nil];
 
+    NSRegularExpression *phoneRegex = self.phoneRegex;
     logString = [phoneRegex stringByReplacingMatchesInString:logString
                                                      options:0
                                                        range:NSMakeRange(0, [logString length])
@@ -25,15 +71,17 @@ NS_ASSUME_NONNULL_BEGIN
     // We capture only the first two characters of the hex string for logging.
     // example log line: "Called someFunction with nsData: <01234567 89abcdef>"
     //  scrubbed output: "Called someFunction with nsData: [ REDACTED_DATA:01 ]"
-    NSRegularExpression *dataRegex =
-        [NSRegularExpression regularExpressionWithPattern:@"<([\\da-f]{2})[\\da-f]{6}( [\\da-f]{8})*>"
-                                                  options:NSRegularExpressionCaseInsensitive
-                                                    error:nil];
-
+    NSRegularExpression *dataRegex = self.dataRegex;
     logString = [dataRegex stringByReplacingMatchesInString:logString
                                                     options:0
                                                       range:NSMakeRange(0, [logString length])
                                                withTemplate:@"[ REDACTED_DATA:$1... ]"];
+
+    NSRegularExpression *ipV4AddressRegex = self.ipV4AddressRegex;
+    logString = [ipV4AddressRegex stringByReplacingMatchesInString:logString
+                                                           options:0
+                                                             range:NSMakeRange(0, [logString length])
+                                                      withTemplate:@"[ REDACTED_IPV4_ADDRESS:...$1 ]"];
 
     return logString;
 }
