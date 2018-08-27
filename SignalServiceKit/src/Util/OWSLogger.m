4f2f4a44a0381be5d0ef0943fcd7701697dfcf22
Matthew Chen <matthew@signal.org>
2018-08-27 10:04:27 -0400

    Respond to CR.

--- SignalServiceKit/src/Util/OWSLogger.m
+++ SignalServiceKit/src/Util/OWSLogger.m
@@ -8,27 +8,27 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation OWSLogger
 
-+ (void)verbose:(NSString *)logString;
++ (void)verbose:(NSString *)logString
 {
     DDLogVerbose(@"%@", logString);
 }
 
-+ (void)debug:(NSString *)logString;
++ (void)debug:(NSString *)logString
 {
     DDLogDebug(@"%@", logString);
 }
 
-+ (void)info:(NSString *)logString;
++ (void)info:(NSString *)logString
 {
     DDLogInfo(@"%@", logString);
 }
 
-+ (void)warn:(NSString *)logString;
++ (void)warn:(NSString *)logString
 {
     DDLogWarn(@"%@", logString);
 }
 
-+ (void)error:(NSString *)logString;
++ (void)error:(NSString *)logString
 {
     DDLogError(@"%@", logString);
 }
