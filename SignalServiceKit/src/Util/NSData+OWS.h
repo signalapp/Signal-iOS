b00858921df4ad5ec0b3a0cc167b090bc9986051
Matthew Chen <matthew@signal.org>
2018-08-29 16:26:08 -0400

    Update Cocoapods.

--- SignalServiceKit/src/Util/NSData+OWS.h
+++ SignalServiceKit/src/Util/NSData+OWS.h
@@ -2,14 +2,11 @@
 //  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
 //
 
+#import <AxolotlKit/NSData+SPK.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSData (OWS)
-
-/**
- * Compares data in constant time so as to help avoid potential timing attacks.
- */
-- (BOOL)ows_constantTimeIsEqualToData:(NSData *)other;
 
 - (NSData *)dataByAppendingData:(NSData *)data;
 
