e784f9feebf08b7a9f97266148bb623de01e0f3a
Matthew Chen <matthew@signal.org>
2018-09-07 17:24:17 -0400

    Move fakes to SSK/tests.

--- SignalServiceKit/src/Tests/OWSFakeNetworkManager.m
+++ SignalServiceKit/src/Tests/OWSFakeNetworkManager.m
@@ -1,5 +1,6 @@
-//  Created by Michael Kirk on 10/19/16.
-//  Copyright © 2016 Open Whisper Systems. All rights reserved.
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+//
 
 #import "OWSFakeNetworkManager.h"
 
