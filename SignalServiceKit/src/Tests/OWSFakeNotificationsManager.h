72db509a7ac38dd90a24b00086c34bc79587e6d5
Matthew Chen <matthew@signal.org>
2018-09-07 17:24:17 -0400

    Move fakes to SSK/tests.

--- SignalServiceKit/src/Tests/OWSFakeNotificationsManager.h
+++ SignalServiceKit/src/Tests/OWSFakeNotificationsManager.h
@@ -1,5 +1,6 @@
-//  Created by Michael Kirk on 12/18/16.
-//  Copyright © 2016 Open Whisper Systems. All rights reserved.
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+//
 
 #import "NotificationsProtocol.h"
 
