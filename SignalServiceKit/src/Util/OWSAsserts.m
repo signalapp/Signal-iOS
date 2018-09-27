3c22d0b0c80ee0c1add4d69032c486fe648f0478
Matthew Chen <matthew@signal.org>
2018-09-27 14:51:39 -0400

    Swift exit().

--- SignalServiceKit/src/Util/OWSAsserts.m
+++ SignalServiceKit/src/Util/OWSAsserts.m
@@ -3,7 +3,15 @@
 //
 
 #import "OWSAsserts.h"
+#import <SignalServiceKit/SignalServiceKit-Swift.h>
 
 NS_ASSUME_NONNULL_BEGIN
+
+void SwiftExit(NSString *message, const char *file, const char *function, int line)
+{
+    NSString *_file = [NSString stringWithFormat:@"%s", file];
+    NSString *_function = [NSString stringWithFormat:@"%s", function];
+    [OWSSwiftUtils owsFail:message file:_file function:_function line:line];
+}
 
 NS_ASSUME_NONNULL_END
