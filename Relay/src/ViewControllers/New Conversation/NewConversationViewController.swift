2e170eb3b146a384724eb5f20b5ed7fa706950de
Mark Descalzo <mdescalzo@mac.com>
2018-12-03 15:29:29 -0700

    Fixed some deprecated code warnings.

--- Relay/src/ViewControllers/New Conversation/NewConversationViewController.swift
+++ Relay/src/ViewControllers/New Conversation/NewConversationViewController.swift
@@ -650,18 +650,18 @@ extension String {
     
     func substring(from: Int) -> String {
         let fromIndex = index(from: from)
-        return substring(from: fromIndex)
+        return String(self[fromIndex...])
     }
     
     func substring(to: Int) -> String {
         let toIndex = index(from: to)
-        return substring(to: toIndex)
+        return String(self[..<toIndex])
     }
     
     func substring(with r: Range<Int>) -> String {
         let startIndex = index(from: r.lowerBound)
         let endIndex = index(from: r.upperBound)
-        return substring(with: startIndex..<endIndex)
+        return String(self[startIndex..<endIndex])
     }
 
     // https://stackoverflow.com/questions/30450434/figure-out-size-of-uilabel-based-on-string-in-swift#30450559
