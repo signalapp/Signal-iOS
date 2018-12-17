3be85638988773ca9a8ec8617388e07c718a1aac
Michael Kirk <michael@signal.org>
2018-12-17 16:35:39 -0700

    fixup broken tests

--- Signal/src/util/Backup/OWSBackupLazyRestore.swift
+++ Signal/src/util/Backup/OWSBackupLazyRestore.swift
@@ -77,6 +77,9 @@ public class BackupLazyRestore: NSObject {
     public func runIfNecessary() {
         AssertIsOnMainThread()
 
+        guard !CurrentAppContext().isRunningTests else {
+            return
+        }
         guard AppReadiness.isAppReady() else {
             return
         }
