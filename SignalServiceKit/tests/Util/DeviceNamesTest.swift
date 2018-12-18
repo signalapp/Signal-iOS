cd194af31a4d9fa863ce96f73a6c8f5ee3d6736f
Matthew Chen <matthew@signal.org>
2018-12-18 11:26:03 -0500

    Respond to CR.

--- SignalServiceKit/tests/Util/DeviceNamesTest.swift
+++ SignalServiceKit/tests/Util/DeviceNamesTest.swift
@@ -20,7 +20,7 @@ class DeviceNamesTest: SSKBaseTestSwift {
 
     // MARK: 
 
-    func testNotEncrypted() {
+    func testNotEncrypted1() {
 
         let identityKeyPair = Curve25519.generateKeyPair()
 
@@ -34,8 +34,32 @@ class DeviceNamesTest: SSKBaseTestSwift {
             _ = try DeviceNames.decryptDeviceName(inputData: plaintextData,
                                                   identityKeyPair: identityKeyPair)
             XCTFail("Unexpectedly did not throw error.")
+        } catch DeviceNameError.invalidInput {
+            // Expected error.
         } catch {
-            // Failure is expected.
+            owsFailDebug("Unexpected \(error)")
+        }
+    }
+
+    func testNotEncrypted2() {
+
+        let identityKeyPair = Curve25519.generateKeyPair()
+
+        let plaintext = "alice"
+        guard let plaintextData = plaintext.data(using: .utf8) else {
+            XCTFail("Could not convert text to UTF-8.")
+            return
+        }
+        let base64Data = plaintextData.base64EncodedData()
+
+        do {
+            _ = try DeviceNames.decryptDeviceName(inputData: base64Data,
+                                                  identityKeyPair: identityKeyPair)
+            XCTFail("Unexpectedly did not throw error.")
+        } catch DeviceNameError.invalidInput {
+            // Expected error.
+        } catch {
+            owsFailDebug("Unexpected \(error)")
         }
     }
 
