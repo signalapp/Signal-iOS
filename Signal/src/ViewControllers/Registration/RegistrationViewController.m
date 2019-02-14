0fbf3c76d4eeb608a5845d6302728c95f513c206
Matthew Chen <matthew@signal.org>
2019-02-14 13:56:20 -0500

    Move accessibility id macros to UIUtil.h.

--- Signal/src/ViewControllers/Registration/RegistrationViewController.m
+++ Signal/src/ViewControllers/Registration/RegistrationViewController.m
@@ -18,11 +18,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#define SUBVIEW_ACCESSIBILITY_IDENTIFIER(_root_view, _variable_name)                                                   \
-    ([NSString stringWithFormat:@"%@.%@", _root_view.class, _variable_name])
-#define SET_SUBVIEW_ACCESSIBILITY_IDENTIFIER(_root_view, _variable_name)                                               \
-    _variable_name.accessibilityIdentifier = SUBVIEW_ACCESSIBILITY_IDENTIFIER(_root_view, (@ #_variable_name))
-
 #ifdef DEBUG
 
 NSString *const kKeychainService_LastRegistered = @"kKeychainService_LastRegistered";
