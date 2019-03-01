8e7aeccf07edc7d296863223b12757b5595e008d
Matthew Chen <matthew@signal.org>
2019-03-01 16:49:10 -0500

    Enable image editor in production.

--- SignalMessaging/Views/ImageEditor/ImageEditorModel.swift
+++ SignalMessaging/Views/ImageEditor/ImageEditorModel.swift
@@ -43,7 +43,7 @@ public class ImageEditorModel: NSObject {
 
     @objc
     public static var isFeatureEnabled: Bool {
-        return _isDebugAssertConfiguration()
+        return true
     }
 
     @objc
