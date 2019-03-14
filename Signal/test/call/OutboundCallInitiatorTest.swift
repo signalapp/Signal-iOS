75e626a0d38887efe1ab61c92982894e3d42aa99
TJ McConnell <tj.mcconnell@gmail.com>
2019-03-14 12:36:53 -0700

    fix whitespace

--- Signal/test/call/OutboundCallInitiatorTest.swift
+++ Signal/test/call/OutboundCallInitiatorTest.swift
@@ -1,13 +1,13 @@
 //
 //  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
-// 
+//
 
 import XCTest
 @testable import Signal
 
 class FakeOutboundCallInitiator: OutboundCallInitiator {
     var passedRecipientId : String?
-    
+
     override public func initiateCall(recipientId: String,
                                       isVideo: Bool) -> Bool {
         passedRecipientId = recipientId
@@ -16,13 +16,13 @@ class FakeOutboundCallInitiator: OutboundCallInitiator {
 }
 
 class OutboundCallInitiatorTest: SignalBaseTest {
-    
+
     func testMissingCountryCode() {
         let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
         fakeOutboundCallInitiator.initiateCall(handle: "555555555")
         XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
     }
-    
+
     func testE164Number() {
         let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
         fakeOutboundCallInitiator.initiateCall(handle: "+1555555555")
