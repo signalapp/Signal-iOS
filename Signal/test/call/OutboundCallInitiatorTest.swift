bfb9b624c6efabe0bf8819c5ee5c1af8c3c275ea
TJ McConnell <tj.mcconnell@gmail.com>
2019-03-14 13:05:54 -0700

    add more tests

--- Signal/test/call/OutboundCallInitiatorTest.swift
+++ Signal/test/call/OutboundCallInitiatorTest.swift
@@ -18,14 +18,50 @@ class FakeOutboundCallInitiator: OutboundCallInitiator {
 class OutboundCallInitiatorTest: SignalBaseTest {
 
     func testMissingCountryCode() {
+        let testNumber = "5555555555"
         let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
-        fakeOutboundCallInitiator.initiateCall(handle: "555555555")
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
         XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, "+15555555555")
+    }
+
+    func testMissingCountryCodeWithFormat() {
+        let testNumber = "(555) 555-5555"
+        let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
+        XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, "+15555555555")
     }
 
     func testE164Number() {
+        let testNumber = "+15555555555"
         let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
-        fakeOutboundCallInitiator.initiateCall(handle: "+1555555555")
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
         XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, testNumber)
+    }
+
+    func testE164BrazilNumber() {
+        let testNumber = "+553212345678"
+        let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
+        XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, testNumber)
+    }
+
+    func testFranceWithoutPlus() {
+        let testNumber = "33 1 70 39 38 00"
+        let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
+        XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, "+133170393800")
+    }
+
+    func testE164NumberWithFormat() {
+        let testNumber = "+1 (555) 555-5555"
+        let fakeOutboundCallInitiator = FakeOutboundCallInitiator()
+        fakeOutboundCallInitiator.initiateCall(handle: testNumber)
+        XCTAssertNotNil(fakeOutboundCallInitiator.passedRecipientId)
+        XCTAssertEqual(fakeOutboundCallInitiator.passedRecipientId, "+15555555555")
     }
 }
