5df4a0bc17369006bbb93ad241faf63486e8736c
Matthew Chen <matthew@signal.org>
2019-03-19 13:07:52 -0400

    Rework attachment download progress for audio files.

--- Signal/src/ViewControllers/ConversationView/Cells/OWSMessageBubbleView.m
+++ Signal/src/ViewControllers/ConversationView/Cells/OWSMessageBubbleView.m
@@ -838,7 +838,7 @@ NS_ASSUME_NONNULL_BEGIN
                                                                           conversationStyle:self.conversationStyle];
     self.viewItem.lastAudioMessageView = audioMessageView;
     [audioMessageView createContents];
-    [self addProgressViewsIfNecessary:audioMessageView];
+    [self addProgressViewsIfNecessary:audioMessageView shouldShowDownloadProgress:NO];
 
     self.loadCellContentBlock = ^{
         // Do nothing.
@@ -857,7 +857,7 @@ NS_ASSUME_NONNULL_BEGIN
     OWSGenericAttachmentView *attachmentView =
         [[OWSGenericAttachmentView alloc] initWithAttachment:attachment isIncoming:self.isIncoming];
     [attachmentView createContentsWithConversationStyle:self.conversationStyle];
-    [self addProgressViewsIfNecessary:attachmentView];
+    [self addProgressViewsIfNecessary:attachmentView shouldShowDownloadProgress:NO];
 
     self.loadCellContentBlock = ^{
         // Do nothing.
@@ -895,7 +895,7 @@ NS_ASSUME_NONNULL_BEGIN
     // progress or tap-to-retry UI.
     UIView *attachmentView = [UIView new];
 
-    [self addProgressViewsIfNecessary:attachmentView];
+    [self addProgressViewsIfNecessary:attachmentView shouldShowDownloadProgress:YES];
 
     self.loadCellContentBlock = ^{
         // Do nothing.
@@ -907,12 +907,12 @@ NS_ASSUME_NONNULL_BEGIN
     return attachmentView;
 }
 
-- (void)addProgressViewsIfNecessary:(UIView *)bodyMediaView
+- (void)addProgressViewsIfNecessary:(UIView *)bodyMediaView shouldShowDownloadProgress:(BOOL)shouldShowDownloadProgress
 {
     if (self.viewItem.attachmentStream) {
         [self addUploadViewIfNecessary:bodyMediaView];
     } else if (self.viewItem.attachmentPointer) {
-        [self addDownloadViewIfNecessary:bodyMediaView];
+        [self addDownloadViewIfNecessary:bodyMediaView shouldShowDownloadProgress:(BOOL)shouldShowDownloadProgress];
     }
 }
 
@@ -934,7 +934,7 @@ NS_ASSUME_NONNULL_BEGIN
     [uploadView setCompressionResistanceLow];
 }
 
-- (void)addDownloadViewIfNecessary:(UIView *)bodyMediaView
+- (void)addDownloadViewIfNecessary:(UIView *)bodyMediaView shouldShowDownloadProgress:(BOOL)shouldShowDownloadProgress
 {
     OWSAssertDebug(self.viewItem.attachmentPointer);
 
@@ -953,6 +953,9 @@ NS_ASSUME_NONNULL_BEGIN
         case TSAttachmentPointerTypeUnknown:
         case TSAttachmentPointerTypeIncoming:
             break;
+    }
+    if (!shouldShowDownloadProgress) {
+        return;
     }
     NSString *_Nullable uniqueId = self.viewItem.attachmentPointer.uniqueId;
     if (uniqueId.length < 1) {
