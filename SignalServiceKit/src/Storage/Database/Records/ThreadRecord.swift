2038709a6f7b7fbdbda237a75fabfd5a7fd9352d
Michael Kirk <michael@signal.org>
2019-04-10 23:45:32 -0600

    Fix MessageMapping

--- SignalServiceKit/src/Storage/Database/Records/ThreadRecord.swift
+++ SignalServiceKit/src/Storage/Database/Records/ThreadRecord.swift
@@ -10,12 +10,49 @@ import GRDBCipher
 // information to get a migration scaffolded and message windowing working for GRDB.
 
 public extension TSThread {
-    class func fromRecord(_ threadRecord: ThreadRecord) -> TSThread {
-        switch threadRecord.threadType {
-        case .contact:
-            return TSContactThread(uniqueId: threadRecord.uniqueId)
-        case .group:
-            fatalError("TODO")
+    class func fromRecord(_ record: ThreadRecord) -> TSThread {
+        let archivedAsOfMessageSortId: NSNumber?
+        if let value = record.archivedAsOfMessageSortId {
+            archivedAsOfMessageSortId = NSNumber(value: value)
+        } else {
+            archivedAsOfMessageSortId = nil
+        }
+
+        switch record.recordType {
+        case .contactThread:
+            return TSContactThread(uniqueId: record.uniqueId,
+                                   archivalDate: record.archivalDate,
+                                   archivedAsOfMessageSortId: archivedAsOfMessageSortId,
+                                   conversationColorName: record.conversationColorName,
+                                   creationDate: record.creationDate,
+                                   isArchivedByLegacyTimestampForSorting: record.isArchivedByLegacyTimestampForSorting,
+                                   lastMessageDate: record.lastMessageDate,
+                                   messageDraft: record.messageDraft,
+                                   mutedUntilDate: record.mutedUntilDate,
+                                   shouldThreadBeVisible: record.shouldThreadBeVisible,
+                                   hasDismissedOffers: record.hasDismissedOffers ?? false)
+        case .groupThread:
+            guard let serializedGroupModel = record.groupModel else {
+                owsFail("serializedGroupModel was unexpectedly nil")
+            }
+
+            guard let groupModel: TSGroupModel = try? SDSDeserializer.unarchive(serializedGroupModel) else {
+                owsFail("invalid record")
+            }
+
+            return TSGroupThread(uniqueId: record.uniqueId,
+                                 archivalDate: record.archivalDate,
+                                 archivedAsOfMessageSortId: archivedAsOfMessageSortId,
+                                 conversationColorName: record.conversationColorName,
+                                 creationDate: record.creationDate,
+                                 isArchivedByLegacyTimestampForSorting: record.isArchivedByLegacyTimestampForSorting,
+                                 lastMessageDate: record.lastMessageDate,
+                                 messageDraft: record.messageDraft,
+                                 mutedUntilDate: record.mutedUntilDate,
+                                 shouldThreadBeVisible: record.shouldThreadBeVisible,
+                                 groupModel: groupModel)
+        default:
+            owsFail("thread record shouldn't be saved with non-thread recordType: \(record.recordType)")
         }
     }
 }
@@ -29,19 +66,43 @@ extension ThreadRecordType: Codable { }
 extension ThreadRecordType: DatabaseValueConvertible { }
 
 public struct ThreadRecord: Codable, FetchableRecord, PersistableRecord, TableRecord {
-    public static let databaseTableName: String = "threads"
+    public static let databaseTableName: String = TSThreadSerializer.table.tableName
 
     public let id: Int
+    public let recordType: SDSRecordType
     public let uniqueId: String
-    public let shouldBeVisible: Bool
+    public let archivalDate: Date?
+    public let archivedAsOfMessageSortId: UInt64?
+    public let conversationColorName: String
     public let creationDate: Date
-    public let threadType: ThreadRecordType
+    public let isArchivedByLegacyTimestampForSorting: Bool
+    public let lastMessageDate: Date?
+    public let messageDraft: String?
+    public let mutedUntilDate: Date?
+    public let shouldThreadBeVisible: Bool
+    public let groupModel: Data?
+    public let hasDismissedOffers: Bool?
 
     public enum CodingKeys: String, CodingKey, ColumnExpression, CaseIterable {
-        case id, uniqueId, shouldBeVisible, creationDate, threadType
+        case id
+        case recordType
+        case uniqueId
+        case archivalDate
+        case archivedAsOfMessageSortId
+        case conversationColorName
+        case creationDate
+        case isArchivedByLegacyTimestampForSorting
+        case lastMessageDate
+        case messageDraft
+        case mutedUntilDate
+        case shouldThreadBeVisible
+        case groupModel
+        case hasDismissedOffers
     }
 
     public static func columnName(_ column: ThreadRecord.CodingKeys) -> String {
         return column.rawValue
     }
 }
+
+extension SDSRecordType: Codable { }
