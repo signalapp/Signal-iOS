f6dd9812606e76d43d4356c7ea3ba08e64a6ed5c
Matthew Chen <matthew@signal.org>
2019-04-16 12:12:27 -0400

    Respond to CR.

--- Scripts/sds_codegen/sds_codegen.sh
+++ Scripts/sds_codegen/sds_codegen.sh
@@ -25,9 +25,7 @@ REPO_ROOT=`git rev-parse --show-toplevel`
 # and generates fake Obj-c headers (.h) that @interface and @protocol
 # stubs for each swift class.  This is analogous to a very simplified 
 # version of the "-Swift.h" files used by Swift for bridging.
-# $REPO_ROOT/Scripts/sds_codegen/sds_parse_swift_bridging.py --src-path  . --swift-bridging-path $REPO_ROOT/Scripts/sds_codegen/sds-includes --intermediates
 $REPO_ROOT/Scripts/sds_codegen/sds_parse_swift_bridging.py --src-path  . --swift-bridging-path $REPO_ROOT/Scripts/sds_codegen/sds-includes
-
 
 
 # We parse Obj-C source files (.m only, not .mm yet) to extract simple class descriptions (class name, base class, property names and types, etc.)
@@ -44,8 +42,6 @@ $REPO_ROOT/Scripts/sds_codegen/sds_parse_objc.py --src-path SignalServiceKit/src
 $REPO_ROOT/Scripts/sds_codegen/sds_parse_objc.py --src-path Signal/src/ViewControllers/ConversationView/TypingIndicatorInteraction.swift --swift-bridging-path $REPO_ROOT/Scripts/sds_codegen/sds-includes
 $REPO_ROOT/Scripts/sds_codegen/sds_parse_objc.py --src-path SignalServiceKit/src/Messages --swift-bridging-path $REPO_ROOT/Scripts/sds_codegen/sds-includes
 
-# $REPO_ROOT/Scripts/sds_codegen/sds_parse_objc.py --src-path SignalServiceKit/src/Contacts/TSThread.m --swift-bridging-path $REPO_ROOT/Scripts/sds_codegen/sds-includes --intermediates
-
 
 # We parse Swift source files to extract simple class descriptions (class name, base class, property names and types, etc.)
 #
@@ -53,8 +49,5 @@ $REPO_ROOT/Scripts/sds_codegen/sds_parse_objc.py --src-path SignalServiceKit/src
 #
 # $REPO_ROOT/Scripts/sds_codegen/sds_parse_swift.py --src-path SignalDataStoreCommon/
 
-
-
 # We generate Swift extensions to handle serialization, etc. for models.
 $REPO_ROOT/Scripts/sds_codegen/sds_generate.py  --src-path SignalServiceKit/  --search-path . --record-type-swift-path SignalServiceKit/src/Storage/Database/SDSRecordType.swift  --record-type-json-path $REPO_ROOT/Scripts/sds_codegen/sds_config/sds_record_type_map.json --config-json-path $REPO_ROOT/Scripts/sds_codegen/sds-config/sds-config.json
-# $REPO_ROOT/Scripts/sds_codegen/sds_generate.py  --src-path SignalServiceKit/src/Messages/Interactions/TSInteraction.m.sdsjson  --search-path . --record-type-swift-path SignalServiceKit/src/Storage/Database/SDSRecordType.swift  --record-type-json-path $REPO_ROOT/Scripts/sds_codegen/sds_config/sds_record_type_map.json --config-json-path $REPO_ROOT/Scripts/sds_codegen/sds-config/sds-config.json
