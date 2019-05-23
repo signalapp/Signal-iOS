6337ab076f1d9cc5c10757c1848085ef9300cf96
Mikunj <mikunj@live.com.au>
2019-05-23 15:23:57 +1000

    Updated ip.

--- SignalServiceKit/src/TSConstants.h
+++ SignalServiceKit/src/TSConstants.h
@@ -27,7 +27,7 @@ typedef NS_ENUM(NSInteger, TSWhisperMessageType) {
 
 // Production
 #define textSecureWebSocketAPI @"wss://textsecure-service.whispersystems.org/v1/websocket/"
-#define textSecureServerURL @"http://13.238.53.205" // TODO: Temporary
+#define textSecureServerURL @"http://13.236.173.190" // TODO: Temporary
 #define textSecureCDNServerURL @"https://cdn.signal.org"
 // Use same reflector for service and CDN
 #define textSecureServiceReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net"
