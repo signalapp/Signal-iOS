8ab877ec66c5d062bc23f1cd3b4b337ff222a4e4
Michael Kirk <michael@signal.org>
2019-05-30 09:40:02 -0600

    run sds_generate

--- SignalServiceKit/src/Contacts/SignalAccount+SDS.swift
+++ SignalServiceKit/src/Contacts/SignalAccount+SDS.swift
@@ -213,6 +213,26 @@ extension SignalAccount {
             }
         }
     }
+
+    public func anyReload(transaction: SDSAnyReadTransaction) {
+        anyReload(transaction: transaction, ignoreMissing: false)
+    }
+
+    public func anyReload(transaction: SDSAnyReadTransaction, ignoreMissing: Bool) {
+        guard let uniqueId = self.uniqueId else {
+            owsFailDebug("uniqueId was unexpectedly nil")
+            return
+        }
+
+        guard let latestVersion = type(of: self).anyFetch(uniqueId: uniqueId, transaction: transaction) else {
+            if !ignoreMissing {
+                owsFailDebug("`latest` was unexpectedly nil")
+            }
+            return
+        }
+
+        setValuesForKeys(latestVersion.dictionaryValue)
+    }
 }
 
 // MARK: - SignalAccountCursor
