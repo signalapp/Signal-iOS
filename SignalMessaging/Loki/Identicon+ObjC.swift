09aea1b2f004467c51b3c45578457a15cf7b5f7f
Niels Andriesse <andriesseniels@gmail.com>
2019-06-18 15:03:42 +1000

    Make avatar background white

--- SignalMessaging/Loki/Identicon+ObjC.swift
+++ SignalMessaging/Loki/Identicon+ObjC.swift
@@ -4,6 +4,16 @@ import IGIdenticon
 final class Identicon : NSObject {
     
     @objc static func generateIcon(string: String, size: CGSize) -> UIImage {
-        return IGIdenticon.Identicon().icon(from: string, size: size)!
+        let identicon = IGIdenticon.Identicon().icon(from: string, size: size)!
+        let rect = CGRect(origin: CGPoint.zero, size: identicon.size)
+        UIGraphicsBeginImageContextWithOptions(identicon.size, false, UIScreen.main.scale)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(UIColor.white.cgColor)
+        context.fill(rect)
+        context.draw(identicon.cgImage!, in: rect)
+        context.drawPath(using: CGPathDrawingMode.fill)
+        let result = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return result
     }
 }
