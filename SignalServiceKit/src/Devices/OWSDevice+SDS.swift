aadb418cd9286d67b0d15eebdfbd96fbed01153a
Matthew Chen <matthew@signal.org>
2019-06-25 11:47:25 -0400

    Convert message sending to use any transactions (code generated changes).

--- SignalServiceKit/src/Devices/OWSDevice+SDS.swift
+++ SignalServiceKit/src/Devices/OWSDevice+SDS.swift
@@ -370,6 +370,27 @@ public extension OWSDevice {
             return DeviceRecord.ows_fetchCount(grdbTransaction.database)
         }
     }
+
+    // WARNING: Do not use this method for any models which do cleanup
+    //          in their anyWillRemove(), anyDidRemove() methods.
+    class func anyRemoveAllWithoutInstantation(transaction: SDSAnyWriteTransaction) {
+        switch transaction.writeTransaction {
+        case .yapWrite(let ydbTransaction):
+            ydbTransaction.removeAllObjects(inCollection: OWSDevice.collection())
+        case .grdbWrite(let grdbTransaction):
+            do {
+                try DeviceRecord.deleteAll(grdbTransaction.database)
+            } catch {
+                owsFailDebug("deleteAll() failed: \(error)")
+            }
+        }
+    }
+
+    class func anyRemoveAllWithInstantation(transaction: SDSAnyWriteTransaction) {
+        anyEnumerate(transaction: transaction) { (instance, _) in
+            instance.anyRemove(transaction: transaction)
+        }
+    }
 }
 
 // MARK: - Swift Fetch
--- SignalServiceKit/src/Messages/Stickers/KnownStickerPack+SDS.swift
+++ SignalServiceKit/src/Messages/Stickers/KnownStickerPack+SDS.swift
@@ -365,6 +365,27 @@ public extension KnownStickerPack {
             return KnownStickerPackRecord.ows_fetchCount(grdbTransaction.database)
         }
     }
+
+    // WARNING: Do not use this method for any models which do cleanup
+    //          in their anyWillRemove(), anyDidRemove() methods.
+    class func anyRemoveAllWithoutInstantation(transaction: SDSAnyWriteTransaction) {
+        switch transaction.writeTransaction {
+        case .yapWrite(let ydbTransaction):
+            ydbTransaction.removeAllObjects(inCollection: KnownStickerPack.collection())
+        case .grdbWrite(let grdbTransaction):
+            do {
+                try KnownStickerPackRecord.deleteAll(grdbTransaction.database)
+            } catch {
+                owsFailDebug("deleteAll() failed: \(error)")
+            }
+        }
+    }
+
+    class func anyRemoveAllWithInstantation(transaction: SDSAnyWriteTransaction) {
+        anyEnumerate(transaction: transaction) { (instance, _) in
+            instance.anyRemove(transaction: transaction)
+        }
+    }
 }
 
 // MARK: - Swift Fetch
