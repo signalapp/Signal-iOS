85203bac08d69c979033d9ebfef1c5be5b8fedc2
Niels Andriesse <andriesseniels@gmail.com>
2019-06-25 15:29:38 +1000

    Prepare for alpha release

--- SignalServiceKit/src/TSConstants.h
+++ SignalServiceKit/src/TSConstants.h
@@ -66,7 +66,7 @@ BOOL IsUsingProductionService(void);
 #define textSecureProfileAvatarFormAPI @"v1/profile/form/avatar"
 #define textSecure2FAAPI @"/v1/accounts/pin"
 
-#define SignalApplicationGroup @"group.com.niels-andriesse.loki-network"
+#define SignalApplicationGroup @"group.com.loki-project.loki-messenger"
 
 #endif
 
