9f046c5a82d3a7828bbe1c5a28205c6fdb513232
Michael Kirk <michael@signal.org>
2019-07-12 10:10:13 -0600

    adapt to lossless certs

--- SignalServiceKit/tests/Messages/OWSUDManagerTest.swift
+++ SignalServiceKit/tests/Messages/OWSUDManagerTest.swift
@@ -4,6 +4,7 @@
 
 import XCTest
 import Foundation
+import Curve25519Kit
 import SignalCoreKit
 import SignalMetadataKit
 @testable import SignalServiceKit
@@ -22,7 +23,7 @@ class MockCertificateValidator: NSObject, SMKCertificateValidator {
 
 class OWSUDManagerTest: SSKBaseTestSwift {
 
-    // MARK: - Singletons
+    // MARK: - Dependencies
 
     private var tsAccountManager: TSAccountManager {
         return TSAccountManager.sharedInstance()
@@ -36,7 +37,8 @@ class OWSUDManagerTest: SSKBaseTestSwift {
         return SSKEnvironment.shared.profileManager
     }
 
-    // MARK: registration
+    // MARK: - Setup/Teardown
+
     let aliceRecipientId = "+13213214321"
 
     override func setUp() {
@@ -49,23 +51,16 @@ class OWSUDManagerTest: SSKBaseTestSwift {
 
         udManager.certificateValidator = MockCertificateValidator()
 
-        let serverCertificate = SMKServerCertificate(keyId: 1,
-                                                     key: try! ECPublicKey(keyData: Randomness.generateRandomBytes(ECCKeyLength)),
-                                                     signatureData: Randomness.generateRandomBytes(ECCSignatureLength))
-        let senderCertificate = SMKSenderCertificate(signer: serverCertificate,
-                                                     key: try! ECPublicKey(keyData: Randomness.generateRandomBytes(ECCKeyLength)),
-                                                     senderDeviceId: 1,
-                                                     senderRecipientId: aliceRecipientId,
-                                                     expirationTimestamp: NSDate.ows_millisecondTimeStamp() + kWeekInMs,
-                                                     signatureData: Randomness.generateRandomBytes(ECCSignatureLength))
-
-        udManager.setSenderCertificate(try! senderCertificate.serialized())
+        let senderCertificate = try! SMKSenderCertificate(serializedData: buildSenderCertificateProto().serializedData())
+        udManager.setSenderCertificate(senderCertificate.serializedData)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
+    // MARK: - Tests
 
     func testMode_self() {
 
@@ -176,5 +171,39 @@ class OWSUDManagerTest: SSKBaseTestSwift {
         XCTAssertEqual(.unrestricted, udManager.unidentifiedAccessMode(forRecipientId: bobRecipientId))
         udAccess = udManager.udAccess(forRecipientId: bobRecipientId, requireSyncAccess: false)!
         XCTAssert(udAccess.isRandomKey)
+    }
+
+    // MARK: - Util
+
+    func buildServerCertificateProto() -> SMKProtoServerCertificate {
+        let serverKey = try! Curve25519.generateKeyPair().ecPublicKey().serialized
+        let certificateData = try! SMKProtoServerCertificateCertificate.builder(id: 1,
+                                                                                key: serverKey ).buildSerializedData()
+
+        let signatureData = Randomness.generateRandomBytes(ECCSignatureLength)!
+
+        let wrapperProto = SMKProtoServerCertificate.builder(certificate: certificateData,
+                                                             signature: signatureData)
+
+        return try! wrapperProto.build()
+    }
+
+    func buildSenderCertificateProto() -> SMKProtoSenderCertificate {
+        let expires = NSDate.ows_millisecondTimeStamp() + kWeekInMs
+        let identityKey = try! Curve25519.generateKeyPair().ecPublicKey().serialized
+        let signer = buildServerCertificateProto()
+        let certificateData = try! SMKProtoSenderCertificateCertificate.builder(sender: aliceRecipientId,
+                                                                                senderDevice: 1,
+                                                                                expires: expires,
+                                                                                identityKey: identityKey,
+                                                                                signer: signer)
+            .buildSerializedData()
+
+        let signatureData = Randomness.generateRandomBytes(ECCSignatureLength)!
+
+        let wrapperProto = try! SMKProtoSenderCertificate.builder(certificate: certificateData,
+                                                                  signature: signatureData).build()
+
+        return wrapperProto
     }
 }
