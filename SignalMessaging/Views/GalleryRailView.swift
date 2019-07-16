c36d691d1135bb2208a3d5802358af5eae76dcf9
Michael Kirk <michael@signal.org>
2019-07-16 11:21:18 -0600

    Fix: selecting last message from tileView doesn't focus
    The mostRecentAttachment is a separate and redundant instance with whats in the
    media gallery item cache.
    
    Alternatively, we could double down on the one-instance-per-value paradigm by
    creating a way to manually add existing instances to the cache, but the value
    semantics proposed here seemed more natural.
    
    I wish we could have just made RailViewItem: Equatable, but alas "protocol can
    only be used as a generic constraint because it has Self or associated type
    requirements"

--- SignalMessaging/Views/GalleryRailView.swift
+++ SignalMessaging/Views/GalleryRailView.swift
@@ -8,8 +8,18 @@ public protocol GalleryRailItemProvider: class {
     var railItems: [GalleryRailItem] { get }
 }
 
-public protocol GalleryRailItem: class {
+public protocol GalleryRailItem {
     func buildRailItemView() -> UIView
+    func isEqualToGalleryRailItem(_ other: GalleryRailItem?) -> Bool
+}
+
+public extension GalleryRailItem where Self: Equatable {
+    func isEqualToGalleryRailItem(_ other: GalleryRailItem?) -> Bool {
+        guard let other = other as? Self else {
+            return false
+        }
+        return self == other
+    }
 }
 
 protocol GalleryRailCellViewDelegate: class {
@@ -157,7 +167,7 @@ public class GalleryRailView: UIView, GalleryRailCellViewDelegate {
                 return false
             }
             for (index, element) in lhs.enumerated() {
-                guard element === rhs[index] else {
+                guard element.isEqualToGalleryRailItem(rhs[index]) else {
                     return false
                 }
             }
@@ -246,7 +256,7 @@ public class GalleryRailView: UIView, GalleryRailCellViewDelegate {
     func updateFocusedItem(_ focusedItem: GalleryRailItem?) {
         var selectedCellView: GalleryRailCellView?
         cellViews.forEach { cellView in
-            if cellView.item === focusedItem {
+            if let item = cellView.item, item.isEqualToGalleryRailItem(focusedItem) {
                 assert(selectedCellView == nil)
                 selectedCellView = cellView
                 cellView.setIsSelected(true)
