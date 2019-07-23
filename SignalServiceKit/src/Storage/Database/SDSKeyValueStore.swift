f7da4d7aa7108156fe7c0b68b4869982460b475d
Matthew Chen <matthew@signal.org>
2019-07-23 12:33:16 -0300

    Remove YDB connections.

--- SignalServiceKit/src/Storage/Database/SDSKeyValueStore.swift
+++ SignalServiceKit/src/Storage/Database/SDSKeyValueStore.swift
@@ -283,9 +283,6 @@ public class SDSKeyValueStore: NSObject {
             }
         case .grdbRead(let grdbRead):
             var stop: ObjCBool = false
-            // PERF - we could enumerate with a single query rather than
-            // fetching keys then fetching objects one by one. In practice
-            // the collections that use this are pretty small.
             for key in allKeys(grdbTransaction: grdbRead) {
                 guard !stop.boolValue else {
                     return
