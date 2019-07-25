8c17fa4f29c1443270c65c8ab307b62df268599e
Matthew Chen <matthew@signal.org>
2019-07-25 14:03:56 -0300

    Add tests around YDB-to-GRDB migration.

--- Signal/test/util/YDBToGRDBMigrationTest.swift
+++ Signal/test/util/YDBToGRDBMigrationTest.swift
@@ -310,7 +310,7 @@ class YDBToGRDBMigrationTest: SignalBaseTest {
             XCTAssertEqual(value2, store3.getData("key0", transaction: transaction))
             XCTAssertEqual(value3, store1.getData("key1", transaction: transaction))
             XCTAssertEqual(value4, store2.getData("key1", transaction: transaction))
-            XCTAssertNil(store3.getInt("key1", transaction: transaction))
+            XCTAssertNil(store3.getData("key1", transaction: transaction))
         }
     }
 
@@ -378,7 +378,7 @@ class YDBToGRDBMigrationTest: SignalBaseTest {
             XCTAssertEqual(value2, store3.getString("key0", transaction: transaction))
             XCTAssertEqual(value3, store1.getString("key1", transaction: transaction))
             XCTAssertEqual(value4, store2.getString("key1", transaction: transaction))
-            XCTAssertNil(store3.getInt("key1", transaction: transaction))
+            XCTAssertNil(store3.getString("key1", transaction: transaction))
         }
     }
 
@@ -457,7 +457,7 @@ class YDBToGRDBMigrationTest: SignalBaseTest {
             XCTAssertEqual(value2, store3.getDate("key0", transaction: transaction))
             XCTAssertEqual(value3, store1.getDate("key1", transaction: transaction))
             XCTAssertEqual(value4, store2.getDate("key1", transaction: transaction))
-            XCTAssertNil(store3.getInt("key1", transaction: transaction))
+            XCTAssertNil(store3.getDate("key1", transaction: transaction))
 
             // Date edge cases.
             XCTAssertEqual(2, store4.numberOfKeys(transaction: transaction))
@@ -466,6 +466,77 @@ class YDBToGRDBMigrationTest: SignalBaseTest {
             XCTAssertFalse(store4.hasValue(forKey: "key2", transaction: transaction))
             XCTAssertEqual(value0, store4.getDate("key0", transaction: transaction))
             XCTAssertEqual(value1, store4.getDate("key1", transaction: transaction))
+        }
+    }
+
+    func testKeyValueObject() {
+        let store1 = SDSKeyValueStore(collection: "store1")
+        let store2 = SDSKeyValueStore(collection: "store2")
+        let store3 = SDSKeyValueStore(collection: "store3")
+
+        let randomValue = {
+            return UUID().uuidString
+        }
+        let value0: String = randomValue()
+        let value1: [String] = [randomValue()]
+        let value2: [String: String] =  [ randomValue(): randomValue() ]
+        let value3: [String] = [randomValue(), randomValue()]
+        let value4: [String: String] = [
+            randomValue(): randomValue(),
+            randomValue(): randomValue()
+        ]
+
+        self.read { transaction in
+            XCTAssertEqual(0, store1.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(0, store2.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(0, store3.numberOfKeys(transaction: transaction))
+        }
+
+        self.yapWrite { transaction in
+            store1.setObject(value0, key: "key0", transaction: transaction.asAnyWrite)
+            store2.setObject(value1, key: "key0", transaction: transaction.asAnyWrite)
+            store3.setObject(value2, key: "key0", transaction: transaction.asAnyWrite)
+            store1.setObject(value3, key: "key1", transaction: transaction.asAnyWrite)
+            store2.setObject(value4, key: "key1", transaction: transaction.asAnyWrite)
+        }
+
+        self.read { transaction in
+            XCTAssertEqual(0, store1.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(0, store2.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(0, store3.numberOfKeys(transaction: transaction))
+        }
+
+        let migratorGroups = [
+            GRDBMigratorGroup { ydbTransaction in
+                return [
+                    GRDBKeyValueStoreMigrator<Any>(label: "store1", keyStore: store1, ydbTransaction: ydbTransaction, memorySamplerRatio: 1.0),
+                    GRDBKeyValueStoreMigrator<Any>(label: "store2", keyStore: store2, ydbTransaction: ydbTransaction, memorySamplerRatio: 1.0),
+                    GRDBKeyValueStoreMigrator<Any>(label: "store3", keyStore: store3, ydbTransaction: ydbTransaction, memorySamplerRatio: 1.0)
+                ]
+            }
+        ]
+
+        try! YDBToGRDBMigration().migrate(migratorGroups: migratorGroups)
+
+        self.read { transaction in
+            XCTAssertEqual(2, store1.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(2, store2.numberOfKeys(transaction: transaction))
+            XCTAssertEqual(1, store3.numberOfKeys(transaction: transaction))
+
+            XCTAssertTrue(store1.hasValue(forKey: "key0", transaction: transaction))
+            XCTAssertTrue(store2.hasValue(forKey: "key0", transaction: transaction))
+            XCTAssertTrue(store3.hasValue(forKey: "key0", transaction: transaction))
+
+            XCTAssertTrue(store1.hasValue(forKey: "key1", transaction: transaction))
+            XCTAssertTrue(store2.hasValue(forKey: "key1", transaction: transaction))
+            XCTAssertFalse(store3.hasValue(forKey: "key1", transaction: transaction))
+
+            XCTAssertEqual(value0, store1.getObject("key0", transaction: transaction) as! String)
+            XCTAssertEqual(value1, store2.getObject("key0", transaction: transaction) as! [String])
+            XCTAssertEqual(value2, store3.getObject("key0", transaction: transaction) as! [String: String])
+            XCTAssertEqual(value3, store1.getObject("key1", transaction: transaction) as! [String])
+            XCTAssertEqual(value4, store2.getObject("key1", transaction: transaction) as! [String: String])
+            XCTAssertNil(store3.getObject("key1", transaction: transaction))
         }
     }
 }
