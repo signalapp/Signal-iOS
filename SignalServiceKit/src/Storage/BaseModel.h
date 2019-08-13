5f92ccc5e2c40e448a4bd9f376026e656a475d08
Matthew Chen <matthew@signal.org>
2019-08-13 12:08:40 -0300

    Remove YDB & primary storage usage.

--- SignalServiceKit/src/Storage/BaseModel.h
+++ SignalServiceKit/src/Storage/BaseModel.h
@@ -17,25 +17,16 @@ NS_ASSUME_NONNULL_BEGIN
 // This will break Swift usage and generate warnings for Obj-C usage.
 // Note: this functionality can still be accessed (for example by the
 // SDS model extensions) via the ydb_ methods of TSYapDatabaseObject.
-+ (NSUInteger)numberOfKeysInCollection NS_UNAVAILABLE;
 + (NSUInteger)numberOfKeysInCollectionWithTransaction:(YapDatabaseReadTransaction *)transaction NS_UNAVAILABLE;
-+ (void)removeAllObjectsInCollection NS_UNAVAILABLE;
-+ (NSArray *)allObjectsInCollection NS_UNAVAILABLE;
-+ (void)enumerateCollectionObjectsUsingBlock:(void (^)(id obj, BOOL *stop))block NS_UNAVAILABLE;
 + (void)enumerateCollectionObjectsWithTransaction:(YapDatabaseReadTransaction *)transaction
                                        usingBlock:(void (^)(id object, BOOL *stop))block NS_UNAVAILABLE;
 + (nullable instancetype)fetchObjectWithUniqueID:(NSString *)uniqueID
                                      transaction:(YapDatabaseReadTransaction *)transaction NS_UNAVAILABLE;
-+ (nullable instancetype)fetchObjectWithUniqueID:(NSString *)uniqueID NS_UNAVAILABLE;
-- (void)save NS_UNAVAILABLE;
-- (void)reload NS_UNAVAILABLE;
 - (void)reloadWithTransaction:(YapDatabaseReadTransaction *)transaction NS_UNAVAILABLE;
 - (void)reloadWithTransaction:(YapDatabaseReadTransaction *)transaction
                 ignoreMissing:(BOOL)ignoreMissing NS_UNAVAILABLE;
-- (void)saveAsyncWithCompletionBlock:(void (^_Nullable)(void))completionBlock NS_UNAVAILABLE;
 - (void)saveWithTransaction:(YapDatabaseReadWriteTransaction *)transaction NS_UNAVAILABLE;
 - (void)removeWithTransaction:(YapDatabaseReadWriteTransaction *)transaction NS_UNAVAILABLE;
-- (void)remove NS_UNAVAILABLE;
 
 @end
 
