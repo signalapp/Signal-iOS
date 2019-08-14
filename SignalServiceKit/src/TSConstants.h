738b7f11d59b896356e1ca7b7f8e22b5dd17d16f
Matthew Chen <matthew@signal.org>
2019-08-14 17:25:11 -0300

    Ad hoc build for ken.

--- SignalServiceKit/src/TSConstants.h
+++ SignalServiceKit/src/TSConstants.h
@@ -25,56 +25,56 @@ typedef NS_ENUM(NSInteger, TSWhisperMessageType) {
 //#ifndef DEBUG
 
 // Production
-#define textSecureWebSocketAPI @"wss://textsecure-service.whispersystems.org/v1/websocket/"
-#define textSecureServerURL @"https://textsecure-service.whispersystems.org/"
-#define textSecureCDNServerURL @"https://cdn.signal.org"
-// Use same reflector for service and CDN
-#define textSecureServiceReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net"
-#define textSecureCDNReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net"
-#define contactDiscoveryURL @"https://api.directory.signal.org"
-#define keyBackupURL @"https://api.backup.signal.org"
-#define storageServiceURL @"https://storage.signal.org"
-#define kUDTrustRoot @"BXu6QIKVz5MA8gstzfOgRQGqyLqOwNKHL6INkv3IHWMF"
-
-#define serviceCensorshipPrefix @"service"
-#define cdnCensorshipPrefix @"cdn"
-#define contactDiscoveryCensorshipPrefix @"directory"
-#define keyBackupCensorshipPrefix @"backup"
-
-#define contactDiscoveryEnclaveName @"cd6cfc342937b23b1bdd3bbf9721aa5615ac9ff50a75c5527d441cd3276826c9"
-#define contactDiscoveryMrEnclave contactDiscoveryEnclaveName
-
-#define keyBackupEnclaveName @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
-#define keyBackupMrEnclave @"94029382f0a8947a72df682e6972f58bbb6dda2f5ec51ab0974bd40c781b719b"
-#define keyBackupServiceId @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
-
-#define USING_PRODUCTION_SERVICE
-
-//#else
-
-//// Staging
-//#define textSecureWebSocketAPI @"wss://textsecure-service-staging.whispersystems.org/v1/websocket/"
-//#define textSecureServerURL @"https://textsecure-service-staging.whispersystems.org/"
-//#define textSecureCDNServerURL @"https://cdn-staging.signal.org"
-//#define textSecureServiceReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net";
-//#define textSecureCDNReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net";
-//#define contactDiscoveryURL @"https://api-staging.directory.signal.org"
-//#define keyBackupURL @"https://api-staging.backup.signal.org"
-//#define storageServiceURL @"https://storage.signal.org" // For now, staging is using the production URL
-//#define kUDTrustRoot @"BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx"
+//#define textSecureWebSocketAPI @"wss://textsecure-service.whispersystems.org/v1/websocket/"
+//#define textSecureServerURL @"https://textsecure-service.whispersystems.org/"
+//#define textSecureCDNServerURL @"https://cdn.signal.org"
+//// Use same reflector for service and CDN
+//#define textSecureServiceReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net"
+//#define textSecureCDNReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net"
+//#define contactDiscoveryURL @"https://api.directory.signal.org"
+//#define keyBackupURL @"https://api.backup.signal.org"
+//#define storageServiceURL @"https://storage.signal.org"
+//#define kUDTrustRoot @"BXu6QIKVz5MA8gstzfOgRQGqyLqOwNKHL6INkv3IHWMF"
 //
-//#define serviceCensorshipPrefix @"service-staging"
-//#define cdnCensorshipPrefix @"cdn-staging"
-//#define contactDiscoveryCensorshipPrefix @"directory-staging"
-//#define keyBackupCensorshipPrefix @"backup-staging"
+//#define serviceCensorshipPrefix @"service"
+//#define cdnCensorshipPrefix @"cdn"
+//#define contactDiscoveryCensorshipPrefix @"directory"
+//#define keyBackupCensorshipPrefix @"backup"
 //
-//// CDS uses the same EnclaveName and MrEnclave
-//#define contactDiscoveryEnclaveName @"e0f7dee77dc9d705ccc1376859811da12ecec3b6119a19dc39bdfbf97173aa18"
+//#define contactDiscoveryEnclaveName @"cd6cfc342937b23b1bdd3bbf9721aa5615ac9ff50a75c5527d441cd3276826c9"
 //#define contactDiscoveryMrEnclave contactDiscoveryEnclaveName
 //
 //#define keyBackupEnclaveName @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
 //#define keyBackupMrEnclave @"94029382f0a8947a72df682e6972f58bbb6dda2f5ec51ab0974bd40c781b719b"
 //#define keyBackupServiceId @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
+//
+//#define USING_PRODUCTION_SERVICE
+
+//#else
+
+//// Staging
+#define textSecureWebSocketAPI @"wss://textsecure-service-staging.whispersystems.org/v1/websocket/"
+#define textSecureServerURL @"https://textsecure-service-staging.whispersystems.org/"
+#define textSecureCDNServerURL @"https://cdn-staging.signal.org"
+#define textSecureServiceReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net";
+#define textSecureCDNReflectorHost @"europe-west1-signal-cdn-reflector.cloudfunctions.net";
+#define contactDiscoveryURL @"https://api-staging.directory.signal.org"
+#define keyBackupURL @"https://api-staging.backup.signal.org"
+#define storageServiceURL @"https://storage.signal.org" // For now, staging is using the production URL
+#define kUDTrustRoot @"BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx"
+
+#define serviceCensorshipPrefix @"service-staging"
+#define cdnCensorshipPrefix @"cdn-staging"
+#define contactDiscoveryCensorshipPrefix @"directory-staging"
+#define keyBackupCensorshipPrefix @"backup-staging"
+
+// CDS uses the same EnclaveName and MrEnclave
+#define contactDiscoveryEnclaveName @"e0f7dee77dc9d705ccc1376859811da12ecec3b6119a19dc39bdfbf97173aa18"
+#define contactDiscoveryMrEnclave contactDiscoveryEnclaveName
+
+#define keyBackupEnclaveName @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
+#define keyBackupMrEnclave @"94029382f0a8947a72df682e6972f58bbb6dda2f5ec51ab0974bd40c781b719b"
+#define keyBackupServiceId @"281b2220946102e8447b1d72a02b52d413c390780bae3e3a5aad27398999e7a3"
 
 //#endif
 
