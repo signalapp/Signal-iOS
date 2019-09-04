d7289d0e2a3adf1612428f0dffcfad739d86c866
Matthew Chen <matthew@signal.org>
2019-09-04 14:04:03 -0300

    Set feature flags for production build.

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -18,7 +18,7 @@ extension FeatureBuild {
     }
 }
 
-let build: FeatureBuild = OWSIsDebugBuild() ? .dev : .beta
+let build: FeatureBuild = .production
 
 /// By centralizing feature flags here and documenting their rollout plan, it's easier to review
 /// which feature flags are in play.
