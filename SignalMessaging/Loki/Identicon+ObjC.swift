2bda269a45444a56fdbb8e2f213c117721bc5e7a
Mikunj Varsani <mikunj@live.com.au>
2019-09-04 15:18:55 +1000

    Removed useless code.

--- SignalMessaging/Loki/Identicon+ObjC.swift
+++ SignalMessaging/Loki/Identicon+ObjC.swift
@@ -1,21 +1,5 @@
-import IGIdenticon
-
 @objc(LKIdenticon)
 final class Identicon : NSObject {
-    
-//    @objc static func generateIcon(string: String, size: CGSize) -> UIImage {
-//        let identicon = IGIdenticon.Identicon().icon(from: string, size: size)!
-//        let rect = CGRect(origin: CGPoint.zero, size: identicon.size)
-//        UIGraphicsBeginImageContextWithOptions(identicon.size, false, UIScreen.main.scale)
-//        let context = UIGraphicsGetCurrentContext()!
-//        context.setFillColor(UIColor.white.cgColor)
-//        context.fill(rect)
-//        context.draw(identicon.cgImage!, in: rect)
-//        context.drawPath(using: CGPathDrawingMode.fill)
-//        let result = UIGraphicsGetImageFromCurrentImageContext()!
-//        UIGraphicsEndImageContext()
-//        return result
-//    }
     
     @objc static func generateIcon(string: String, size: CGFloat) -> UIImage {
         let icon = JazzIcon(seed: string)
