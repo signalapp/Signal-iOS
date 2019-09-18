d91118bd041ceb8bb127a7e8d23a26dd8f46449c
Matthew Chen <matthew@signal.org>
2019-09-18 09:26:16 -0300

    Update to reflect rebase.

--- SignalServiceKit/src/Util/TestModel+SDS.swift
+++ SignalServiceKit/src/Util/TestModel+SDS.swift
@@ -170,7 +170,7 @@ extension TestModelSerializer {
     // where this model (and any subclasses) are persisted.
     static let idColumn = SDSColumnMetadata(columnName: "id", columnType: .primaryKey, columnIndex: 0)
     static let recordTypeColumn = SDSColumnMetadata(columnName: "recordType", columnType: .int64, columnIndex: 1)
-    static let uniqueIdColumn = SDSColumnMetadata(columnName: "uniqueId", columnType: .unicodeString, columnIndex: 2)
+    static let uniqueIdColumn = SDSColumnMetadata(columnName: "uniqueId", columnType: .unicodeString, isUnique: true, columnIndex: 2)
     // Base class properties
     static let dateValueColumn = SDSColumnMetadata(columnName: "dateValue", columnType: .double, isOptional: true, columnIndex: 3)
     static let doubleValueColumn = SDSColumnMetadata(columnName: "doubleValue", columnType: .double, columnIndex: 4)
