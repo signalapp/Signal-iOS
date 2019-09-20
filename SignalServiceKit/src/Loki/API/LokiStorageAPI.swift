7fe7245c643d76fc9f5afb0fd1836bbe1c65482b
Niels Andriesse <andriesseniels@gmail.com>
2019-09-20 11:42:10 +1000

    Refactor

--- SignalServiceKit/src/Loki/API/LokiStorageAPI.swift
+++ SignalServiceKit/src/Loki/API/LokiStorageAPI.swift
@@ -7,18 +7,19 @@ public final class LokiStorageAPI : NSObject {
     override private init() { }
     
     // MARK: Public API
-    public static func addSlaveAccount(with hexEncodedPublicKey: String) -> Promise<Void> {
-        // Adds the given slave account to the user's device mapping on the server
+    public static func addDeviceLink(_ deviceLink: LokiDeviceLink) -> Promise<Void> {
+        // Adds the given device link to the user's device mapping on the server
         notImplemented()
     }
     
-    public static func removeSlaveAccount(with hexEncodedPublicKey: String) -> Promise<Void> {
-        // Removes the given slave account from the user's device mapping on the server
+    public static func removeDeviceLink(_ deviceLink: LokiDeviceLink) -> Promise<Void> {
+        // Removes the given device link from the user's device mapping on the server
         notImplemented()
     }
     
-    public static func getAssociatedAccounts(for hexEncodedPublicKey: String) -> Promise<[String]> {
-        // Gets the accounts associated with the given hex encoded public key from the server
+    public static func getDeviceLinks(associatedWith hexEncodedPublicKey: String) -> Promise<Set<LokiDeviceLink>> {
+        // Gets the device links associated with the given hex encoded public key from the
+        // server and stores and returns the valid ones
         notImplemented()
     }
 }
--- SignalServiceKit/src/Loki/API/Multi Device/LokiDeviceLink.swift
+++ SignalServiceKit/src/Loki/API/Multi Device/LokiDeviceLink.swift
@@ -1,12 +1,12 @@
 
-public struct LokiDeviceLink {
+public struct LokiDeviceLink : Hashable {
     public let master: Device
     public let slave: Device
     
     public var isAuthorized: Bool { return master.signature != nil }
     
     // MARK: Types
-    public struct Device {
+    public struct Device : Hashable {
         public let hexEncodedPublicKey: String
         public let signature: Data?
         
@@ -14,11 +14,22 @@ public struct LokiDeviceLink {
             self.hexEncodedPublicKey = hexEncodedPublicKey
             self.signature = signature
         }
+        
+        public func hash(into hasher: inout Hasher) {
+            hexEncodedPublicKey.hash(into: &hasher)
+            signature?.hash(into: &hasher)
+        }
     }
     
     // MARK: Lifecycle
     public init(between master: Device, and slave: Device) {
         self.master = master
         self.slave = slave
+    }
+    
+    // MARK: Hashing
+    public func hash(into hasher: inout Hasher) {
+        master.hash(into: &hasher)
+        slave.hash(into: &hasher)
     }
 }
