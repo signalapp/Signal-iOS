9fcb0f610f83fadf1be5db908defed6b0e09ed81
Niels Andriesse <andriesseniels@gmail.com>
2019-10-02 16:10:22 +1000

    Fix crash

--- Signal/src/ViewControllers/ConversationView/ConversationViewController.m
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController.m
@@ -2150,7 +2150,7 @@ typedef enum : NSUInteger {
 
     NSValue *_Nullable contentOffset = [self contentOffsetForMenuActionInteraction];
     if (contentOffset == nil) {
-        OWSFailDebug(@"Missing contentOffset.");
+        // OWSFailDebug(@"Missing contentOffset.");
         return;
     }
     [self.collectionView setContentOffset:contentOffset.CGPointValue animated:animated];
@@ -2180,7 +2180,7 @@ typedef enum : NSUInteger {
     UICollectionViewLayoutAttributes *_Nullable layoutAttributes =
         [self.layout layoutAttributesForItemAtIndexPath:indexPath];
     if (layoutAttributes == nil) {
-        OWSFailDebug(@"Missing layoutAttributes.");
+        // OWSFailDebug(@"Missing layoutAttributes.");
         return nil;
     }
     CGRect cellFrame = layoutAttributes.frame;
