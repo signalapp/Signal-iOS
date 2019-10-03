88c3d0c12e65e7699d78a492dc7e67a272a64262
Matthew Chen <matthew@signal.org>
2019-10-03 10:29:20 -0300

    Address perf hotspot in thread mapping.

--- SignalServiceKit/src/Storage/Database/GRDBDatabaseStorageAdapter.swift
+++ SignalServiceKit/src/Storage/Database/GRDBDatabaseStorageAdapter.swift
@@ -215,12 +215,18 @@ public class GRDBDatabaseStorageAdapter: NSObject {
                     ThreadRecord.columnName(.shouldThreadBeVisible)
                 ]
             )
-            try db.create(index: "index_interactions_for_inbox_thread_enumeration",
+            try db.create(index: "index_interactions_for_special_messages",
                           on: InteractionRecord.databaseTableName,
                           columns: [
                             InteractionRecord.columnName(.threadUniqueId),
                             InteractionRecord.columnName(.errorType),
                             InteractionRecord.columnName(.messageType)
+                ])
+            try db.create(index: "index_interactions_for_inbox_thread_enumeration",
+                          on: InteractionRecord.databaseTableName,
+                          columns: [
+                            InteractionRecord.columnName(.threadUniqueId),
+                            InteractionRecord.columnName(.storedShouldAppearInHomeView)
                 ])
 
             // User Profile
