9683a74c18830721dc812394e82d03772254b87f
Matthew Chen <matthew@signal.org>
2019-10-21 15:33:11 -0300

    Revert "Add logging around home view updates."
    This reverts commit e6c3d6df1934110a19a0a223ad4678432cc6cf26.

--- Signal/src/Models/ThreadMapping.swift
+++ Signal/src/Models/ThreadMapping.swift
@@ -145,8 +145,6 @@ class ThreadMapping: NSObject {
             }
             threads = newThreads
         }
-        let firstThread: TSThread? = threads.first
-        Logger.info("---- firstThread: \(firstThread?.uniqueId)")
     }
 
     @objc
