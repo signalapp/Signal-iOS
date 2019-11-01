72496a2e045e292e1c171162160c1225ebaa9ea3
Nora Trapp <nora@signal.org>
2019-11-01 13:45:41 -0700

    Update protos

---
+++ SignalServiceKit/src/Messages/DeviceSyncing/OWSSyncFetchLatestMessage.h
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OWSSyncFetchLatestMessage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
