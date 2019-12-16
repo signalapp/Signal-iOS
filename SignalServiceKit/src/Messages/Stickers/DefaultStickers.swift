6f6a32997f43b29b98658176f3b987d0b3148125
Matthew Chen <matthew@signal.org>
2019-12-16 17:06:48 -0300

    Add blessed sticker packs.

--- SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
+++ SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
@@ -21,7 +21,7 @@ class DefaultStickerPack {
     private class func parseAll() -> [StickerPackInfo: DefaultStickerPack] {
         let packs = [
             // Bandit the Cat
-            DefaultStickerPack(packIdHex: "ceba4fbc5736a48bdce4dfb85e368b09", packKeyHex: "fc8a61d7b8c44de0450801978dd9bf382f741f06ac0fef9048433f4da52f8d01", shouldAutoInstall: true)!,
+            DefaultStickerPack(packIdHex: "9acc9e8aba563d26a4994e69263e3b25", packKeyHex: "5a6dff3948c28efb9b7aaf93ecc375c69fc316e78077ed26867a14d10a0f6a12", shouldAutoInstall: true)!,
             // Zozo the French Bulldog
             DefaultStickerPack(packIdHex: "fb535407d2f6497ec074df8b9c51dd1d", packKeyHex: "17e971c134035622781d2ee249e6473b774583750b68c11bb82b7509c68b6dfd", shouldAutoInstall: true)!
         ]
