0dfccd27497418d8f4cc211b8a6e20a051aa2bc6
Michael Kirk <michael@signal.org>
2019-12-16 17:42:20 -0800

    avoid compiler warning, mark protocol conformance

--- Signal/src/ViewControllers/ThreadSettings/OWSAddToContactViewController.m
+++ Signal/src/ViewControllers/ThreadSettings/OWSAddToContactViewController.m
@@ -12,7 +12,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface OWSAddToContactViewController () <ContactsViewHelperDelegate>
+@interface OWSAddToContactViewController () <CNContactViewControllerDelegate, ContactsViewHelperDelegate>
 
 @property (nonatomic) SignalServiceAddress *address;
 
