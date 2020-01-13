0c94915a981c81a099c23aad13aad3b2c385b2c4
Ryan ZHAO <ryanzhaors@qq.com>
2020-01-13 14:03:49 +1100

    change to new ssl hostname for registering toke to our server

--- SignalServiceKit/src/Loki/API/LokiPushNotificationManager.swift
+++ SignalServiceKit/src/Loki/API/LokiPushNotificationManager.swift
@@ -13,7 +13,7 @@ final class LokiPushNotificationManager : NSObject {
         print("Registering device token: (\(hexEncodedToken))")
         // Send token to Loki server
         let parameters = [ "token" : hexEncodedToken ]
-        let url = URL(string: "http://88.99.14.72:5000/register")!
+        let url = URL(string: "https://live.apns.getsession.org/register")!
         let request = TSRequest(url: url, method: "POST", parameters: parameters)
         request.allHTTPHeaderFields = [ "Content-Type" : "application/json" ]
         TSNetworkManager.shared().makeRequest(request, success: { _, response in
