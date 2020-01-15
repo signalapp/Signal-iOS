478c4fcec0efdc5e812b9715ec539b12c35d7630
Michael Kirk <michael@signal.org>
2020-01-15 14:25:59 -0700

    per cr: disable modern discovery service in staging

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -192,7 +192,7 @@ public class FeatureFlags: NSObject {
     public static var pinsForEveryone = build.includes(.dev)
 
     @objc
-    public static let useOnlyModernContactDiscovery = !isUsingProductionService && build.includes(.dev)
+    public static let useOnlyModernContactDiscovery = false
 
     @objc
     public static let compareLegacyContactDiscoveryAgainstModern = false
