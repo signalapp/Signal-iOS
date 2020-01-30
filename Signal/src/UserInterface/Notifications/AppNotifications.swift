425509d4f83f05a163eb81826e319303ec08fbef
Niels Andriesse <andriesseniels@gmail.com>
2020-01-30 11:35:44 +1100

    Fix group name handling

--- Signal/src/UserInterface/Notifications/AppNotifications.swift
+++ Signal/src/UserInterface/Notifications/AppNotifications.swift
@@ -371,8 +371,8 @@ public class NotificationPresenter: NSObject, NotificationsProtocol {
                 notificationTitle = senderName
             case is TSGroupThread:
                 var groupName = thread.name()
-                if groupName.count < 1 || groupName == "New Group" {
-                    groupName = DisplayNameUtilities.getDisplayName(for: thread as! TSGroupThread)
+                if groupName.count < 1 {
+                    groupName = MessageStrings.newGroupDefaultTitle
                 }
                 notificationTitle = String(format: NotificationStrings.incomingGroupMessageTitleFormat,
                                            senderName,
