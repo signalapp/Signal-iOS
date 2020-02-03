009b6127b9e0d06dedb846078b88dc6d390426ca
Nora Trapp <nora@signal.org>
2020-02-03 15:18:57 -0800

    Add All tab to reaction detail view

--- Signal/src/ViewControllers/ConversationView/Reactions Details/EmojiReactorsTableView.swift
+++ Signal/src/ViewControllers/ConversationView/Reactions Details/EmojiReactorsTableView.swift
@@ -1,11 +1,13 @@
 //
-//  Copyright (c) 2019 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2020 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
 
 class EmojiReactorsTableView: UITableView {
-    private var reactorItems = [(thread: TSContactThread, displayName: String, profileName: String?)]()
+    private var reactorItems = [(thread: TSContactThread, displayName: String, profileName: String?, emoji: String?)]() {
+        didSet { reloadData() }
+    }
 
     var contactsManager: OWSContactsManager {
         return Environment.shared.contactsManager
@@ -27,12 +29,28 @@ class EmojiReactorsTableView: UITableView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func configure(for emoji: String?, transaction: SDSAnyReadTransaction) {
-        defer { reloadData() }
-        guard let emoji = emoji else {
-            reactorItems = []
-            return
+    func configureForAll(transaction: SDSAnyReadTransaction) {
+        reactorItems = finder.allReactions(transaction: transaction).compactMap { reaction in
+            guard let thread = TSContactThread.getWithContactAddress(reaction.reactor, transaction: transaction) else {
+                owsFailDebug("unexpectedly missing thread for address: \(reaction.reactor)")
+                return nil
+            }
+
+            let displayName = contactsManager.displayName(for: thread, transaction: transaction)
+
+            let profileName: String?
+            if FeatureFlags.profileDisplayChanges || contactsManager.hasNameInSystemContacts(for: reaction.reactor) {
+                profileName = nil
+            } else {
+                profileName = contactsManager.formattedProfileName(for: reaction.reactor, transaction: transaction)
+            }
+
+            return (thread: thread, displayName: displayName, profileName: profileName, emoji: reaction.emoji)
         }
+    }
+
+    func configure(for emoji: String?, transaction: SDSAnyReadTransaction) {
+        guard let emoji = emoji else { return configureForAll(transaction: transaction) }
         reactorItems = finder.reactors(for: emoji, transaction: transaction).compactMap { address in
             guard let thread = TSContactThread.getWithContactAddress(address, transaction: transaction) else {
                 owsFailDebug("unexpectedly missing thread for address: \(address)")
@@ -48,7 +66,7 @@ class EmojiReactorsTableView: UITableView {
                 profileName = contactsManager.formattedProfileName(for: address, transaction: transaction)
             }
 
-            return (thread: thread, displayName: displayName, profileName: profileName)
+            return (thread: thread, displayName: displayName, profileName: profileName, emoji: emoji)
         }
     }
 }
@@ -65,13 +83,13 @@ extension EmojiReactorsTableView: UITableViewDataSource {
             return cell
         }
 
-        guard let (thread, displayName, profileName) = reactorItems[safe: indexPath.row] else {
+        guard let (thread, displayName, profileName, emoji) = reactorItems[safe: indexPath.row] else {
             owsFailDebug("unexpected indexPath")
             return cell
         }
 
         contactCell.backgroundColor = .clear
-        contactCell.configure(thread: thread, displayName: displayName, profileName: profileName)
+        contactCell.configure(thread: thread, displayName: displayName, profileName: profileName, emoji: emoji)
 
         return contactCell
     }
@@ -84,6 +102,7 @@ private class EmojiReactorCell: UITableViewCell {
     let avatarDiameter: CGFloat = 36
     let nameLabel = UILabel()
     let profileLabel = UILabel()
+    let emojiLabel = UILabel()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -101,7 +120,6 @@ private class EmojiReactorCell: UITableViewCell {
         let labelStackView = UIStackView()
         labelStackView.axis = .vertical
         contentView.addSubview(labelStackView)
-        labelStackView.autoPinTrailingToSuperviewMargin()
         labelStackView.autoPinLeading(toTrailingEdgeOf: avatarView, offset: 8)
         labelStackView.autoPinHeightToSuperviewMargins()
 
@@ -112,19 +130,28 @@ private class EmojiReactorCell: UITableViewCell {
         profileLabel.font = .ows_dynamicTypeCaption1Clamped
         profileLabel.textColor = Theme.secondaryTextAndIconColor
         labelStackView.addArrangedSubview(profileLabel)
+
+        emojiLabel.font = .boldSystemFont(ofSize: 28)
+        contentView.addSubview(emojiLabel)
+        emojiLabel.autoPinLeading(toTrailingEdgeOf: labelStackView, offset: 8)
+        emojiLabel.setContentHuggingHorizontalHigh()
+        emojiLabel.autoPinHeightToSuperviewMargins()
+        emojiLabel.autoPinTrailingToSuperviewMargin()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func configure(thread: TSContactThread, displayName: String, profileName: String?) {
+    func configure(thread: TSContactThread, displayName: String, profileName: String?, emoji: String?) {
 
         let avatarBuilder = OWSContactAvatarBuilder(
             address: thread.contactAddress,
             colorName: thread.conversationColorName,
             diameter: UInt(avatarDiameter)
         )
+
+        emojiLabel.text = emoji
 
         if thread.contactAddress.isLocalAddress {
             nameLabel.text = String(format: NSLocalizedString(
