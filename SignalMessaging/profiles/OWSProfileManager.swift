ff10f11d28feafc1e13b5a168faef3242edc6236
Nora Trapp <nora@signal.org>
2020-02-20 19:23:46 -0800

    Fix a bug where we never stored the local users avatar URL

--- SignalMessaging/profiles/OWSProfileManager.swift
+++ SignalMessaging/profiles/OWSProfileManager.swift
@@ -282,7 +282,7 @@ extension OWSProfileManager {
 
         attempt.userProfile.updateWith(givenName: attempt.update.profileGivenName,
                                    familyName: attempt.update.profileFamilyName,
-                                   avatarUrlPath: nil,
+                                   avatarUrlPath: attempt.avatarUrlPath,
                                    avatarFileName: attempt.avatarFilename,
                                    transaction: transaction,
                                    completion: nil)
