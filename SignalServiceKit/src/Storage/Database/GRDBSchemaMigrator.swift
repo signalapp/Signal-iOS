213ecd508942ef7e9287665f7c7e33abc67e9534
Matthew Chen <matthew@signal.org>
2020-03-04 15:36:30 -0300

    Increment grdbSchemaVersionLatest due to changes in TSInfoMessage.infoMessageUserInfo.

--- SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
+++ SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
@@ -89,7 +89,7 @@ public class GRDBSchemaMigrator: NSObject {
     }
 
     public static let grdbSchemaVersionDefault: UInt = 0
-    public static let grdbSchemaVersionLatest: UInt = 4
+    public static let grdbSchemaVersionLatest: UInt = 5
 
     // An optimization for new users, we have the first migration import the latest schema
     // and mark any other migrations as "already run".
