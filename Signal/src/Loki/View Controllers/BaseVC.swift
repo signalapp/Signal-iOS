c74de9698c16d8b6d844e1e85cce4765f6718c6e
gmbnt <andriesseniels@gmail.com>
2020-03-27 10:49:53 +1100

    Fix copy

--- Signal/src/Loki/View Controllers/BaseVC.swift
+++ Signal/src/Loki/View Controllers/BaseVC.swift
@@ -15,7 +15,7 @@ class BaseVC : UIViewController {
     @objc private func handleUnexpectedDeviceLinkRequestReceivedNotification() {
         guard DeviceLinkingUtilities.shouldShowUnexpectedDeviceLinkRequestReceivedAlert else { return }
         DispatchQueue.main.async {
-            let alert = UIAlertController(title: "Device Link Request Received", message: "Open the device link screen by going to \"Settings\"> \"Devices\" > \"Link a Device\" to link your devices.", preferredStyle: .alert)
+            let alert = UIAlertController(title: "Device Link Request Received", message: "Open the device link screen by going to \"Settings\" > \"Devices\" > \"Link a Device\" to link your devices.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
