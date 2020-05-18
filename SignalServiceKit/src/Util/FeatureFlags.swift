4cbdd662061a1974bfbbfb3b9f2ec9c1a53cbe44
Matthew Chen <matthew@signal.org>
2020-05-18 16:42:57 -0300

    Set feature flags for production.

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -18,7 +18,7 @@ extension FeatureBuild {
     }
 }
 
-let build: FeatureBuild = .qa
+let build: FeatureBuild = OWSIsDebugBuild() ? .dev : .beta
 
 // MARK: -
 
@@ -174,7 +174,7 @@ public class FeatureFlags: NSObject {
     public static let multiRing: Bool = false
 
     @objc
-    public static let groupsV2 = true
+    public static let groupsV2 = build.includes(.qa) && !isUsingProductionService
 
     // Don't consult this feature flag directly; instead
     // consult RemoteConfig.groupsV2CreateGroups.
@@ -261,7 +261,7 @@ public class DebugFlags: NSObject {
     public static let groupsV2dontSendUpdates = false
 
     @objc
-    public static let groupsV2showV2Indicator = true
+    public static let groupsV2showV2Indicator = FeatureFlags.groupsV2 && build.includes(.qa)
 
     // If set, v2 groups will be created and updated with invalid avatars
     // so that we can test clients' robustness to this case.
