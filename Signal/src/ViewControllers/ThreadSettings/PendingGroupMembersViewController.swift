32e3ee920d3401be16fc7a89b791754c111f0bcc
Matthew Chen <matthew@signal.org>
2020-05-20 15:34:07 -0300

    Refine "who can revoke invites" behavior.

--- Signal/src/ViewControllers/ThreadSettings/PendingGroupMembersViewController.swift
+++ Signal/src/ViewControllers/ThreadSettings/PendingGroupMembersViewController.swift
@@ -94,8 +94,9 @@ public class PendingGroupMembersViewController: OWSTableViewController {
         let localSection = OWSTableSection()
         localSection.headerTitle = NSLocalizedString("PENDING_GROUP_MEMBERS_SECTION_TITLE_PEOPLE_YOU_INVITED",
                                                      comment: "Title for the 'people you invited' section of the 'pending group members' view.")
-        let canRevoke = groupViewHelper.canEditConversationMembership
         if membersInvitedByLocalUser.count > 0 {
+            // The local user can always revoke invites from themself.
+            let canRevoke = true
             for address in membersInvitedByLocalUser {
                 localSection.add(OWSTableItem(customCellBlock: {
                     let cell = ContactTableViewCell()
@@ -127,6 +128,10 @@ public class PendingGroupMembersViewController: OWSTableViewController {
                 self.contactsManager.sortSignalServiceAddresses(Array(membersInvitedByOtherUsers.keys),
                                                                 transaction: transaction)
             }
+            // Admins can revoke any invite.
+            let canRevoke = (!groupViewHelper.threadViewModel.hasPendingMessageRequest &&
+                groupMembership.isPendingOrNonPendingMember(localAddress) &&
+                groupMembership.isAdministrator(localAddress))
             for inviterAddress in inviterAddresses {
                 guard let invitedAddresses = membersInvitedByOtherUsers[inviterAddress] else {
                     owsFailDebug("Missing invited addresses.")
@@ -140,8 +145,7 @@ public class PendingGroupMembersViewController: OWSTableViewController {
                     }
 
                     let cell = ContactTableViewCell()
-
-                    cell.selectionStyle = .none
+                    cell.selectionStyle = canRevoke ? .default : .none
 
                     let inviterName = self.contactsManager.displayName(for: inviterAddress)
                     let customName: String
