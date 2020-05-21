109c331d0e36db5b823884458d39db307d1ec370
Nora Trapp <nora@signal.org>
2020-05-21 12:36:14 -0700

    Feature flags for production
    This reverts commit ec2f9a9309de76ef7d4f6dbe5d073115402a554b.

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -18,7 +18,7 @@ extension FeatureBuild {
     }
 }
 
-let build: FeatureBuild = .qa
+let build: FeatureBuild = OWSIsDebugBuild() ? .dev : .beta
 
 // MARK: -
 
@@ -113,7 +113,7 @@ public class FeatureFlags: NSObject {
     }
 
     @objc
-    public static let uuidCapabilities = true
+    public static let uuidCapabilities = allowUUIDOnlyContacts && useOnlyModernContactDiscovery
 
     @objc
     public static var storageModeDescription: String {
@@ -135,7 +135,7 @@ public class FeatureFlags: NSObject {
     public static let strictYDBExtensions = build.includes(.beta)
 
     @objc
-    public static var allowUUIDOnlyContacts = true
+    public static var allowUUIDOnlyContacts = useOnlyModernContactDiscovery
 
     @objc
     public static var uuidSafetyNumbers = allowUUIDOnlyContacts
@@ -179,7 +179,7 @@ public class FeatureFlags: NSObject {
     public static let answerCallsOnSecondaryDevice: Bool = build.includes(.beta)
 
     @objc
-    public static let groupsV2 = true
+    public static let groupsV2 = build.includes(.qa) && !isUsingProductionService
 
     // Don't consult this feature flag directly; instead
     // consult RemoteConfig.groupsV2CreateGroups.
@@ -278,7 +278,7 @@ public class DebugFlags: NSObject {
     public static let groupsV2dontSendUpdates = false
 
     @objc
-    public static let groupsV2showV2Indicator = true
+    public static let groupsV2showV2Indicator = FeatureFlags.groupsV2 && build.includes(.qa)
 
     // If set, v2 groups will be created and updated with invalid avatars
     // so that we can test clients' robustness to this case.
