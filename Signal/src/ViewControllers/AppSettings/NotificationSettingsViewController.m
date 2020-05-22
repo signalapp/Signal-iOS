370873c359dbfe4ff7fe2c8fb45830e4eeddfb12
nielsandriesse <andriesseniels@gmail.com>
2020-05-22 16:27:56 +1000

    Clean

--- Signal/src/ViewControllers/AppSettings/NotificationSettingsViewController.m
+++ Signal/src/ViewControllers/AppSettings/NotificationSettingsViewController.m
@@ -67,15 +67,15 @@
     OWSTableSection *strategySection = [OWSTableSection new];
     strategySection.headerTitle = NSLocalizedString(@"Notification Strategy", @"");
     [strategySection addItem:[OWSTableItem switchItemWithText:NSLocalizedString(@"Use APNs", @"")
-                               accessibilityIdentifier:ACCESSIBILITY_IDENTIFIER_WITH_NAME(self, @"push_notification_strategy")
-                               isOnBlock:^{
-                                   return [NSUserDefaults.standardUserDefaults boolForKey:@"isUsingFullAPNs"];
-                               }
-                               isEnabledBlock:^{
-                                   return YES;
-                               }
-                               target:weakSelf
-                               selector:@selector(didToggleAPNsSwitch:)]];
+                              accessibilityIdentifier:ACCESSIBILITY_IDENTIFIER_WITH_NAME(self, @"push_notification_strategy")
+                              isOnBlock:^{
+                                  return [NSUserDefaults.standardUserDefaults boolForKey:@"isUsingFullAPNs"];
+                              }
+                              isEnabledBlock:^{
+                                  return YES;
+                              }
+                              target:weakSelf
+                              selector:@selector(didToggleAPNsSwitch:)]];
     strategySection.footerTitle = NSLocalizedString(@"Session will use the Apple Push Notification Service to receive push notifications. You’ll be notified of new messages reliably and immediately. Using APNs means that this device will communicate directly with Apple’s servers to retrieve push notifications, which will expose your IP address to Apple. Your messages will still be onion-routed and end-to-end encrypted, so the contents of your messages will remain completely private.", @"");
     [contents addSection:strategySection];
 
