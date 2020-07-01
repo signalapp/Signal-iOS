2bbdb14cdb0b499874df0f3dd8458c61d1774722
Michelle Linington <michelle@signal.org>
2020-06-30 17:14:27 -0700

    Adopting some PR feedback:
    - Remove explicit "self."
    - Add comment going into detail about the UIKit behavior we're working
      around
    - Remove force-unwrap from assert. It's safe, but eyebrow raising
    
    Also fixed a minor bug I found:
    percentOfIndexPathVisibleAboveBottom is explicitly checking "above the
    bottom". Rect intersection is pretty, but if a single cell extended
    above the top and bottom margins it would return an incorrect value.
    I'm not sure if this is possible, but it's still incorrect.
    
    Instead, this mostly reverts to the original behavior, comparing the top
    of the cell frame with the bottom of the content frame. It leverages the
    new function to compute the visibleContentRect to do so.

--- Signal/src/ViewControllers/ConversationView/ConversationViewController+LastVisibleSortId.swift
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController+LastVisibleSortId.swift
@@ -9,20 +9,30 @@ extension ConversationViewController {
     /// The visble content rect in the collection view's coordinate space
     /// This rect does not include displayed cells occluded by content inset
     @objc var visibleContentRect: CGRect {
-        let collectionViewBounds = self.collectionView.bounds
-        let insetBounds = collectionViewBounds.inset(by: self.collectionView.adjustedContentInset)
+        let collectionViewBounds = collectionView.bounds
+        let insetBounds = collectionViewBounds.inset(by: collectionView.adjustedContentInset)
         return insetBounds
     }
 
     /// The index path of the last item in the collection view's visible rect
     @objc var lastVisibleIndexPath: IndexPath? {
-        let visibleLayoutAttributes = self.layout.layoutAttributesForElements(in: self.visibleContentRect) ?? []
+        // For people looking at this in the future, UICollectionView has a very similar looking
+        // property: -indexPathsForVisibleItems. Why aren't we using that?
+        //
+        // That property *almost* gives us what we want, but UIKit ordering isn't favorable. That property
+        // gets updated after -scrollViewDidScroll: returns. But sometimes we want to know what cells are visible
+        // with the updated -contentOffset in -scrollViewDidScroll:. So instead, we'll just see what layoutAttributes
+        // are now in the collection view's visible content rect. This should be safe, since it's computed from the
+        // already updated -contentOffset.
+        let visibleLayoutAttributes = layout.layoutAttributesForElements(in: visibleContentRect) ?? []
 
         let lastVisibleIndexPath = visibleLayoutAttributes
             .map { $0.indexPath }
             .max { $0.row < $1.row }
 
-        assert(lastVisibleIndexPath == nil || percentOfIndexPathVisibleAboveBottom(lastVisibleIndexPath!) > 0)
+        if let lastVisibleIndexPath = lastVisibleIndexPath {
+            assert(percentOfIndexPathVisibleAboveBottom(lastVisibleIndexPath) > 0)
+        }
         return lastVisibleIndexPath
     }
 
@@ -52,8 +62,7 @@ extension ConversationViewController {
         let onScreenPercentageToSave: Double
 
         if let lastVisibleIndexPath = lastVisibleIndexPath,
-            let (indexPath, sortId) = firstIndexPathWithSortId(atOrBeforeIndexPath: lastVisibleIndexPath),
-            self.layout.layoutAttributesForItem(at: indexPath) != nil {
+            let (indexPath, sortId) = firstIndexPathWithSortId(atOrBeforeIndexPath: lastVisibleIndexPath) {
 
             sortIdToSave = sortId
             onScreenPercentageToSave = Double(percentOfIndexPathVisibleAboveBottom(indexPath))
@@ -78,14 +87,15 @@ extension ConversationViewController {
 
     private func percentOfIndexPathVisibleAboveBottom(_ indexPath: IndexPath) -> CGFloat {
         // If we don't have layout attributes, it's not visible
-        guard let attributes = self.layout.layoutAttributesForItem(at: indexPath) else { return 0.0 }
+        guard let attributes = layout.layoutAttributesForItem(at: indexPath) else { return 0.0 }
 
         // Map everything to view controller's coordinate space
-        let cellFrame_vcView = self.view.convert(attributes.frame, from: self.collectionView)
-        let visibleRect_vcView = self.view.convert(self.visibleContentRect, from: self.collectionView)
+        let cellFrameInPrimaryCoordinateSpace = view.convert(attributes.frame, from: collectionView)
+        let contentFrameInPrimaryCoordinateSpace = view.convert(visibleContentRect, from: collectionView)
 
-        let intersectionRect = visibleRect_vcView.intersection(cellFrame_vcView)
-        return CGFloatClamp01(intersectionRect.height / cellFrame_vcView.height)
+        // Distance between top edge of cell's frame and the bottom of the content frame
+        let heightAboveBottom = contentFrameInPrimaryCoordinateSpace.maxY - cellFrameInPrimaryCoordinateSpace.minY
+        return CGFloatClamp01(heightAboveBottom / cellFrameInPrimaryCoordinateSpace.height)
     }
 
     private func firstIndexPathWithSortId(atOrBeforeIndexPath indexPath: IndexPath) -> (indexPath: IndexPath, sortId: UInt64)? {
