597486f54a0fac6b3b4c6351c3c2c8ad12f9f0e2
Matthew Chen <matthew@signal.org>
2020-07-22 11:49:59 -0300

    Add MVP legacy group indicator.

--- SignalServiceKit/src/Util/NSString+SSK.h
+++ SignalServiceKit/src/Util/NSString+SSK.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2019 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2020 Open Whisper Systems. All rights reserved.
 //
 
 NS_ASSUME_NONNULL_BEGIN
@@ -7,6 +7,14 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSString (SSK)
 
 @property (nonatomic, nullable, readonly) NSString *dominantLanguageWithLegacyLinguisticTagger;
+
+@end
+
+#pragma mark -
+
+@interface NSMutableAttributedString (SSK)
+
+- (void)setAttributes:(NSDictionary<NSAttributedStringKey, id> *)attributes forSubstring:(NSString *)substring;
 
 @end
 
