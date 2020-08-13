5bad03f96d43eb21b74ab9c2a7e6eaf0662ab7bc
Matthew Chen <matthew@signal.org>
2020-08-13 09:11:15 -0300

    Add test UI around skipping unregistered users in message sends.

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -196,8 +196,19 @@ public class FeatureFlags: BaseFlags {
     @objc
     public static let attachmentUploadV3ForV1GroupAvatars = false
 
+    private static let _ignoreCDSUnregisteredUsersInMessageSends = AtomicBool(true)
     @objc
-    public static let ignoreCDSUnregisteredUsersInMessageSends = true
+    public static var ignoreCDSUnregisteredUsersInMessageSends: Bool {
+        get {
+            guard build.includes(.qa) else {
+                return true
+            }
+            return _ignoreCDSUnregisteredUsersInMessageSends.get()
+        }
+        set {
+            _ignoreCDSUnregisteredUsersInMessageSends.set(newValue)
+        }
+    }
 
     public static func buildFlagMap() -> [String: Any] {
         BaseFlags.buildFlagMap(for: FeatureFlags.self) { (key: String) -> Any? in
