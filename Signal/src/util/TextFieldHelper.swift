b72019ba944a4a994a1b4c62e0ea18490122b570
Matthew Chen <matthew@signal.org>
2020-08-25 10:04:00 -0300

    Fix edge case in textfield filtering.

--- Signal/src/util/TextFieldHelper.swift
+++ Signal/src/util/TextFieldHelper.swift
@@ -21,18 +21,33 @@ import UIKit
 
         // Filtering the string for display may insert some new characters. We need
         // to verify that after insertion the string is still within our byte bounds.
-        let filteredForDisplay = (existingString as NSString)
+        let notFilteredForDisplay = (existingString as NSString)
             .replacingCharacters(in: editingRange, with: replacementString)
-            .filterStringForDisplay()
+        let filteredForDisplay = notFilteredForDisplay.filterStringForDisplay()
 
-        let newLength = byteLength(filteredForDisplay)
+        let newUnfilteredLength = byteLength(notFilteredForDisplay)
+        let newFilteredLength = byteLength(filteredForDisplay)
 
-        if (newLength <= byteLimit) {
+        if newUnfilteredLength <= byteLimit && newFilteredLength <= byteLimit {
+            // Only allow the textfield to insert the replacement
+            // if _both_ it's filtered and unfiltered length are
+            // valid.
+            //
+            // * We can't measure just the filtered length or we
+            //   would allow unlimited whitespace to be appended
+            //   to the end of the string.
+            // * We can't measure just the unfiltered length, since
+            //   filterStringForDisplay() can increase the length
+            //   of the string (e.g. appending Bidi characters).
+            // * We can't replace the textfield contents with the
+            //   filtered string, or we would prevent users from
+            //   (legitimately) appending whitespace to the tail of
+            //   of the string.
             return true
         }
 
         // Don't allow any change if inserting a single char is already over the limit (typically this means typing)
-        if (replacementString.count < 2) {
+        if replacementString.count < 2 {
             return false
         }
 
@@ -47,7 +62,7 @@ import UIKit
                 .replacingCharacters(in: editingRange, with: maybeAcceptableSubstring)
                 .filterStringForDisplay()
 
-            if (byteLength(newFilteredString) <= byteLimit) {
+            if byteLength(newFilteredString) <= byteLimit {
                 acceptableSubstring = maybeAcceptableSubstring
             } else {
                 break
