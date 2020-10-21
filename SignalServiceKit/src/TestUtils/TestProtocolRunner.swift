07c20a5f6f1a3be168f7f633caae9809b5e3df7a
Jordan Rose <jrose@signal.org>
2020-10-21 14:59:46 -0700

    Rename the SignalClient test protocol to avoid conflicts

--- SignalServiceKit/src/TestUtils/TestProtocolRunner.swift
+++ SignalServiceKit/src/TestUtils/TestProtocolRunner.swift
@@ -13,7 +13,7 @@ public struct TestProtocolRunner {
 
     let accountIdentifierFinder  = OWSAccountIdFinder()
 
-    public func initialize(senderClient: SignalClient, recipientClient: SignalClient, transaction: SDSAnyWriteTransaction) throws {
+    public func initialize(senderClient: TestSignalClient, recipientClient: TestSignalClient, transaction: SDSAnyWriteTransaction) throws {
 
         let senderIdentifier = accountIdentifierFinder.ensureAccountId(forAddress: senderClient.address, transaction: transaction)
         let recipientIdentifier = accountIdentifierFinder.ensureAccountId(forAddress: recipientClient.address, transaction: transaction)
@@ -29,12 +29,12 @@ public struct TestProtocolRunner {
                                                        protocolContext: transaction)
     }
 
-    public func encrypt(plaintext: Data, senderClient: SignalClient, recipientAccountId: SignalAccountIdentifier, protocolContext: SPKProtocolWriteContext?) throws -> CipherMessage {
+    public func encrypt(plaintext: Data, senderClient: TestSignalClient, recipientAccountId: SignalAccountIdentifier, protocolContext: SPKProtocolWriteContext?) throws -> CipherMessage {
         let sessionCipher = try senderClient.sessionCipher(for: recipientAccountId)
         return try sessionCipher.encryptMessage(plaintext, protocolContext: protocolContext)
     }
 
-    public func decrypt(cipherMessage: CipherMessage, recipientClient: SignalClient, senderAccountId: SignalAccountIdentifier, protocolContext: SPKProtocolWriteContext?) throws -> Data {
+    public func decrypt(cipherMessage: CipherMessage, recipientClient: TestSignalClient, senderAccountId: SignalAccountIdentifier, protocolContext: SPKProtocolWriteContext?) throws -> Data {
         let sessionCipher = try recipientClient.sessionCipher(for: senderAccountId)
         return try sessionCipher.decrypt(cipherMessage, protocolContext: protocolContext)
     }
@@ -46,7 +46,7 @@ public typealias SignalAccountIdentifier = String
 
 /// Represents a Signal installation, it can represent the local client or
 /// a remote client.
-public protocol SignalClient {
+public protocol TestSignalClient {
     var identityKeyPair: ECKeyPair { get }
     var identityKey: IdentityKey { get }
     var e164Identifier: SignalE164Identifier? { get }
@@ -63,7 +63,7 @@ public protocol SignalClient {
     func sessionCipher(for accountId: SignalAccountIdentifier) throws -> SessionCipher
 }
 
-public extension SignalClient {
+public extension TestSignalClient {
     var identityKey: IdentityKey {
         return identityKeyPair.publicKey
     }
@@ -96,7 +96,7 @@ public extension SignalClient {
 
 /// Can be used to represent the protocol state held by a remote client.
 /// i.e. someone who's sending messages to the local client.
-public struct FakeSignalClient: SignalClient {
+public struct FakeSignalClient: TestSignalClient {
 
     public var sessionStore: SessionStore { return protocolStore }
     public var preKeyStore: PreKeyStore { return protocolStore }
@@ -128,7 +128,7 @@ public struct FakeSignalClient: SignalClient {
 
 /// Represents the local user, backed by the same protocol stores, etc.
 /// used in the app.
-public struct LocalSignalClient: SignalClient {
+public struct LocalSignalClient: TestSignalClient {
 
     public init() { }
 
@@ -181,7 +181,7 @@ public struct FakeService {
         return SDSDatabaseStorage.shared
     }
 
-    public func envelopeBuilder(fromSenderClient senderClient: SignalClient, bodyText: String? = nil) throws -> SSKProtoEnvelope.SSKProtoEnvelopeBuilder {
+    public func envelopeBuilder(fromSenderClient senderClient: TestSignalClient, bodyText: String? = nil) throws -> SSKProtoEnvelope.SSKProtoEnvelopeBuilder {
         envelopeId += 1
         let builder = SSKProtoEnvelope.builder(timestamp: envelopeId)
         builder.setType(.ciphertext)
@@ -196,7 +196,7 @@ public struct FakeService {
         return builder
     }
 
-    public func buildEncryptedContentData(fromSenderClient senderClient: SignalClient, bodyText: String?) throws -> Data {
+    public func buildEncryptedContentData(fromSenderClient senderClient: TestSignalClient, bodyText: String?) throws -> Data {
         let plaintext = try buildContentData(bodyText: bodyText)
         let cipherMessage: CipherMessage = databaseStorage.write { transaction in
             return try! self.runner.encrypt(plaintext: plaintext,
