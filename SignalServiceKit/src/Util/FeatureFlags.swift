6e2f808bc058e2b62c44ba17bf3ae8a6bd383ca2
Nora Trapp <nora@signal.org>
2020-10-28 22:02:04 -0700

    Enable for QA

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -202,7 +202,7 @@ public class FeatureFlags: BaseFlags {
     public static let supportAnimatedStickers_AnimatedWebp = true
 
     @objc
-    public static let groupCalling = build.includes(.dev)
+    public static let groupCalling = build.includes(.qa)
 
     public static func buildFlagMap() -> [String: Any] {
         BaseFlags.buildFlagMap(for: FeatureFlags.self) { (key: String) -> Any? in
