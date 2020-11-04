4ff2c0d6683d92af6cd6c613dd37f49be6697ab0
nielsandriesse <andriesseniels@gmail.com>
2020-11-04 14:18:15 +1100

    Limit voice messages to one minute

--- Signal/src/Loki/Components/MentionCandidateSelectionViewDelegate.swift
+++
@@ -1,6 +0,0 @@
-
-@objc(LKMentionCandidateSelectionViewDelegate)
-protocol MentionCandidateSelectionViewDelegate {
-    
-    func handleMentionCandidateSelected(_ mentionCandidate: Mention, from mentionCandidateSelectionView: MentionCandidateSelectionView)
-}
