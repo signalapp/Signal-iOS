604041e8222d2ac7cf6ca3812ce0e16bdf1c3165
Nora Trapp <nora@signal.org>
2021-01-13 17:48:56 -0800

    Increase sticker size limits to match Android.

--- SignalServiceKit/src/Messages/Stickers/CDNDownloadOperation.swift
+++ SignalServiceKit/src/Messages/Stickers/CDNDownloadOperation.swift
@@ -39,8 +39,8 @@ open class CDNDownloadOperation: OWSOperation {
         task?.cancel()
     }
 
-    let kMaxStickerDataDownloadSize: UInt = 300 * 1000
-    let kMaxStickerPackDownloadSize: UInt = 250 * 1000
+    let kMaxStickerDataDownloadSize: UInt = 1000 * 1000
+    let kMaxStickerPackDownloadSize: UInt = 1000 * 1000
 
     public func tryToDownload(urlPath: String, maxDownloadSize: UInt?) throws -> Promise<Data> {
         guard !isCorrupt(urlPath: urlPath) else {
