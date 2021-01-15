29837b4bae2f92ce4411bebfdd8b2320d997603e
Matthew Chen <matthew@signal.org>
2021-01-15 16:44:19 -0300

    Allow cancellation of attachment downloads.

--- SignalServiceKit/src/Util/FeatureFlags.swift
+++ SignalServiceKit/src/Util/FeatureFlags.swift
@@ -249,7 +249,7 @@ public class DebugFlags: BaseFlags {
     public static let suppressBackgroundActivity = false
 
     @objc
-    public static let reduceLogChatter = true
+    public static let reduceLogChatter = false
 
     @objc
     public static let logSQLQueries = build.includes(.dev) && !reduceLogChatter
