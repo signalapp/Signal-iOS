933693e78aa750f35d32d51a9d3ad70930f41ce0
Matthew Chen <matthew@signal.org>
2021-01-20 10:45:33 -0300

    Add animated sticker pack.

--- SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
+++ SignalServiceKit/src/Messages/Stickers/DefaultStickers.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2020 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -24,14 +24,15 @@ class DefaultStickerPack {
         }
         let packs = [
             // Bandit the Cat
-            DefaultStickerPack(packIdHex: "9acc9e8aba563d26a4994e69263e3b25", packKeyHex: "5a6dff3948c28efb9b7aaf93ecc375c69fc316e78077ed26867a14d10a0f6a12", shouldAutoInstall: false)!,
+            DefaultStickerPack(packIdHex: "9acc9e8aba563d26a4994e69263e3b25", packKeyHex: "5a6dff3948c28efb9b7aaf93ecc375c69fc316e78077ed26867a14d10a0f6a12", shouldAutoInstall: true)!,
             // Zozo the French Bulldog
-            DefaultStickerPack(packIdHex: "fb535407d2f6497ec074df8b9c51dd1d", packKeyHex: "17e971c134035622781d2ee249e6473b774583750b68c11bb82b7509c68b6dfd", shouldAutoInstall: false)!,
+            DefaultStickerPack(packIdHex: "fb535407d2f6497ec074df8b9c51dd1d", packKeyHex: "17e971c134035622781d2ee249e6473b774583750b68c11bb82b7509c68b6dfd", shouldAutoInstall: true)!,
             // Swoon Hands
             DefaultStickerPack(packIdHex: "e61fa0867031597467ccc036cc65d403", packKeyHex: "13ae7b1a7407318280e9b38c1261ded38e0e7138b9f964a6ccbb73e40f737a9b", shouldAutoInstall: false)!,
             // Swoon Faces
-            DefaultStickerPack(packIdHex: "cca32f5b905208b7d0f1e17f23fdc185", packKeyHex: "8bf8e95f7a45bdeafe0c8f5b002ef01ab95b8f1b5baac4019ccd6b6be0b1837a", shouldAutoInstall: false)!
-
+            DefaultStickerPack(packIdHex: "cca32f5b905208b7d0f1e17f23fdc185", packKeyHex: "8bf8e95f7a45bdeafe0c8f5b002ef01ab95b8f1b5baac4019ccd6b6be0b1837a", shouldAutoInstall: false)!,
+            // Day by Day
+            DefaultStickerPack(packIdHex: "cfc50156556893ef9838069d3890fe49", packKeyHex: "5f5beab7d382443cb00a1e48eb95297b6b8cadfd0631e5d0d9dc949e6999ff4b", shouldAutoInstall: true)!
         ]
         var result = [StickerPackInfo: DefaultStickerPack]()
         for pack in packs {
@@ -44,7 +45,7 @@ class DefaultStickerPack {
     private static let all = DefaultStickerPack.parseAll()
 
     static var packsToAutoInstall: [StickerPackInfo] {
-        return all.values.filter {
+        all.values.filter {
             $0.shouldAutoInstall
             }.map {
                 $0.info
@@ -52,7 +53,7 @@ class DefaultStickerPack {
     }
 
     static var packsToNotAutoInstall: [StickerPackInfo] {
-        return all.values.filter {
+        all.values.filter {
             !$0.shouldAutoInstall
             }.map {
                 $0.info
@@ -60,6 +61,10 @@ class DefaultStickerPack {
     }
 
     class func isDefaultStickerPack(stickerPackInfo: StickerPackInfo) -> Bool {
-        return all[stickerPackInfo] != nil
+        all[stickerPackInfo] != nil
+    }
+
+    class func getDefaultStickerPack(stickerPackInfo: StickerPackInfo) -> DefaultStickerPack? {
+        all[stickerPackInfo]
     }
 }
