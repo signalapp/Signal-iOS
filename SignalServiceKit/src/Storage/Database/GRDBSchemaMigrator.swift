f8b1985930c47dfc7c53ea694cd87183a86b54fd
Jordan Rose <jrose@signal.org>
2021-01-27 15:39:46 -0800

    Bump the database schema version

--- SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
+++ SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
@@ -141,7 +141,7 @@ public class GRDBSchemaMigrator: NSObject {
     }
 
     public static let grdbSchemaVersionDefault: UInt = 0
-    public static let grdbSchemaVersionLatest: UInt = 18
+    public static let grdbSchemaVersionLatest: UInt = 19
 
     // An optimization for new users, we have the first migration import the latest schema
     // and mark any other migrations as "already run".
