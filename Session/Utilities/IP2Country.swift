59d071d9f5a9003fd48cdf4d0765ca332b4d45af
Niels Andriesse <andriesseniels@gmail.com>
2021-01-27 14:06:28 +1100

    Improve onion request path loading efficiency

--- Session/Utilities/IP2Country.swift
+++ Session/Utilities/IP2Country.swift
@@ -2,8 +2,17 @@
 final class IP2Country {
     var countryNamesCache: [String:String] = [:]
 
-    private lazy var ipv4Table = try! CSV(name: "GeoLite2-Country-Blocks-IPv4", extension: "csv", bundle: .main, delimiter: ",", encoding: .utf8, loadColumns: true)!
-    private lazy var countryNamesTable = try! CSV(name: "GeoLite2-Country-Locations-English", extension: "csv", bundle: .main, delimiter: ",", encoding: .utf8, loadColumns: true)!
+    private lazy var ipv4Table: [String:[String]] = {
+        let url = Bundle.main.url(forResource: "GeoLite2-Country-Blocks-IPv4", withExtension: nil)!
+        let data = try! Data(contentsOf: url)
+        return try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as! [String:[String]]
+    }()
+    
+    private lazy var countryNamesTable: [String:[String]] = {
+        let url = Bundle.main.url(forResource: "GeoLite2-Country-Locations-English", withExtension: nil)!
+        let data = try! Data(contentsOf: url)
+        return try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as! [String:[String]]
+    }()
 
     private static let workQueue = DispatchQueue(label: "IP2Country.workQueue", qos: .utility) // It's important that this is a serial queue
 
@@ -25,10 +34,10 @@ final class IP2Country {
         var truncatedIP = ip
         func getCountryInternal() -> String {
             if let country = countryNamesCache[ip] { return country }
-            if let ipv4TableIndex = ipv4Table.namedColumns["network"]!.firstIndex(where: { $0.starts(with: truncatedIP) }) {
-                let countryID = ipv4Table.namedColumns["registered_country_geoname_id"]![ipv4TableIndex]
-                if let countryNamesTableIndex = countryNamesTable.namedColumns["geoname_id"]!.firstIndex(of: countryID) {
-                    let country = countryNamesTable.namedColumns["country_name"]![countryNamesTableIndex]
+            if let ipv4TableIndex = ipv4Table["network"]!.firstIndex(where: { $0.starts(with: truncatedIP) }) {
+                let countryID = ipv4Table["registered_country_geoname_id"]![ipv4TableIndex]
+                if let countryNamesTableIndex = countryNamesTable["geoname_id"]!.firstIndex(of: countryID) {
+                    let country = countryNamesTable["country_name"]![countryNamesTableIndex]
                     countryNamesCache[ip] = country
                     return country
                 }
