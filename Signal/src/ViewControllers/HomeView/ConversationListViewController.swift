7a6159400bd7e9d1bda1b6c81e14ae4d4f283c41
Matthew Chen <matthew@signal.org>
2021-03-17 09:58:54 -0300

    Fix formatting of payment amounts in notifications.

--- Signal/src/ViewControllers/HomeView/ConversationListViewController.swift
+++ Signal/src/ViewControllers/HomeView/ConversationListViewController.swift
@@ -26,7 +26,9 @@ public extension ConversationListViewController {
 
         let userName = contactsManager.shortDisplayName(for: address, transaction: transaction)
         let formattedAmount = PaymentsFormat.format(paymentAmount: paymentAmount,
-                                                    isShortForm: true)
+                                                    isShortForm: true,
+                                                    withCurrencyCode: true,
+                                                    withSpace: true)
         let format = NSLocalizedString("PAYMENTS_NOTIFICATION_BANNER_1_WITH_DETAILS_FORMAT",
                                        comment: "Format for the payments notification banner for a single payment notification with details. Embeds: {{ %1$@ the name of the user who sent you the payment, %2$@ the amount of the payment }}.")
         let title = String(format: format, userName, formattedAmount)
