ae1031f5840329e4c64459189502be9e3e587e5a
Matthew Chen <matthew@signal.org>
2021-04-13 15:47:58 -0300

    Fix perf hotspot in CVRenderItem.

--- Signal/src/ViewControllers/ConversationView/CV/CVRenderItem.swift
+++ Signal/src/ViewControllers/ConversationView/CV/CVRenderItem.swift
@@ -17,6 +17,8 @@ public class CVRenderItem: NSObject {
     public let cellMeasurement: CVCellMeasurement
     public var cellSize: CGSize { cellMeasurement.cellSize }
 
+    private let incomingMessageAuthorUuid: String?
+
     @objc
     public var cellReuseIdentifier: String {
         rootComponent.cellReuseIdentifier.rawValue
@@ -37,6 +39,13 @@ public class CVRenderItem: NSObject {
         self.itemModel = itemModel
         self.rootComponent = rootComponent
         self.cellMeasurement = cellMeasurement
+
+        // This value is used in a particularly hot code path.
+        if let incomingMessage = itemModel.interaction as? TSIncomingMessage {
+            self.incomingMessageAuthorUuid = incomingMessage.authorUUID
+        } else {
+            self.incomingMessageAuthorUuid = nil
+        }
     }
 
     enum UpdateMode {
@@ -110,9 +119,9 @@ extension CVRenderItem: ConversationViewLayoutItem {
         case .incomingMessage:
             switch previousInteraction.interactionType() {
             case .incomingMessage:
-                if let incomingMessage = interaction as? TSIncomingMessage,
-                   let previousIncomingMessage = previousInteraction as? TSIncomingMessage,
-                   incomingMessage.authorAddress == previousIncomingMessage.authorAddress {
+                if let selfAuthorUuid = self.incomingMessageAuthorUuid,
+                   let prevAuthorUuid = previousLayoutItem.incomingMessageAuthorUuid,
+                   selfAuthorUuid == prevAuthorUuid {
                     return ConversationStyle.compactMessageSpacing
                 }
                 return ConversationStyle.defaultMessageSpacing
