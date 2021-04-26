ea1b556b71a1b6f7174a9df64d7021e736ada7fd
Matthew Chen <matthew@signal.org>
2021-04-26 15:09:59 -0300

    Don't dismiss "low trust warning" interactions when user taps to reveal blurred avatars.

--- Signal/src/ViewControllers/ConversationView/CV/CVItemViewState.swift
+++ Signal/src/ViewControllers/ConversationView/CV/CVItemViewState.swift
@@ -620,7 +620,8 @@ fileprivate extension CVMessageMapping {
     }
     func shouldShowUnknownThreadWarning(thread: TSThread,
                                         transaction: SDSAnyReadTransaction) -> Bool {
-        !canLoadOlder && Self.contactsManagerImpl.shouldBlurAvatar(thread: thread,
+        !canLoadOlder && Self.contactsManagerImpl.isLowTrustThread(thread: thread,
+                                                                   allowUserToDismiss: false,
                                                                    transaction: transaction)
     }
 }
