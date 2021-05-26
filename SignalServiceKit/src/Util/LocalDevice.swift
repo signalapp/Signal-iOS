e7d9482fe955f5b40e4ad676d69244de01764724
Matthew Chen <matthew@signal.org>
2021-05-25 21:44:36 -0300

    Respond to CR.

--- SignalServiceKit/src/Util/LocalDevice.swift
+++ SignalServiceKit/src/Util/LocalDevice.swift
@@ -8,8 +8,6 @@ import Foundation
 public class LocalDevice: NSObject {
     @objc
     public static var allCoreCount: Int {
-        // iOS can shut down cores, so we consult activeProcessorCount,
-        // not processorCount.
         ProcessInfo.processInfo.processorCount
     }
 
@@ -18,16 +16,5 @@ public class LocalDevice: NSObject {
         // iOS can shut down cores, so we consult activeProcessorCount,
         // not processorCount.
         ProcessInfo.processInfo.activeProcessorCount
-    }
-
-    @objc
-    public static var offMainCoreCount: UInt {
-        // When working off the main thread, we want to avoid spawning
-        // so many threads that we affect the main thread. Therefore
-        // in any place that we control fan out (e.g. OperationQueue,
-        // GCDQueueCluster, etc.) we limit concurrency to the number of
-        // active cores less one.
-
-        UInt(max(1, activeCoreCount - 1))
     }
 }
