c98cfe420d09000aa07474f46702ec65f5b38b42
nielsandriesse <andriesseniels@gmail.com>
2021-05-28 09:58:33 +1000

    Fix spacing

--- SessionUtilitiesKit/General/General.swift
+++ SessionUtilitiesKit/General/General.swift
@@ -5,7 +5,6 @@
 @inline(never)
 public func touch<Value>(_ value: Value) { /* Do nothing */ }
 
-
 /// Returns `f(x!)` if `x != nil`, or `nil` otherwise.
 public func given<T, U>(_ x: T?, _ f: (T) throws -> U) rethrows -> U? { return try x.map(f) }
 
