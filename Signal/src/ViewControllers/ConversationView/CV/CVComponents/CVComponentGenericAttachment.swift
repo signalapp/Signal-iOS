b973406d9ba32b309acf7ed0e25507b1344f4fb5
Artem Varaksa <artvaraksa@gmail.com>
2021-07-15 13:57:29 +0300

    Show a `PKAddPassesViewController` if a pass attachment was tapped.

--- Signal/src/ViewControllers/ConversationView/CV/CVComponents/CVComponentGenericAttachment.swift
+++ Signal/src/ViewControllers/ConversationView/CV/CVComponents/CVComponentGenericAttachment.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import QuickLook
+import PassKit
 
 @objc
 public class CVComponentGenericAttachment: CVComponentBase, CVComponent {
@@ -316,6 +317,17 @@ public class CVComponentGenericAttachment: CVComponentBase, CVComponent {
             return false
         }
         return QLPreviewController.canPreview(url as NSURL)
+    }
+
+    /// Returns the `PKPass` represented by this attachment, if any.
+    public func representedPKPass() -> PKPass? {
+        guard attachmentStream?.contentType == "application/vnd.apple.pkpass" else {
+            return nil
+        }
+        guard let data = try? attachmentStream?.readDataFromFile() else {
+            return nil
+        }
+        return try? PKPass(data: data)
     }
 
     @objc(showShareUIFromView:)
