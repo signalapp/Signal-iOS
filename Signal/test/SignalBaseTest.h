523d7be380cfaccd30794e3a99fe4a325ceeff32
Matthew Chen <matthew@signal.org>
2021-07-20 16:43:17 -0700

    Revert header import fix.

--- Signal/test/SignalBaseTest.h
+++ Signal/test/SignalBaseTest.h
@@ -2,7 +2,7 @@
 //  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
 //
 
-#import <SignalServiceKit/MockEnvironment.h>
+#import "MockEnvironment.h"
 #import <SignalServiceKit/MockSSKEnvironment.h>
 #import <XCTest/XCTest.h>
 
