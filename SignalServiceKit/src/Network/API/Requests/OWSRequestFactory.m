7acce7d9fdcbdb687f1992f6f970b48811b268cc
Matthew Chen <matthew@signal.org>
2021-08-09 15:43:36 -0300

    Respond to CR.

--- SignalServiceKit/src/Network/API/Requests/OWSRequestFactory.m
+++ SignalServiceKit/src/Network/API/Requests/OWSRequestFactory.m
@@ -505,6 +505,10 @@ NSString *const OWSRequestKey_AuthKey = @"AuthKey";
     capabilities[@"transfer"] = @(YES);
     capabilities[@"announcementGroup"] = @(YES);
 
+    // If the storage service requires (or will require) secondary devices
+    // to have a capability in order to be linked, we might need to always
+    // set that capability here if isSecondaryDevice is true.
+
     if (SSKFeatureFlags.groupsV2MigrationSetCapability
         && !SSKDebugFlags.groupsV2migrationsDisableMigrationCapability.value) {
         capabilities[@"gv1-migration"] = @(YES);
