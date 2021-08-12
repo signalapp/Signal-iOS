b513eeb898d12eb80c9ce1dd9ba3c9e9be20e555
Niels Andriesse <andriesseniels@gmail.com>
2021-08-12 11:46:04 +1000

    Add mock TURN server implementation

---
+++ SessionMessagingKit/Calls/TURNServer.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) 2018 Open Whisper Systems. All rights reserved.
+// 
+
+import PromiseKit
+
+enum MockTURNSserver {
+    
+    static func getICEServerURL() -> Promise<String> {
+        HTTP.execute(.get, "https://appr.tc/params").map2 { json in
+            guard let url = json["ice_server_url"] as? String else { throw HTTP.Error.invalidJSON }
+            return url
+        }
+    }
+    
+    static func makeTurnServerRequest(iceServerURL: String) -> Promise<JSON> {
+        let headers = [ "referer" : "https://appr.tc" ]
+        return HTTP.execute(.post, iceServerURL, body: nil, headers: headers)
+    }
+}
