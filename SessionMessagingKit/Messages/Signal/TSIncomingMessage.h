d319840c045e666f4ac0bafd75d1ac0010124b53
ryanzhao <ryanzhaors@qq.com>
2021-08-23 11:15:05 +1000

    clean unused server timestamp

--- SessionMessagingKit/Messages/Signal/TSIncomingMessage.h
+++ SessionMessagingKit/Messages/Signal/TSIncomingMessage.h
@@ -59,7 +59,6 @@ NS_ASSUME_NONNULL_BEGIN
                  expiresInSeconds:(uint32_t)expiresInSeconds
                     quotedMessage:(nullable TSQuotedMessage *)quotedMessage
                       linkPreview:(nullable OWSLinkPreview *)linkPreview
-                  serverTimestamp:(nullable NSNumber *)serverTimestamp
                   wasReceivedByUD:(BOOL)wasReceivedByUD
           openGroupInvitationName:(nullable NSString *)openGroupInvitationName
            openGroupInvitationURL:(nullable NSString *)openGroupInvitationURL
