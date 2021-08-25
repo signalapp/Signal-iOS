ecc0d98bff450af239aa5f59f4c08ae6e8a56402
Matthew Chen <matthew@signal.org>
2021-08-25 11:01:10 -0300

    Fix crash when scanning QR codes.

--- Signal/src/ViewControllers/AppSettings/Linked Devices/ScanQRCodeViewController.m
+++ Signal/src/ViewControllers/AppSettings/Linked Devices/ScanQRCodeViewController.m
@@ -31,7 +31,8 @@ NS_ASSUME_NONNULL_BEGIN
     // * Is now VNBarcodeSymbologyQR in Obj-C in Xcode 13/iOS SDK 15.
     //
     // If we use the old symbol in Swift, the app will crash when scanning
-    // if built using Xcode 13/iOS SDK 15.
+    // if built using Xcode 13/iOS SDK 15 when scanning on devices running
+    // iOS 14 or earlier.
     //
     // We cannot use the new symbol in Swift, because we still use
     // both Xcode 12 and 13 to cut builds.
