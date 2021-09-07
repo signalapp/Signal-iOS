f817c40370d1ca9d26aac13e3798f9f2a5a8982c
Nora Trapp <nora@signal.org>
2021-09-07 15:32:07 -0700

    Revert changes around error handling now that when(fulfilled:) works correctly

--- SignalServiceKit/src/Messages/MessageSender.m
+++ SignalServiceKit/src/Messages/MessageSender.m
@@ -699,9 +699,6 @@ NSString *const MessageSenderSpamChallengeResolvedException = @"SpamChallengeRes
             // completion promise to execute until _all_ send promises
             // have either succeeded or failed. Fulfilled executes as
             // soon as any of its input promises fail.
-            //
-            // TODO: We need something like PMKJoin() which will reject
-            //       if any of the sendPromises reject.
             return [AnyPromise whenResolved:sendPromises];
         });
 }
@@ -804,12 +801,7 @@ NSString *const MessageSenderSpamChallengeResolvedException = @"SpamChallengeRes
                                       }
                                   }];
         })
-        .doneInBackground(^(id value) {
-            @synchronized(sendErrors) {
-                OWSAssertDebug(sendErrors.count == 0);
-            }
-            successHandler();
-        })
+        .doneInBackground(^(id value) { successHandler(); })
         .catchInBackground(^(id failure) {
             // Ignore the failure value; consult sendErrors and sendErrorPerRecipientCopy.
             NSError *firstRetryableError = nil;
