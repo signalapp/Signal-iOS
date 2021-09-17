4ee4cd0c4e4a6668f867d8ad9fdf0ea07e9a9843
Matthew Chen <matthew@signal.org>
2021-09-17 16:00:01 -0300

    Tweak the label for quoted photos.

--- Signal/src/ViewControllers/ConversationView/Cells/QuotedMessageView.swift
+++ Signal/src/ViewControllers/ConversationView/Cells/QuotedMessageView.swift
@@ -289,9 +289,6 @@ public class QuotedMessageView: ManualStackViewWithLayer {
             } else if MIMETypeUtil.isVideo(contentType) {
                 return NSLocalizedString("QUOTED_REPLY_TYPE_VIDEO",
                                          comment: "Indicates this message is a quoted reply to a video file.")
-            } else if MIMETypeUtil.isImage(contentType) {
-                return NSLocalizedString("QUOTED_REPLY_TYPE_IMAGE",
-                                         comment: "Indicates this message is a quoted reply to an image file.")
             } else if MIMETypeUtil.isAnimated(contentType) {
                 if contentType.caseInsensitiveCompare(OWSMimeTypeImageGif) == .orderedSame {
                     return NSLocalizedString("QUOTED_REPLY_TYPE_GIF",
@@ -300,6 +297,9 @@ public class QuotedMessageView: ManualStackViewWithLayer {
                     return NSLocalizedString("QUOTED_REPLY_TYPE_IMAGE",
                                              comment: "Indicates this message is a quoted reply to an image file.")
                 }
+            } else if MIMETypeUtil.isImage(contentType) {
+                return NSLocalizedString("QUOTED_REPLY_TYPE_PHOTO",
+                                         comment: "Indicates this message is a quoted reply to a photo file.")
             }
             return nil
         }
