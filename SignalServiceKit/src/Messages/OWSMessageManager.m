cea81733e75f3707474c976e61f1d9562f30d3e3
Matthew Chen <matthew@signal.org>
2021-10-04 11:23:01 -0300

    Refine logging around NSE message processing.

--- SignalServiceKit/src/Messages/OWSMessageManager.m
+++ SignalServiceKit/src/Messages/OWSMessageManager.m
@@ -520,7 +520,7 @@ NS_ASSUME_NONNULL_BEGIN
         return;
     }
 
-    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+    if (SSKDebugFlags.internalLogging) {
         OWSLogInfo(@"1 timestamp: %llu, serviceTimestamp: %llu, %@",
             envelope.timestamp,
             serverDeliveryTimestamp,
@@ -558,7 +558,7 @@ NS_ASSUME_NONNULL_BEGIN
         }
     }
 
-    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+    if (SSKDebugFlags.internalLogging) {
         OWSLogInfo(@"2 timestamp: %llu, serviceTimestamp: %llu, ", envelope.timestamp, serverDeliveryTimestamp);
     }
 
@@ -578,7 +578,7 @@ NS_ASSUME_NONNULL_BEGIN
         }
     }
 
-    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+    if (SSKDebugFlags.internalLogging) {
         OWSLogInfo(@"3 timestamp: %llu, serviceTimestamp: %llu, ", envelope.timestamp, serverDeliveryTimestamp);
     }
 
@@ -594,7 +594,7 @@ NS_ASSUME_NONNULL_BEGIN
         return;
     }
 
-    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+    if (SSKDebugFlags.internalLogging) {
         OWSLogInfo(@"4 timestamp: %llu, serviceTimestamp: %llu, ", envelope.timestamp, serverDeliveryTimestamp);
     }
 
@@ -2112,6 +2112,10 @@ NS_ASSUME_NONNULL_BEGIN
     // The sender may have resent the message. If so, we should swap it in place of the placeholder
     [message insertOrReplacePlaceholderFrom:authorAddress transaction:transaction];
 
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 1: %@", messageDescription);
+    }
+
     NSArray<TSAttachmentPointer *> *attachmentPointers =
         [TSAttachmentPointer attachmentPointersFromProtos:dataMessage.attachments albumMessage:message];
 
@@ -2128,7 +2132,15 @@ NS_ASSUME_NONNULL_BEGIN
     }
     OWSAssertDebug(message.hasRenderableContent);
 
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 2: %@", messageDescription);
+    }
+
     [self.earlyMessageManager applyPendingMessagesFor:message transaction:transaction];
+
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 3: %@", messageDescription);
+    }
 
     // Any messages sent from the current user - from this device or another - should be automatically marked as read.
     if (envelope.sourceAddress.isLocalAddress) {
@@ -2143,11 +2155,21 @@ NS_ASSUME_NONNULL_BEGIN
                            transaction:transaction];
     }
 
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 4: %@", messageDescription);
+    }
+
     [self.attachmentDownloads enqueueDownloadOfAttachmentsForNewMessage:message transaction:transaction];
 
-    [SSKEnvironment.shared.notificationsManager notifyUserForIncomingMessage:message
-                                                                      thread:thread
-                                                                 transaction:transaction];
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 5: %@", messageDescription);
+    }
+
+    [self.notificationsManager notifyUserForIncomingMessage:message thread:thread transaction:transaction];
+
+    if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
+        OWSLogInfo(@"Inserted 6: %@", messageDescription);
+    }
 
     dispatch_async(dispatch_get_main_queue(), ^{
         [self.typingIndicatorsImpl didReceiveIncomingMessageInThread:thread
@@ -2156,7 +2178,7 @@ NS_ASSUME_NONNULL_BEGIN
     });
 
     if (SSKDebugFlags.internalLogging || CurrentAppContext().isNSE) {
-        OWSLogInfo(@"Inserted: %@", messageDescription);
+        OWSLogInfo(@"Inserted 7: %@", messageDescription);
     }
 
     return message;
