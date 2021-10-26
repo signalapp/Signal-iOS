e2de82a11d076ff32a130a716082e6a4e2bf79b4
ryanzhao <ryanzhaors@qq.com>
2021-10-26 16:36:04 +1100

    WIP: webRTC + callkit

---
+++ SessionMessagingKit/Calls/WebRTCSession+CallKit.swift
@@ -0,0 +1,11 @@
+import CallKit
+
+extension WebRTCSession: CXProviderDelegate {
+    public func providerDidReset(_ provider: CXProvider) {
+        
+    }
+    
+    public func reportIncomingCall() {
+        
+    }
+}
