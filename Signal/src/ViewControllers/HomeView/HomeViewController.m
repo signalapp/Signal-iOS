11539bbde7d3b01ef565dbaf12ae47e5e1160b32
Nora Trapp <nora@signal.org>
2021-12-10 13:39:55 -0800

    Don't show experience upgrades when the get started banner is present

--- Signal/src/ViewControllers/HomeView/HomeViewController.m
+++ Signal/src/ViewControllers/HomeView/HomeViewController.m
@@ -447,7 +447,7 @@ NSString *const kArchiveButtonPseudoGroup = @"kArchiveButtonPseudoGroup";
 {
     [super viewDidAppear:animated];
 
-    if (![ExperienceUpgradeManager presentNextFromViewController:self] && !self.hasEverAppeared) {
+    if (!self.getStartedBanner && ![ExperienceUpgradeManager presentNextFromViewController:self] && !self.hasEverAppeared) {
         [OWSActionSheets showIOSUpgradeNagIfNecessary];
         [self presentGetStartedBannerIfNecessary];
     }
