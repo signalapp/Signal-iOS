c1763515afcb50a1a9c5692eb9dd8c4f8c60640d
Martin Böttcher <95238930+mboe-signal@users.noreply.github.com>
2022-02-17 19:59:46 +0100

    a theme change will keep the responder status of the textfield (#4013)

--- Signal/src/ViewControllers/ConversationView/ConversationViewController+BottomBar.swift
+++ Signal/src/ViewControllers/ConversationView/ConversationViewController+BottomBar.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -178,8 +178,12 @@ public extension ConversationViewController {
                                                 messageDraft: messageDraft,
                                                 voiceMemoDraft: voiceMemoDraft)
 
+        let hadFocus = self.inputToolbar?.isInputViewFirstResponder() ?? false
         self.inputToolbar = newInputToolbar
 
+        if hadFocus {
+            self.inputToolbar?.beginEditingMessage()
+        }
         newInputToolbar.updateFontSizes()
 
         updateBottomBar()
