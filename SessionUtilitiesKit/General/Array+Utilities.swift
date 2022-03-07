b6a6f77d4e93c339c4861c29ea3c3f3d1522c275
Morgan Pretty <morgan.t.pretty@gmail.com>
2022-03-08 10:45:58 +1100

    Adding back code removed due to merge conflict

--- SessionUtilitiesKit/General/Array+Utilities.swift
+++ SessionUtilitiesKit/General/Array+Utilities.swift
@@ -1,8 +1,24 @@
+import Foundation
 
-public extension Array where Element : CustomStringConvertible {
-
+public extension Array where Element: CustomStringConvertible {
     var prettifiedDescription: String {
         return "[ " + map { $0.description }.joined(separator: ", ") + " ]"
+    }
+}
+
+public extension Array {
+    func appending(_ other: Element) -> [Element] {
+        var updatedArray: [Element] = self
+        updatedArray.append(other)
+
+        return updatedArray
+    }
+    
+    func appending(_ other: [Element]) -> [Element] {
+        var updatedArray: [Element] = self
+        updatedArray.append(contentsOf: other)
+
+        return updatedArray
     }
 }
 
