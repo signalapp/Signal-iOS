2e10ccf99d6c81a2399f62e8fafd100ac3131c3b
Morgan Pretty <morgan.t.pretty@gmail.com>
2022-03-15 16:06:53 +1100

    Updated the code to cache the retrieval of the user public key

--- SessionMessagingKit/Utilities/General.swift
+++ SessionMessagingKit/Utilities/General.swift
@@ -1,7 +1,25 @@
+import Foundation
+
+public enum General {
+    public enum Cache {
+        public static var cachedEncodedPublicKey: String? = nil
+    }
+}
+
+@objc(SNGeneralUtilities)
+public class GeneralUtilities: NSObject {
+    @objc public static func getUserPublicKey() -> String {
+        return getUserHexEncodedPublicKey()
+    }
+}
 
 public func getUserHexEncodedPublicKey() -> String {
+    if let cachedKey: String = General.Cache.cachedEncodedPublicKey { return cachedKey }
+    
     if let keyPair = OWSIdentityManager.shared().identityKeyPair() { // Can be nil under some circumstances
+        General.Cache.cachedEncodedPublicKey = keyPair.hexEncodedPublicKey
         return keyPair.hexEncodedPublicKey
     }
+    
     return ""
 }
