3cfa7fa90fb28667d401974aef53ae5a572e0a3d
Nora Trapp <nora@signal.org>
2022-03-17 10:39:29 -0700

    Style the tab bar to match the nav bar

--- Signal/src/ViewControllers/HomeView/HomeTabBarController.swift
+++ Signal/src/ViewControllers/HomeView/HomeTabBarController.swift
@@ -37,6 +37,9 @@ class HomeTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Use our custom tab bar.
+        setValue(OWSTabBar(), forKey: "tabBar")
+
         delegate = self
 
         // Don't render the tab bar if stories isn't enabled.
@@ -122,5 +125,159 @@ extension HomeTabBarController: UITabBarControllerDelegate {
         }
 
         return true
+    }
+}
+
+@objc
+public class OWSTabBar: UITabBar {
+
+    @objc
+    public var fullWidth: CGFloat {
+        return superview?.frame.width ?? .zero
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        notImplemented()
+    }
+
+    @objc
+    public static let backgroundBlurMutingFactor: CGFloat = 0.5
+    var blurEffectView: UIVisualEffectView?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        applyTheme()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(themeDidChange),
+                                               name: .ThemeDidChange,
+                                               object: nil)
+    }
+
+    // MARK: Theme
+
+    var tabBarBackgroundColor: UIColor {
+        switch currentStyle {
+        case .secondaryBar: return Theme.secondaryBackgroundColor
+        default: return Theme.navbarBackgroundColor
+        }
+    }
+
+    private func applyTheme() {
+        guard respectsTheme else {
+            return
+        }
+
+        if currentStyle == .secondaryBar {
+            barTintColor = tabBarBackgroundColor
+            self.backgroundImage = UIImage(color: tabBarBackgroundColor)
+        } else if UIAccessibility.isReduceTransparencyEnabled {
+            blurEffectView?.isHidden = true
+            self.backgroundImage = UIImage(color: tabBarBackgroundColor)
+        } else {
+            let blurEffect = Theme.barBlurEffect
+
+            let blurEffectView: UIVisualEffectView = {
+                if let existingBlurEffectView = self.blurEffectView {
+                    existingBlurEffectView.isHidden = false
+                    return existingBlurEffectView
+                }
+
+                let blurEffectView = UIVisualEffectView()
+                blurEffectView.isUserInteractionEnabled = false
+
+                self.blurEffectView = blurEffectView
+                self.insertSubview(blurEffectView, at: 0)
+                blurEffectView.autoPinEdgesToSuperviewEdges()
+
+                return blurEffectView
+            }()
+
+            blurEffectView.effect = blurEffect
+
+            // remove hairline below bar.
+            self.shadowImage = UIImage()
+
+            // Alter the visual effect view's tint to match our background color
+            // so the tabbar, when over a solid color background matching tabBarBackgroundColor,
+            // exactly matches the background color. This is brittle, but there is no way to get
+            // this behavior from UIVisualEffectView otherwise.
+            if let tintingView = blurEffectView.subviews.first(where: {
+                String(describing: type(of: $0)) == "_UIVisualEffectSubview"
+            }) {
+                tintingView.backgroundColor = tabBarBackgroundColor.withAlphaComponent(OWSNavigationBar.backgroundBlurMutingFactor)
+                self.backgroundImage = UIImage()
+            } else {
+                if #available(iOS 16, *) { owsFailDebug("Check if this still works on new iOS version.") }
+
+                owsFailDebug("Unexpectedly missing visual effect subview")
+                // If we can't find the tinting subview (e.g. a new iOS version changed the behavior)
+                // We'll make the tabBar more translucent by setting a background color.
+                let color = tabBarBackgroundColor.withAlphaComponent(OWSNavigationBar.backgroundBlurMutingFactor)
+                self.backgroundImage = UIImage(color: color)
+            }
+        }
+    }
+
+    @objc
+    public func themeDidChange() {
+        applyTheme()
+    }
+
+    @objc
+    public var respectsTheme: Bool = true {
+        didSet {
+            themeDidChange()
+        }
+    }
+
+    // MARK: Override Theme
+
+    @objc
+    public enum TabBarStyle: Int {
+        case `default`, secondaryBar
+    }
+
+    private var currentStyle: TabBarStyle?
+
+    @objc
+    public func switchToStyle(_ style: TabBarStyle, animated: Bool = false) {
+        AssertIsOnMainThread()
+
+        guard currentStyle != style else { return }
+
+        if animated {
+            let animation = CATransition()
+            animation.duration = 0.35
+            animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+            animation.type = .fade
+            layer.add(animation, forKey: "ows_fade")
+        } else {
+            layer.removeAnimation(forKey: "ows_fade")
+        }
+
+        func applySecondaryBarOverride() {
+            self.blurEffectView?.isHidden = true
+            self.shadowImage = UIImage()
+        }
+
+        func removeSecondaryBarOverride() {
+            self.blurEffectView?.isHidden = false
+            self.shadowImage = nil
+        }
+
+        currentStyle = style
+
+        switch style {
+        case .default:
+            respectsTheme = true
+            removeSecondaryBarOverride()
+            applyTheme()
+        case .secondaryBar:
+            respectsTheme = true
+            applySecondaryBarOverride()
+            applyTheme()
+        }
     }
 }
