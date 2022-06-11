f1aa92e54985674802f241db4ef30a324fe1496b
mauriciomaniglia <manigliamauricio@gmail.com>
2022-06-11 13:48:29 -0300

    Test `onboardingSplashDidComplete` shows onboarding permission screen.

---
+++ Signal/test/ViewControllers/OnboardingControllerTest.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
+//
+
+import XCTest
+import UIKit
+@testable import Signal
+
+class OnboardingControllerTest: SignalBaseTest {
+
+    func test_onboardingSplashDidComplete_showsOnboardingPermissionsScreen() {
+        let sut = OnboardingController(onboardingMode: .registering)
+        let viewController = UIViewController()
+        let navigationController = UINavigationControllerSpy(rootViewController: viewController)
+
+        sut.onboardingSplashDidComplete(viewController: viewController)
+
+        XCTAssertNotNil(navigationController.currentViewController as? OnboardingPermissionsViewController)
+    }
+}
+
+// MARK: - Helpers
+
+class UINavigationControllerSpy: UINavigationController {
+    var currentViewController: UIViewController?
+
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        super.pushViewController(viewController, animated: false)
+        currentViewController = viewController
+    }
+}
