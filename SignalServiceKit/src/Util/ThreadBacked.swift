951f5c3708d3224a0768b6e6d1b261ab6196ec4b
Evan Hahn <evanhahn@signal.org>
2022-07-07 16:21:22 -0500

    Remove trailing whitespace
    This is a whitespace-only change that fixes a SwiftLint violation.

--- SignalServiceKit/src/Util/ThreadBacked.swift
+++ SignalServiceKit/src/Util/ThreadBacked.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -9,12 +9,12 @@ import Foundation
 public struct ThreadBacked<Value> {
     private let key: String
     private let defaultValue: Value
-    
+
     public init(key: String, defaultValue: Value) {
         self.key = key
         self.defaultValue = defaultValue
     }
-    
+
     public var wrappedValue: Value {
         get {
             guard let value = Thread.current.threadDictionary[key] else {
