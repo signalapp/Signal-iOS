signalapp/Signal-iOS

commit 16daabdd2eaecef25bdb86393b7108f8df678d89

Use new-style KVO input accessory view placeholder
_I recommend reviewing this with whitespace changes disabled._

This fixes one violation of [SwiftLint's `block_based_kvo` rule][0].

[0]: https://realm.github.io/SwiftLint/block_based_kvo.html

SignalUI/Views/InputAccessoryViewPlaceholder.swift
@@ -162,37 +162,30 @@ public class InputAccessoryViewPlaceholder: UIView {
         return .zero
     }
 
+    private var superviewCenterObserver: NSKeyValueObservation?
+
     public override func willMove(toSuperview newSuperview: UIView?) {
         super.willMove(toSuperview: newSuperview)
 
-        // By observing the "center" property of our superview, we can
+        // By observing the .center property of our superview, we can
         // follow along as the keyboard moves up and down.
-        superview?.removeObserver(self, forKeyPath: "center")
-        newSuperview?.addObserver(self, forKeyPath: "center", options: [.initial, .new], context: nil)
-    }
+        superviewCenterObserver?.invalidate()
+        superviewCenterObserver = superview?.observe(\.center, options: [.initial, .new]) { [weak self] (_, _) in
+            guard let self = self else { return }
 
-    deinit {
-        superview?.removeObserver(self, forKeyPath: "center")
-    }
+            // Do nothing unless the keyboard is currently presented.
+            // We're only checking for interactive dismissal, which
+            // can only happen while presented.
+            guard case .presented = self.keyboardState else { return }
 
-    public override func observeValue(
-        forKeyPath keyPath: String?,
-        of object: Any?,
-        change: [NSKeyValueChangeKey: Any]?,
-        context: UnsafeMutableRawPointer?
-    ) {
-        // Do nothing unless the keyboard is currently presented.
-        // We're only checking for interactive dismissal, which
-        // can only happen while presented.
-        guard case .presented = keyboardState else { return }
+            guard self.superview != nil else { return }
 
-        guard superview != nil else { return }
-
-        // While the visible keyboard height is greater than zero,
-        // and the keyboard is presented, we can safely assume
-        // an interactive dismissal is in progress.
-        if visibleKeyboardHeight > 0 {
-            delegate?.inputAccessoryPlaceholderKeyboardIsDismissingInteractively()
+            // While the visible keyboard height is greater than zero,
+            // and the keyboard is presented, we can safely assume
+            // an interactive dismissal is in progress.
+            if self.visibleKeyboardHeight > 0 {
+                self.delegate?.inputAccessoryPlaceholderKeyboardIsDismissingInteractively()
+            }
         }
     }
 

commit d0fbb47c25e95310c173f9f476416020b2b5bbe4

Fix input keyboard regression
I introduced a bug in 16daabdd2eaecef25bdb86393b7108f8df678d89.

In English: I accidentally added an observer to the _old_ superview
instead of the _new_ one.

In pseudocode: here's a diff of what went wrong:

     remove_observer_from(old_superview)
    -add_observer_to(new_superview)
    +add_observer_to(old_superview)

SignalUI/Views/InputAccessoryViewPlaceholder.swift
@@ -170,7 +170,7 @@ public class InputAccessoryViewPlaceholder: UIView {
         // By observing the .center property of our superview, we can
         // follow along as the keyboard moves up and down.
         superviewCenterObserver?.invalidate()
-        superviewCenterObserver = superview?.observe(\.center, options: [.initial, .new]) { [weak self] (_, _) in
+        superviewCenterObserver = newSuperview?.observe(\.center, options: [.initial, .new]) { [weak self] (_, _) in
             guard let self = self else { return }
 
             // Do nothing unless the keyboard is currently presented.