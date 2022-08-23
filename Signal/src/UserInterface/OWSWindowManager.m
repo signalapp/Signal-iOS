signalapp/Signal-iOS

commit 856ced4062b0b93cd6129922a3237b0585151bbb

Use supportedInterfaceOrientations to force calls to portrait
Previously this used UIDevice.ows_setOrientation, an undocumented hack
to basically lie to the device about which way is up. This sounds
sketchy, but it's the result of a lot of testing various solutions for
something that could reliably force the app into portrait orientation.

However, every UIWindow has its own "supported orientations", provided
by the root view controller. And calls are implemented in their own
UIWindow (so that they don't disrupt whatever you were doing). So for
*calls specifically*, we can use supportedInterfaceOrientations to
force the app into portrait mode.

All other callers of UIDevice.ows_setOrientation work in the shared
main window, so they'll be left alone. This only changes things for
the call window.

Signal/src/UserInterface/OWSWindowManager.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 #import "OWSWindowManager.h"
@@ -53,19 +53,19 @@ const UIWindowLevel UIWindowLevel_ScreenBlocking(void)
 
 #pragma mark -
 
-@interface OWSWindowRootNavigationViewController : UINavigationController
+@interface OWSCallWindowRootNavigationViewController : UINavigationController
 
 @end
 
 #pragma mark -
 
-@implementation OWSWindowRootNavigationViewController : UINavigationController
+@implementation OWSCallWindowRootNavigationViewController : UINavigationController
 
 #pragma mark - Orientation
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
-    return UIDevice.currentDevice.defaultSupportedOrientations;
+    return UIDevice.currentDevice.isIPad ? UIInterfaceOrientationMaskAll : UIInterfaceOrientationMaskPortrait;
 }
 
 @end
@@ -167,8 +167,8 @@ const UIWindowLevel UIWindowLevel_ScreenBlocking(void)
     // It adjusts the size of the navigation bar to reflect the
     // call window.  We don't want those adjustments made within
     // the call window itself.
-    OWSWindowRootNavigationViewController *navigationController =
-        [[OWSWindowRootNavigationViewController alloc] initWithRootViewController:viewController];
+    OWSCallWindowRootNavigationViewController *navigationController =
+        [[OWSCallWindowRootNavigationViewController alloc] initWithRootViewController:viewController];
     navigationController.navigationBarHidden = YES;
     OWSAssertDebug(!self.callNavigationController);
     self.callNavigationController = navigationController;
@@ -238,11 +238,7 @@ const UIWindowLevel UIWindowLevel_ScreenBlocking(void)
     [self.callNavigationController popToRootViewControllerAnimated:NO];
     [self.callNavigationController pushViewController:callViewController animated:NO];
     self.shouldShowCallView = YES;
-    // CallViewController only supports portrait for iPhones, but if we're _already_ landscape it won't
-    // automatically switch.
-    if (!UIDevice.currentDevice.isIPad) {
-        [UIDevice.currentDevice ows_setOrientation:UIDeviceOrientationPortrait];
-    }
+
     [self ensureWindowState];
 }
 

commit 41b381b1b1748fffd278a18c10f366e11353de8b

When starting a call, attemptRotationToDeviceOrientation()
Speculative fix for a hard-to-reproduce issue where the call view ends
up laid out in landscape and then clipped to portrait, rather than
being forced to portrait in the first place.

Possibly fallout from 856ced4062, which replaced an explicit call to
our UIDevice.ows_setOrientation(). We don't want to revert to that
because it messes up legitimate orientation detection.

Signal/src/UserInterface/OWSWindowManager.m
@@ -241,6 +241,11 @@ const UIWindowLevel UIWindowLevel_ScreenBlocking(void)
     self.shouldShowCallView = YES;
 
     [self ensureWindowState];
+    // This shouldn't be necessary, but in practice we've had at least one user end up with the
+    // call view laid out in landscape and then clipped to portrait.
+    // An earlier version of this method used UIDevice.ows_setOrientation();
+    // we don't want to revert to that because it messes up legitimate orientation detection.
+    [UIViewController attemptRotationToDeviceOrientation];
 }
 
 - (void)endCall:(UIViewController<CallViewControllerWindowReference> *)callViewController