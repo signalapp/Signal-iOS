a6b395b11fbbc1394538010ed0a03e42956910a4
Evan Hahn <evanhahn@signal.org>
2022-08-30 17:02:50 -0500

    Remove long-skipped phone number tests for Mexico
    This change should have no user impact.
    
    This removes a test that's been skipped since 2020 (see
    5c976b0acd3a277c2fea330e05b0749111f6db6d). I think this is okay given
    that (1) we've skipped it for over 2 years (2) [we already have some
    tests for Mexican phone numbers][0].
    
    [0]: https://github.com/signalapp/Signal-iOS/blob/895c1b75762ee60374c10bbca942beec59715256/SignalServiceKit/tests/Contacts/PhoneNumberTest.swift#L10-L25

--- SignalServiceKit/tests/Contacts/PhoneNumberTest.m
+++ SignalServiceKit/tests/Contacts/PhoneNumberTest.m
@@ -114,36 +114,6 @@
     XCTAssertEqualObjects(parsed.firstObject, @"+13235551234");
 }
 
-#ifdef BROKEN_TESTS
-
-- (void)testTryParsePhoneNumbersFromsUserSpecifiedText_Mexico1
-{
-    NSArray<NSString *> *parsed =
-        [self unpackTryParsePhoneNumbersFromsUserSpecifiedText:@"528341639157" clientPhoneNumber:@"+528341639144"];
-    XCTAssertTrue(parsed.count >= 1);
-    XCTAssertTrue([parsed containsObject:@"+528341639157"]);
-
-    parsed = [self unpackTryParsePhoneNumbersFromsUserSpecifiedText:@"8341639157" clientPhoneNumber:@"+528341639144"];
-    XCTAssertTrue(parsed.count >= 1);
-    XCTAssertTrue([parsed containsObject:@"+528341639157"]);
-
-    //    parsed = [self unpackTryParsePhoneNumbersFromsUserSpecifiedText:@"341639157"
-    //    clientPhoneNumber:@"+528341639144"]; XCTAssertTrue(parsed.count >= 1);
-    //    // The parsing logic should try adding Mexico's national prefix for cell numbers "1"
-    //    // after the country code.
-    //    XCTAssertTrue([parsed containsObject:@"+521341639157"]);
-
-    parsed = [self unpackTryParsePhoneNumbersFromsUserSpecifiedText:@"528341639157" clientPhoneNumber:@"+13213214321"];
-    XCTAssertTrue(parsed.count >= 1);
-    XCTAssertTrue([parsed containsObject:@"+528341639157"]);
-
-    parsed = [self unpackTryParsePhoneNumbersFromsUserSpecifiedText:@"13235551234" clientPhoneNumber:@"+528341639144"];
-    XCTAssertTrue(parsed.count >= 1);
-    XCTAssertTrue([parsed containsObject:@"+13235551234"]);
-}
-
-#endif
-
 - (void)testMissingAreaCode_USA
 {
     // Add area code to numbers that look like "local" numbers
