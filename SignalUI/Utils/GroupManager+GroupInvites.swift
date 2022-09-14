6aaf11b5d8f0ec4915ea31ca0f4e84ed7cb4b97b
Sasha Weiss <105739406+sashaweiss-signal@users.noreply.github.com>
2022-09-14 11:53:41 -0700

    Update indentation in GroupManager+GroupInvites

--- SignalUI/Utils/GroupManager+GroupInvites.swift
+++ SignalUI/Utils/GroupManager+GroupInvites.swift
@@ -7,17 +7,22 @@ import Foundation
 @objc
 public extension GroupManager {
 
-    static func leaveGroupOrDeclineInviteAsyncWithUI(groupThread: TSGroupThread,
-                                                     fromViewController: UIViewController,
-                                                     replacementAdminUuid: UUID? = nil,
-                                                     success: (() -> Void)?) {
+    static func leaveGroupOrDeclineInviteAsyncWithUI(
+        groupThread: TSGroupThread,
+        fromViewController: UIViewController,
+        replacementAdminUuid: UUID? = nil,
+        success: (() -> Void)?
+    ) {
 
         guard groupThread.isLocalUserMemberOfAnyKind else {
             owsFailDebug("unexpectedly trying to leave group for which we're not a member.")
             return
         }
 
-        ModalActivityIndicatorViewController.present(fromViewController: fromViewController, canCancel: false) { modalView in
+        ModalActivityIndicatorViewController.present(
+            fromViewController: fromViewController,
+            canCancel: false
+        ) { modalView in
             firstly(on: .global()) {
                 databaseStorage.write { transaction in
                     self.localLeaveGroupOrDeclineInvite(
@@ -34,40 +39,51 @@ public extension GroupManager {
             }.catch { error in
                 owsFailDebug("Leave group failed: \(error)")
                 modalView.dismiss {
-                    OWSActionSheets.showActionSheet(title: OWSLocalizedString("LEAVE_GROUP_FAILED",
-                                                                             comment: "Error indicating that a group could not be left."))
+                    OWSActionSheets.showActionSheet(
+                        title: OWSLocalizedString(
+                            "LEAVE_GROUP_FAILED",
+                            comment: "Error indicating that a group could not be left."
+                        )
+                    )
                 }
             }
         }
     }
 
-    static func acceptGroupInviteAsync(_ groupThread: TSGroupThread,
-                                       fromViewController: UIViewController,
-                                       success: @escaping () -> Void) {
-        ModalActivityIndicatorViewController.present(fromViewController: fromViewController,
-                                                     canCancel: false) { modalActivityIndicator in
-                                                        firstly(on: .global()) { () -> Promise<TSGroupThread> in
-                                                            guard let groupModelV2 = groupThread.groupModel as? TSGroupModelV2 else {
-                                                                throw OWSAssertionError("Invalid group model")
-                                                            }
+    static func acceptGroupInviteAsync(
+        _ groupThread: TSGroupThread,
+        fromViewController: UIViewController,
+        success: @escaping () -> Void
+    ) {
+        ModalActivityIndicatorViewController.present(
+            fromViewController: fromViewController,
+            canCancel: false
+        ) { modalActivityIndicator in
+            firstly(on: .global()) { () -> Promise<TSGroupThread> in
+                guard let groupModelV2 = groupThread.groupModel as? TSGroupModelV2 else {
+                    throw OWSAssertionError("Invalid group model")
+                }
 
-                                                            return self.localAcceptInviteToGroupV2(
-                                                                groupModel: groupModelV2,
-                                                                waitForMessageProcessing: true
-                                                            )
-                                                        }.done(on: .main) { _ in
-                                                            modalActivityIndicator.dismiss {
-                                                                success()
-                                                            }
-                                                        }.catch { error in
-                                                            owsFailDebug("Error: \(error)")
+                return self.localAcceptInviteToGroupV2(
+                    groupModel: groupModelV2,
+                    waitForMessageProcessing: true
+                )
+            }.done(on: .main) { _ in
+                modalActivityIndicator.dismiss {
+                    success()
+                }
+            }.catch { error in
+                owsFailDebug("Error: \(error)")
 
-                                                            modalActivityIndicator.dismiss {
-                                                                let title = OWSLocalizedString("GROUPS_INVITE_ACCEPT_INVITE_FAILED",
-                                                                                              comment: "Error indicating that an error occurred while accepting an invite.")
-                                                                OWSActionSheets.showActionSheet(title: title)
-                                                            }
-                                                        }
+                modalActivityIndicator.dismiss {
+                    let title = OWSLocalizedString(
+                        "GROUPS_INVITE_ACCEPT_INVITE_FAILED",
+                        comment: "Error indicating that an error occurred while accepting an invite."
+                    )
+
+                    OWSActionSheets.showActionSheet(title: title)
+                }
+            }
         }
     }
 }
