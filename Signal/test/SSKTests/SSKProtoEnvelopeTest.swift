abbbad7f90c5f5bb90359d68f50cfb48c64d6c72
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2022-09-27 16:54:59 -0500

    Remove useless setup/teardown from test

--- Signal/test/SSKTests/SSKProtoEnvelopeTest.swift
+++ Signal/test/SSKTests/SSKProtoEnvelopeTest.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2020 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import XCTest
@@ -8,17 +8,6 @@ import XCTest
 import SwiftProtobuf
 
 class SSKProtoEnvelopeTest: SignalBaseTest {
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testParse_EmptyData() {
         let data = Data()
         XCTAssertThrowsError(try SSKProtoEnvelope(serializedData: data))
