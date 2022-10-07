f5c3ac02e95d7ebfac5342e7ed325183a0d85868
Max Radermacher <max@signal.org>
2022-10-07 10:22:38 -0700

    Remove dead code during system contact sync

--- SignalMessaging/contacts/SystemContactsFetcher.swift
+++ SignalMessaging/contacts/SystemContactsFetcher.swift
@@ -392,18 +392,12 @@ public class SystemContactsFetcher: NSObject {
 
             Logger.info("fetching contacts")
 
-            var fetchedContacts: [Contact]?
+            let contacts: [Contact]
             switch self.contactStoreAdapter.fetchContacts() {
             case .success(let result):
-                fetchedContacts = result
+                contacts = result
             case .error(let error):
                 completion(error)
-                return
-            }
-
-            guard let contacts = fetchedContacts else {
-                owsFailDebug("contacts was unexpectedly not set.")
-                completion(nil)
                 return
             }
 
