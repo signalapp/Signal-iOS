f6d1c2484eee0da447d85068623e2b8b07d6a54f
Evan Hahn <evanhahn@signal.org>
2022-12-04 12:39:53 -0600

    Test more of `StickerPackInfo`
    This is a test-only change that should have no user impact.

--- SignalServiceKit/tests/Messages/Stickers/StickerPackInfoTest.swift
+++ SignalServiceKit/tests/Messages/Stickers/StickerPackInfoTest.swift
@@ -7,6 +7,58 @@ import XCTest
 @testable import SignalServiceKit
 
 class StickerPackInfoTest: XCTestCase {
+    func testParsePackHex() {
+        let validPackIdHex = "01020304"
+        let validPackKeyHex = "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"
+        let tooShortPackKey = "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+        let tooLongPackKey = validPackKeyHex + "21"
+
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(nil, packKeyHex: nil))
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(nil, packKeyHex: validPackKeyHex))
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(validPackIdHex, packKeyHex: nil))
+
+        XCTAssertNil(StickerPackInfo.parsePackIdHex("", packKeyHex: ""))
+        XCTAssertNil(StickerPackInfo.parsePackIdHex("", packKeyHex: validPackKeyHex))
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(validPackIdHex, packKeyHex: ""))
+
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(validPackIdHex, packKeyHex: tooShortPackKey))
+        XCTAssertNil(StickerPackInfo.parsePackIdHex(validPackIdHex, packKeyHex: tooLongPackKey))
+
+        let packInfo = StickerPackInfo.parsePackIdHex(validPackIdHex, packKeyHex: validPackKeyHex)!
+        XCTAssertEqual(packInfo.packId, .init([1, 2, 3, 4]))
+        XCTAssertEqual(packInfo.packKey, .init((1...32)))
+    }
+
+    func testParsePack() {
+        let validPackId = Data([1, 2, 3, 4])
+        let validPackKey = Data((1...32))
+        let tooShortPackKey = Data((1...31))
+        let tooLongPackKey = Data((1...33))
+
+        XCTAssertNil(StickerPackInfo.parse(packId: nil, packKey: nil))
+        XCTAssertNil(StickerPackInfo.parse(packId: nil, packKey: validPackKey))
+        XCTAssertNil(StickerPackInfo.parse(packId: validPackId, packKey: nil))
+
+        XCTAssertNil(StickerPackInfo.parse(packId: Data(), packKey: Data()))
+        XCTAssertNil(StickerPackInfo.parse(packId: Data(), packKey: validPackKey))
+        XCTAssertNil(StickerPackInfo.parse(packId: validPackId, packKey: Data()))
+
+        XCTAssertNil(StickerPackInfo.parse(packId: validPackId, packKey: tooShortPackKey))
+        XCTAssertNil(StickerPackInfo.parse(packId: validPackId, packKey: tooLongPackKey))
+
+        let packInfo = StickerPackInfo.parse(packId: validPackId, packKey: validPackKey)!
+        XCTAssertEqual(packInfo.packId, .init([1, 2, 3, 4]))
+        XCTAssertEqual(packInfo.packKey, .init((1...32)))
+    }
+
+    func testShareUrl() {
+        let packInfo = StickerPackInfo(packId: .init([1, 2, 3, 4]), packKey: .init((1...32)))
+        XCTAssertEqual(
+            packInfo.shareUrl(),
+            "https://signal.art/addstickers/#pack_id=01020304&pack_key=0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"
+        )
+    }
+
     func testIsStickerPackShareUrl() throws {
         let validStrings = [
             "https://signal.art/addstickers#pack_id=abc&pack_key=def",
@@ -39,6 +91,37 @@ class StickerPackInfoTest: XCTestCase {
         for string in invalidStrings {
             let url = try XCTUnwrap(URL(string: string))
             XCTAssertFalse(StickerPackInfo.isStickerPackShare(url), string)
+        }
+    }
+
+    func testParseStickerPackShareUrl() throws {
+        let packIdHex = "01020304"
+        let packKeyHex = "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"
+
+        let invalidUrlStrings = [
+            "https://signal.art/addstickers/",
+            "https://signal.art/addstickers/#pack_id=&pack_key=\(packKeyHex)",
+            "https://signal.art/addstickers/#pack_id=\(packIdHex)&pack_key=",
+            "https://signal.art/addstickers/#pack_id=\(packIdHex)&pack_key=\(packKeyHex)ff",
+            "https://signal.art/addstickers/#pack_id=\(packIdHex)",
+            "https://signal.art/addstickers/#pack_key=\(packKeyHex)"
+        ]
+        for urlString in invalidUrlStrings {
+            let url = try XCTUnwrap(URL(string: urlString))
+            XCTAssertNil(StickerPackInfo.parseStickerPackShare(url))
+        }
+
+        let validUrlStrings = [
+            "https://signal.art/addstickers/#pack_id=\(packIdHex)&pack_key=\(packKeyHex)",
+            "https://signal.art/addstickers/#pack_key=\(packKeyHex)&pack_id=\(packIdHex)",
+            "https://signal.art/addstickers/#pack_id=\(packIdHex)&pack_key=\(packKeyHex)&extra=param",
+            "https://signal.art/addstickers/#pack_id=ignored&pack_key=ignored&pack_id=\(packIdHex)&pack_key=\(packKeyHex)"
+        ]
+        for urlString in validUrlStrings {
+            let url = try XCTUnwrap(URL(string: urlString))
+            let packInfo = try XCTUnwrap(StickerPackInfo.parseStickerPackShare(url))
+            XCTAssertEqual(packInfo.packId, .init([1, 2, 3, 4]))
+            XCTAssertEqual(packInfo.packKey, .init((1...32)))
         }
     }
 }
