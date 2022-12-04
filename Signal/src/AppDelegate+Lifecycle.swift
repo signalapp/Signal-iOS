955fad9f14d0c0a3d7ac19f30f8c048e11472af5
Evan Hahn <evanhahn@signal.org>
2022-12-04 11:47:03 -0600

    Swiftify "did become active" and "will resign active" methods
    This change should have no user impact.
    
    This converts `AppDelegate`'s `applicationDidBecomeActive` and
    `applicationWillResignActive` methods to Swift. I also had to move a
    couple of helper methods.
    
    I tried to make no changes to their behavior.

---
+++ Signal/src/AppDelegate+Lifecycle.swift
@@ -0,0 +1,125 @@
+//
+// Copyright 2022 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+import Foundation
+import WebRTC
+import SignalServiceKit
+
+extension AppDelegate {
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AssertIsOnMainThread()
+
+        if didAppLaunchFail {
+            owsFailDebug("app launch failed")
+            return
+        }
+
+        Logger.warn("applicationDidBecomeActive.")
+        if CurrentAppContext().isRunningTests {
+            return
+        }
+
+        AppReadiness.runNowOrWhenAppDidBecomeReadySync { self.handleActivation() }
+
+        // Clear all notifications whenever we become active.
+        // When opening the app from a notification,
+        // AppDelegate.didReceiveLocalNotification will always
+        // be called _before_ we become active.
+        clearAllNotificationsAndRestoreBadgeCount()
+
+        // On every activation, clear old temp directories.
+        ClearOldTemporaryDirectories()
+
+        // Ensure that all windows have the correct frame.
+        windowManager.updateWindowFrames()
+
+        Logger.info("applicationDidBecomeActive completed.")
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        AssertIsOnMainThread()
+
+        if didAppLaunchFail {
+            owsFailDebug("app launch failed")
+            return
+        }
+
+        Logger.warn("applicationWillResignActive.")
+
+        clearAllNotificationsAndRestoreBadgeCount()
+
+        Logger.flush()
+    }
+
+    private static var hasActivated = false
+
+    private func handleActivation() {
+        AssertIsOnMainThread()
+
+        Logger.warn("handleActivation.")
+
+        // Always check prekeys after app launches, and sometimes check on app activation.
+        TSPreKeyManager.checkPreKeysIfNecessary()
+
+        if !Self.hasActivated {
+            Self.hasActivated = true
+
+            RTCInitializeSSL()
+
+            if tsAccountManager.isRegistered {
+                // At this point, potentially lengthy DB locking migrations could be running.
+                // Avoid blocking app launch by putting all further possible DB access in async block
+                DispatchQueue.global(qos: .default).async {
+                    let localAddress = self.tsAccountManager.localAddress
+                    Logger.info("running post launch block for registered user: \(String(describing: localAddress))")
+
+                    // Clean up any messages that expired since last launch immediately
+                    // and continue cleaning in the background.
+                    self.disappearingMessagesJob.startIfNecessary()
+
+                    self.enableBackgroundRefreshIfNecessary()
+                }
+            } else {
+                Logger.info("running post launch block for unregistered user.")
+
+                // Unregistered user should have no unread messages. e.g. if you delete your account.
+                AppEnvironment.shared.notificationPresenter.clearAllNotifications()
+            }
+        }
+
+        // Every time we become active...
+        if tsAccountManager.isRegistered {
+            // At this point, potentially lengthy DB locking migrations could be running.
+            // Avoid blocking app launch by putting all further possible DB access in async block
+            DispatchQueue.main.async {
+                AppEnvironment.shared.contactsManagerImpl.fetchSystemContactsOnceIfAlreadyAuthorized()
+
+                // TODO: Should we run this immediately even if we would like to process
+                // already decrypted envelopes handed to us by the NSE?
+                self.messageFetcherJob.run()
+
+                if !UIApplication.shared.isRegisteredForRemoteNotifications {
+                    Logger.info("Retrying to register for remote notifications since user hasn't registered yet.")
+                    // Push tokens don't normally change while the app is launched, so checking once during launch is
+                    // usually sufficient, but e.g. on iOS11, users who have disabled "Allow Notifications" and disabled
+                    // "Background App Refresh" will not be able to obtain an APN token. Enabling those settings does not
+                    // restart the app, so we check every activation for users who haven't yet registered.
+                    SyncPushTokensJob.run()
+                }
+            }
+        }
+
+        Logger.info("handleActivation completed.")
+    }
+
+    private func clearAllNotificationsAndRestoreBadgeCount() {
+        AssertIsOnMainThread()
+
+        AppReadiness.runNowOrWhenAppDidBecomeReadySync {
+            AppEnvironment.shared.notificationPresenter.clearAllNotifications()
+            self.messageManager.updateApplicationBadgeCount()
+        }
+    }
+}
