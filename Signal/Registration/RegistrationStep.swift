signalapp/Signal-iOS

commit 4bf5f44be125b5fe82252f8e14c99e6d9a0f1c92

Rename enum case in new registration
This change should have no user impact. All it does is rename
`.pinAttemptsExhaustedAndMustCreateNewPin` to
`.pinAttemptsExhaustedWithoutReglock`.

Signal/Registration/RegistrationStep.swift
@@ -39,7 +39,7 @@ public enum RegistrationStep: Equatable {
 
     /// All PIN attempts have been exhausted. The user may still be able to register,
     /// but they cannot recover their kbs backups.
-    case pinAttemptsExhaustedAndMustCreateNewPin(RegistrationPinAttemptsExhaustedViewState)
+    case pinAttemptsExhaustedWithoutReglock(RegistrationPinAttemptsExhaustedViewState)
 
     /// At _any_ point during session-based registration, a captcha challenge may be
     /// requested.

Signal/Registration/RegistrationCoordinatorImpl.swift
@@ -296,7 +296,7 @@ public class RegistrationCoordinatorImpl: RegistrationCoordinator {
                     inMemoryState.pinFromUser = nil
                     inMemoryState.pinFromDisk = nil
                     self.wipeInMemoryStateToPreventKBSPathAttempts()
-                    return .value(.pinAttemptsExhaustedAndMustCreateNewPin(
+                    return .value(.pinAttemptsExhaustedWithoutReglock(
                         .init(mode: .restoringRegistrationRecoveryPassword)
                     ))
                 } else {
@@ -1335,7 +1335,7 @@ public class RegistrationCoordinatorImpl: RegistrationCoordinator {
                             $0.hasGivenUpTryingToRestoreWithKBS = true
                         }
                     }
-                    return .value(.pinAttemptsExhaustedAndMustCreateNewPin(
+                    return .value(.pinAttemptsExhaustedWithoutReglock(
                         .init(mode: .restoringRegistrationRecoveryPassword)
                     ))
                 case .networkError:
@@ -2743,7 +2743,7 @@ public class RegistrationCoordinatorImpl: RegistrationCoordinator {
                     self.db.write { tx in
                         self.updatePersistedState(tx) { $0.hasGivenUpTryingToRestoreWithKBS = true }
                     }
-                    return .value(.pinAttemptsExhaustedAndMustCreateNewPin(
+                    return .value(.pinAttemptsExhaustedWithoutReglock(
                         .init(mode: .restoringBackup)
                     ))
                 case .networkError:

Signal/Registration/ViewControllers/RegistrationNavigationController.swift
@@ -248,7 +248,7 @@ public class RegistrationNavigationController: OWSNavigationController {
                     }
                 }
             )
-        case .pinAttemptsExhaustedAndMustCreateNewPin(let state):
+        case .pinAttemptsExhaustedWithoutReglock(let state):
             return Controller(
                 type: RegistrationPinAttemptsExhaustedAndMustCreateNewPinViewController.self,
                 make: { presenter in

commit 16d642fb8383760e17ee7621f48c4b5884610867

Fix compile error with registration enum
I accidentally introduced this in
4bf5f44be125b5fe82252f8e14c99e6d9a0f1c92.

Signal/Registration/RegistrationStep.swift
@@ -113,7 +113,7 @@ public enum RegistrationStep: Equatable {
         case .verificationCodeEntry: return "verificationCodeEntry"
         case .transferSelection: return "transferSelection"
         case .pinEntry: return "pinEntry"
-        case .pinAttemptsExhaustedAndMustCreateNewPin: return "pinAttemptsExhaustedAndMustCreateNewPin"
+        case .pinAttemptsExhaustedWithoutReglock: return "pinAttemptsExhaustedWithoutReglock"
         case .captchaChallenge: return "captchaChallenge"
         case .reglockTimeout: return "reglockTimeout"
         case .phoneNumberDiscoverability: return "phoneNumberDiscoverability"