557718a81049067eb31b6d5247d49106143a7de8
Evan Hahn <evanhahn@signal.org>
2023-04-21 15:55:01 -0500

    Remove unused string property
    This removes `NSString`'s `dominantLanguageWithLegacyLinguisticTagger`.
    Its last usage was removed in 57a810658abc43485cca6941a188a2f93e039758,
    back when we dropped iOS 10.

--- SignalServiceKit/src/Util/NSString+SSK.h
+++ SignalServiceKit/src/Util/NSString+SSK.h
@@ -11,8 +11,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSString (SSK)<SSKMaybeString>
 
-@property (nonatomic, nullable, readonly) NSString *dominantLanguageWithLegacyLinguisticTagger;
-
 - (NSString *)filterAsE164;
 
 - (NSString *)substringBeforeRange:(NSRange)range;
--- SignalServiceKit/src/Util/NSString+SSK.m
+++ SignalServiceKit/src/Util/NSString+SSK.m
@@ -9,28 +9,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation NSString (SSK)
 
-- (nullable NSString *)dominantLanguageWithLegacyLinguisticTagger
-{
-    @try {
-        // This can throw NSException.
-        NSLinguisticTagger *tagger = [[NSLinguisticTagger alloc] initWithTagSchemes:@[
-            NSLinguisticTagSchemeLanguage,
-        ]
-                                                                            options:0];
-        tagger.string = self;
-        return [tagger tagAtIndex:0 scheme:NSLinguisticTagSchemeLanguage tokenRange:nil sentenceRange:nil];
-    } @catch (NSException *exception) {
-        // If we find a way to reproduce this issue, we might be able to
-        // find a better approach.
-        OWSFailDebug(@"Exception: %@, name: %@, reason: %@, user info: %@.",
-            exception.description,
-            exception.name,
-            exception.reason,
-            exception.userInfo);
-        return nil;
-    }
-}
-
 - (NSString *)filterAsE164
 {
     const NSUInteger maxLength = 256;
