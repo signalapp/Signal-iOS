e1dcf4f6a4b4866b6c632767bdc9eb3401d00c38
Evan Hahn <69474926+EvanHahn-Signal@users.noreply.github.com>
2023-04-24 17:50:50 -0500

    Remove unused `AnySearcher` class.

--- SignalMessaging/utils/Searcher.swift
+++ SignalMessaging/utils/Searcher.swift
@@ -5,22 +5,6 @@
 
 import Foundation
 
-// ObjC compatible searcher
-@objc
-class AnySearcher: NSObject {
-    private let searcher: Searcher<AnyObject>
-
-    public init(indexer: @escaping (AnyObject, SDSAnyReadTransaction) -> String ) {
-        searcher = Searcher(indexer: indexer)
-        super.init()
-    }
-
-    @objc(item:doesMatchQuery:transaction:)
-    public func matches(item: AnyObject, query: String, transaction: SDSAnyReadTransaction) -> Bool {
-        return searcher.matches(item: item, query: query, transaction: transaction)
-    }
-}
-
 // A generic searching class, configurable with an indexing block
 public class Searcher<T> {
 
