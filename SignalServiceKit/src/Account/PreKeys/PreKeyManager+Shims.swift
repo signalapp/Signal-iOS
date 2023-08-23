ea867a1ec93a564a1e899c33663b9e36c4370e1d
Harry <109690906+harry-signal@users.noreply.github.com>
2023-08-23 16:06:40 -0700

    Don't attempt to refresh pni prekeys on linked devices with no pni identity key

--- SignalServiceKit/src/Account/PreKeys/PreKeyManager+Shims.swift
+++ SignalServiceKit/src/Account/PreKeys/PreKeyManager+Shims.swift
@@ -8,12 +8,32 @@ import Foundation
 extension PreKey {
     public enum Manager {
         public enum Shims {
+            public typealias IdentityManager = _PreKeyManager_IdentityManagerShim
             public typealias MessageProcessor = _PreKeyManager_MessageProcessorShim
         }
 
         public enum Wrappers {
+            public typealias IdentityManager = _PreKeyManager_IdentityManagerWrapper
             public typealias MessageProcessor = _PreKeyManager_MessageProcessorWrapper
         }
+    }
+}
+
+// MARK: - IdentityManager
+
+public protocol _PreKeyManager_IdentityManagerShim {
+
+    func identityKeyPair(for identity: OWSIdentity, tx: DBReadTransaction) -> ECKeyPair?
+}
+
+public class _PreKeyManager_IdentityManagerWrapper: _PreKeyManager_IdentityManagerShim {
+    private let identityManager: OWSIdentityManager
+    init(_ identityManager: OWSIdentityManager) {
+        self.identityManager = identityManager
+    }
+
+    public func identityKeyPair(for identity: OWSIdentity, tx: DBReadTransaction) -> ECKeyPair? {
+        identityManager.identityKeyPair(for: identity, transaction: SDSDB.shimOnlyBridge(tx))
     }
 }
 
