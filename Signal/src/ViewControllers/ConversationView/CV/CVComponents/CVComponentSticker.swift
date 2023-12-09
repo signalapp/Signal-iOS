bb62010b71c2f8cb03de6bfa70eff20a20044098
Marissa Le Coz <129999395+marissa-signal@users.noreply.github.com>
2023-12-08 19:45:51 -0500

    Add sticker description to Voiceover

--- Signal/src/ViewControllers/ConversationView/CV/CVComponents/CVComponentSticker.swift
+++ Signal/src/ViewControllers/ConversationView/CV/CVComponents/CVComponentSticker.swift
@@ -197,8 +197,18 @@ public class CVComponentSticker: CVComponentBase, CVComponent {
 
 extension CVComponentSticker: CVAccessibilityComponent {
     public var accessibilityDescription: String {
-        // NOTE: We could include the strings used for sticker suggestion.
-        OWSLocalizedString("ACCESSIBILITY_LABEL_STICKER",
-                          comment: "Accessibility label for stickers.")
+        if let approximateEmoji = stickerMetadata?.firstEmoji {
+            return String(
+                format: OWSLocalizedString(
+                    "ACCESSIBILITY_LABEL_STICKER_FORMAT",
+                    comment: "Accessibility label for stickers. Embeds {{ name of top emoji the sticker resembles }}"),
+                approximateEmoji
+            )
+        } else {
+            return OWSLocalizedString(
+                "ACCESSIBILITY_LABEL_STICKER",
+                comment: "Accessibility label for stickers."
+            )
+        }
     }
 }
