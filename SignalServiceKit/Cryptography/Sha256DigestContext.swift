db49fc1fe490b2429f1f3e2382328da13f6b5910
Ehren Kret <ehren@signal.org>
2024-08-02 10:45:27 -0500

    migrate Sha256DigestContext from CommonCrypto to CryptoKit

--- SignalServiceKit/Cryptography/Sha256DigestContext.swift
+++ SignalServiceKit/Cryptography/Sha256DigestContext.swift
@@ -3,42 +3,43 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 //
 
-import CommonCrypto
+import CryptoKit
 import Foundation
 
+/// Implements SHA-256.
+///
+/// This class is not thread-safe and may only be used from one-thread at a time.
+/// Furthermore, this class is one-shot. After it is used to compute an hmac it
+/// must be thrown away to proceed. All instance functions will throw after
+/// finalization.
 public struct Sha256DigestContext {
-    private var context = CC_SHA256_CTX()
+    private var sha256 = SHA256()
     private var isFinal = false
-
-    public init() {
-        CC_SHA256_Init(&context)
-    }
 
     /// - parameter length: If non-nil, only that many bytes of the input will be read. If nil, the entire input is read.
     public mutating func update(_ data: Data, length: Int? = nil) throws {
-        try data.withUnsafeBytes { try update(bytes: $0, length: length) }
+        guard !isFinal else {
+            throw OWSAssertionError("Unexpectedly attempted to update a finalized sha256 digest")
+        }
+
+        sha256.update(data: data.prefix(length ?? data.count))
     }
 
     /// - parameter length: If non-nil, only that many bytes of the input will be read. If nil, the entire input is read.
     public mutating func update(bytes: UnsafeRawBufferPointer, length: Int? = nil) throws {
         guard !isFinal else {
-            throw OWSAssertionError("Unexpectedly attempted update a finalized hmac digest")
+            throw OWSAssertionError("Unexpectedly attempted to update a finalized sha256 digest")
         }
 
-        CC_SHA256_Update(&context, bytes.baseAddress, numericCast(length ?? bytes.count))
+        sha256.update(data: bytes.prefix(length ?? bytes.count))
     }
 
     public mutating func finalize() throws -> Data {
         guard !isFinal else {
-            throw OWSAssertionError("Unexpectedly attempted to finalize a finalized hmac digest")
+            throw OWSAssertionError("Unexpectedly attempted to finalize a finalized sha256 digest")
         }
 
         isFinal = true
-
-        var digest = Data(repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        _ = digest.withUnsafeMutableBytes {
-            CC_SHA256_Final($0.baseAddress?.assumingMemoryBound(to: UInt8.self), &context)
-        }
-        return digest
+        return Data(sha256.finalize())
     }
 }
