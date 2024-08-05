9caaa0d59208f3eaba7f53be5f68239dd6ae138f
Adam Sharp <sharplet@signal.org>
2024-08-05 17:43:33 -0400

    Add an expanded empty state to unread filter footer

--- Signal/src/ViewControllers/HomeView/Chat List/ChatListFilterFooterCell.swift
+++ Signal/src/ViewControllers/HomeView/Chat List/ChatListFilterFooterCell.swift
@@ -10,6 +10,27 @@ class ChatListFilterFooterCell: UITableViewCell, ReusableTableViewCell {
     static let reuseIdentifier = "ChatListFilterFooterCell"
 
     private let button: ChatListFilterButton
+    private let container: UIView
+    private let expandedHeight: NSLayoutConstraint
+    private let messageLabel: UILabel
+    private let stack: UIStackView
+
+    var isExpanded: Bool = false {
+        didSet {
+            setNeedsUpdateConstraints()
+        }
+    }
+
+    var message: String? {
+        didSet {
+            if let message = message?.nilIfEmpty {
+                messageLabel.isHidden = false
+                messageLabel.text = message
+            } else {
+                messageLabel.isHidden = true
+            }
+        }
+    }
 
     var primaryAction: UIAction? {
         didSet {
@@ -36,16 +57,33 @@ class ChatListFilterFooterCell: UITableViewCell, ReusableTableViewCell {
 
     override init(style: CellStyle, reuseIdentifier: String?) {
         button = ChatListFilterButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
+        container = UIView()
+        container.preservesSuperviewLayoutMargins = true
+        container.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel = UILabel()
+        messageLabel.font = .preferredFont(forTextStyle: .callout)
+        messageLabel.isHidden = true
+        messageLabel.textColor = .secondaryLabel
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        stack = UIStackView(arrangedSubviews: [messageLabel, button])
+        stack.alignment = .center
+        stack.axis = .vertical
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.preservesSuperviewLayoutMargins = true
+        stack.spacing = 20
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        container.addSubview(stack)
+        stack.autoPinHorizontalEdges(toEdgesOf: container)
+        stack.autoVCenterInSuperview()
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(greaterThanOrEqualTo: container.topAnchor, constant: 40),
+            container.bottomAnchor.constraint(greaterThanOrEqualTo: stack.bottomAnchor, constant: 40),
+        ])
+        expandedHeight = container.heightAnchor.constraint(equalToConstant: 0)
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundConfiguration = .clear()
-        contentView.addSubview(button)
-        NSLayoutConstraint.activate([
-            button.heightAnchor.constraint(greaterThanOrEqualToConstant: 28),
-            button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            button.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            button.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
-        ])
+        contentView.addSubview(container)
+        container.autoPinEdgesToSuperviewEdges()
     }
 
     @available(*, unavailable)
@@ -62,5 +100,20 @@ class ChatListFilterFooterCell: UITableViewCell, ReusableTableViewCell {
 
         button.configuration?.title = title ?? primaryAction?.title
         button.showsClearIcon = showsClearIcon
+    }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+
+        expandedHeight.constant = traitCollection.verticalSizeClass == .compact ? 240 : 600
+        expandedHeight.isActive = isExpanded
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if traitCollection.verticalSizeClass != previousTraitCollection?.verticalSizeClass {
+            setNeedsUpdateConstraints()
+        }
     }
 }
