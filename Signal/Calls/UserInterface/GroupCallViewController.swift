b9c3f3957a3c761a0fe5a0fecad75d5076d09669
Elaine <138257830+elaine-signal@users.noreply.github.com>
2024-08-12 16:35:40 -0600

    Disable drawer interaction while joining call

--- Signal/Calls/UserInterface/GroupCallViewController.swift
+++ Signal/Calls/UserInterface/GroupCallViewController.swift
@@ -517,6 +517,7 @@ class GroupCallViewController: UIViewController {
             return
         }
         bottomSheet.setBottomSheetMinimizedHeight()
+        bottomSheet.view.isUserInteractionEnabled = true
         present(self.bottomSheet, animated: true)
     }
 
@@ -1491,7 +1492,11 @@ extension GroupCallViewController: GroupCallObserver {
             if membersAtJoin == nil {
                 membersAtJoin = Set(ringRtcCall.remoteDeviceStates.lazy.map { $0.value.address })
             }
-        case .pending, .joining, .notJoined:
+            self.bottomSheet.view.isUserInteractionEnabled = true
+        case .joining:
+            self.bottomSheet.view.isUserInteractionEnabled = false
+            fallthrough
+        case .pending, .notJoined:
             membersAtJoin = nil
         }
     }
