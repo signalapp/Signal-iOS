e477588e5872bc756f8dcf5680e2a769b0549d21
Elaine <138257830+elaine-signal@users.noreply.github.com>
2024-08-21 15:01:11 -0600

    Pass touches through empty space around flip camera button

--- Signal/Calls/UserInterface/SupplementalCallControlsForFullscreenLocalMember.swift
+++ Signal/Calls/UserInterface/SupplementalCallControlsForFullscreenLocalMember.swift
@@ -95,6 +95,14 @@ class SupplementalCallControlsForFullscreenLocalMember: UIView {
         flipCameraButton.isHidden = !groupCall.isJustMe || call.isOutgoingVideoMuted
     }
 
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event)
+        if view == self {
+            return nil
+        }
+        return view
+    }
+
     // MARK: - Required
 
     required init?(coder: NSCoder) {
