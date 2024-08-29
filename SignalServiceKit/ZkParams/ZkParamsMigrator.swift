407350647d9cf1d260094aee150636990e265c9a
Max Radermacher <max@signal.org>
2024-08-29 15:31:02 -0500

    Simplify ZkParamsMigrator

--- SignalServiceKit/ZkParams/ZkParamsMigrator.swift
+++ SignalServiceKit/ZkParams/ZkParamsMigrator.swift
@@ -58,20 +58,11 @@ class ZkParamsMigrator {
 
     private func performMigration(oldMigrationCounter: Int, tx: DBWriteTransaction) {
         switch oldMigrationCounter {
-        case 0, 1, 2, 3:
-            // If you have anything other than v4, reset everything.
-            fallthrough
-        case 4 where !didStoreFinalV4ServerPublicParams(tx: tx):
-            // If you have v4 and any params other than the ones just prior to the new
-            // migration logic, reset everything.
+        case 0, 1, 2, 3, 4:
             Logger.info("Resetting zkgroup-related state.")
             authCredentialStore.removeAllGroupAuthCredentials(tx: tx)
             versionedProfiles.clearProfileKeyCredentials(tx: tx)
             reuploadLocalProfile()
-            fallthrough
-        case 4:
-            // If you have v4 and the right params, do a "normal" migration to v5. The
-            // "normal" migration removes a key that we no longer need to track.
             migrationStore.removeValue(forKey: Constants.lastServerPublicParamsKey, transaction: tx)
             fallthrough
         case 5:
@@ -80,18 +71,6 @@ class ZkParamsMigrator {
             fallthrough
         default:
             break
-        }
-    }
-
-    private func didStoreFinalV4ServerPublicParams(tx: DBReadTransaction) -> Bool {
-        return migrationStore.getString(Constants.lastServerPublicParamsKey, transaction: tx) == finalV4ServerPublicParams
-    }
-
-    private var finalV4ServerPublicParams: String {
-        if TSConstants.isUsingProductionService {
-            return "AMhf5ywVwITZMsff/eCyudZx9JDmkkkbV6PInzG4p8x3VqVJSFiMvnvlEKWuRob/1eaIetR31IYeAbm0NdOuHH8Qi+Rexi1wLlpzIo1gstHWBfZzy1+qHRV5A4TqPp15YzBPm0WSggW6PbSn+F4lf57VCnHF7p8SvzAA2ZZJPYJURt8X7bbg+H3i+PEjH9DXItNEqs2sNcug37xZQDLm7X36nOoGPs54XsEGzPdEV+itQNGUFEjY6X9Uv+Acuks7NpyGvCoKxGwgKgE5XyJ+nNKlyHHOLb6N1NuHyBrZrgtY/JYJHRooo5CEqYKBqdFnmbTVGEkCvJKxLnjwKWf+fEPoWeQFj5ObDjcKMZf2Jm2Ae69x+ikU5gBXsRmoF94GXTLfN0/vLt98KDPnxwAQL9j5V1jGOY8jQl6MLxEs56cwXN0dqCnImzVH3TZT1cJ8SW1BRX6qIVxEzjsSGx3yxF3suAilPMqGRp4ffyopjMD1JXiKR2RwLKzizUe5e8XyGOy9fplzhw3jVzTRyUZTRSZKkMLWcQ/gv0E4aONNqs4P"
-        } else {
-            return "ABSY21VckQcbSXVNCGRYJcfWHiAMZmpTtTELcDmxgdFbtp/bWsSxZdMKzfCp8rvIs8ocCU3B37fT3r4Mi5qAemeGeR2X+/YmOGR5ofui7tD5mDQfstAI9i+4WpMtIe8KC3wU5w3Inq3uNWVmoGtpKndsNfwJrCg0Hd9zmObhypUnSkfYn2ooMOOnBpfdanRtrvetZUayDMSC5iSRcXKpdlukrpzzsCIvEwjwQlJYVPOQPj4V0F4UXXBdHSLK05uoPBCQG8G9rYIGedYsClJXnbrgGYG3eMTG5hnx4X4ntARBgELuMWWUEEfSK0mjXg+/2lPmWcTZWR9nkqgQQP0tbzuiPm74H2wMO4u1Wafe+UwyIlIT9L7KLS19Aw8r4sPrXZSSsOZ6s7M1+rTJN0bI5CKY2PX29y5Ok3jSWufIKcgKOnWoP67d5b2du2ZVJjpjfibNIHbT/cegy/sBLoFwtHogVYUewANUAXIaMPyCLRArsKhfJ5wBtTminG/PAvuBdJ70Z/bXVPf8TVsR292zQ65xwvWTejROW6AZX6aqucUj"
         }
     }
 
