a03631e620905f11a6bf785a7db2ea4f607f40dc
Max Radermacher <max@signal.org>
2024-09-23 12:07:56 -0500

    Tweak exponential backoff parameters

--- SignalServiceKit/Util/OWSOperation.swift
+++ SignalServiceKit/Util/OWSOperation.swift
@@ -253,23 +253,8 @@ open class OWSOperation: Operation {
 
     // MARK: - Helper Methods
 
-    public static func retryIntervalForExponentialBackoff(failureCount: UInt, maxBackoff: TimeInterval = 15 * kMinuteInterval) -> TimeInterval {
-        // Arbitrary backoff factor...
-        // With backOffFactor of 1.9 and maxBackoff of 15m
-        // retry 1 delay:  0.10s
-        // retry 2 delay:  0.19s
-        // ...
-        // retry 5 delay:  1.30s
-        // ...
-        // retry 11 delay: 61.31s
-        // ...
-        // retry 15 delay: 15 minutes
-        // retry 16 delay: 15 minutes
-        //
+    public static func retryIntervalForExponentialBackoff(failureCount: UInt, maxBackoff: TimeInterval = 14.1 * kMinuteInterval) -> TimeInterval {
         // 110 retries will yield ~24 hours of retry.
-        let backoffFactor = 1.9
-
-        let seconds = min(maxBackoff, 0.1 * pow(backoffFactor, Double(failureCount)))
-        return seconds
+        return min(maxBackoff, pow(2, Double(failureCount)))
     }
 }
