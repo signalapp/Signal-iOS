f70a70a277966e4ef58d9f5e020d6a8472dc52b9
Marissa Le Coz <129999395+marissa-signal@users.noreply.github.com>
2024-11-14 17:27:24 -0500

    Add comment for why chat_item_group_change_chat_update_36/37 are disabled

--- SignalServiceKit/tests/MessageBackup/MessageBackupIntegrationTests.swift
+++ SignalServiceKit/tests/MessageBackup/MessageBackupIntegrationTests.swift
@@ -148,6 +148,9 @@ class MessageBackupIntegrationTests: XCTestCase {
                 case .chatItemGroupCall:
                     return binprotoName.contains("chat_item_group_call_update_")
                 case .chatItemGroupChangeChatUpdate:
+                    // Tests 37 and 38 are intentionally excluded from iOS because they fail
+                    // for spurious reasons. Namely, for GroupMemberAddedUpdate, iOS has neither
+                    // 1) distinct inviterAci/updaterAci fields, nor 2) a need for hasOpenInvite.
                     return binprotoName.contains("chat_item_group_change_chat_update_")
                 case .chatItemIndividualCall:
                     return binprotoName.contains("chat_item_individual_call_update_")
