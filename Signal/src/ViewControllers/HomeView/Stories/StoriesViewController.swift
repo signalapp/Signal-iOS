83a022efe36491a6321fc7b00f5c95f654a17ae3
Pete Walters <pete@signal.org>
2024-11-27 11:52:20 -0600

    No longer need to manager tabbar during stories search

--- Signal/src/ViewControllers/HomeView/Stories/StoriesViewController.swift
+++ Signal/src/ViewControllers/HomeView/Stories/StoriesViewController.swift
@@ -137,9 +137,6 @@ class StoriesViewController: OWSViewController, StoryListDataSourceDelegate, Hom
 
         timestampUpdateTimer?.invalidate()
         timestampUpdateTimer = nil
-
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-        (tabBarController as? HomeTabBarController)?.setTabBarHidden(false, animated: animated)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
