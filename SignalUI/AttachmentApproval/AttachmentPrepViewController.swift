d7e9a88598e2da503d4b845be36ea669e4176717
ChronicLynx <29527680+ChronicLynx@users.noreply.github.com>
2025-02-25 14:04:33 -0800

    Ensure presented view controller is dismissed before presenting in AttachmentPrep

--- SignalUI/AttachmentApproval/AttachmentPrepViewController.swift
+++ SignalUI/AttachmentApproval/AttachmentPrepViewController.swift
@@ -219,10 +219,12 @@ public class AttachmentPrepViewController: OWSViewController {
     private func presentFullScreen(viewController: UIViewController) {
         if let presentedViewController = presentedViewController {
             owsAssertDebug(false, "Already has presented view controller. [\(presentedViewController)]")
-            presentedViewController.dismiss(animated: false)
+            presentedViewController.dismiss(animated: false) { [weak self] in
+                self?.presentFullScreen(viewController: viewController)
+            }
+            return
         }
 
-        viewController.modalPresentationStyle = .fullScreen
         zoomOut(animated: true) { [weak self] in
             self?.presentFullScreen(viewController, animated: false)
         }
