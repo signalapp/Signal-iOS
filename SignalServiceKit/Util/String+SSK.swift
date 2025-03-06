30dfa47df761171104168861b5b659ff3391c348
Max Radermacher <max@signal.org>
2025-03-06 13:55:59 -0600

    Remove unused String.truncated method

--- SignalServiceKit/Util/String+SSK.swift
+++ SignalServiceKit/Util/String+SSK.swift
@@ -33,57 +33,6 @@ public extension String {
         filterStringForDisplay()
     }
 
-    // There appears to be a bug in NSBigMutableString that causes a
-    // crash when using prefix to (not) truncate long strings to their
-    // current length. safePrefix() avoids this crash by only using
-    // prefix() if necessary.
-    func safePrefix(_ maxLength: Int) -> String {
-        guard maxLength < count else {
-            return self
-        }
-        return String(prefix(maxLength))
-    }
-
-    // Truncates string to be less than or equal to byteCount, while ensuring we never truncate partial characters for multibyte characters.
-    func truncated(toByteCount byteCount: UInt) -> String? {
-        var lowerBoundCharCount = 0
-        var upperBoundCharCount = self.count
-
-        while (lowerBoundCharCount < upperBoundCharCount) {
-            guard let upperBoundData = safePrefix(upperBoundCharCount).data(using: .utf8) else {
-                owsFailDebug("upperBoundData was unexpectedly nil")
-                return nil
-            }
-
-            if upperBoundData.count <= byteCount {
-                break
-            }
-
-            // converge
-            if upperBoundCharCount - lowerBoundCharCount == 1 {
-                upperBoundCharCount = lowerBoundCharCount
-                break
-            }
-
-            let midpointCharCount = (lowerBoundCharCount + upperBoundCharCount) / 2
-            let midpointString = safePrefix(midpointCharCount)
-
-            guard let midpointData = midpointString.data(using: .utf8) else {
-                owsFailDebug("midpointData was unexpectedly nil")
-                return nil
-            }
-            let midpointByteCount = midpointData.count
-
-            if midpointByteCount < byteCount {
-                lowerBoundCharCount = midpointCharCount
-            } else {
-                upperBoundCharCount = midpointCharCount
-            }
-        }
-
-        return String(safePrefix(upperBoundCharCount))
-    }
-
     func replaceCharacters(
         characterSet: CharacterSet,
         replacement: String
