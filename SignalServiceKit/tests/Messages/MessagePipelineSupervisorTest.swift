7a6880ae7d626b75ba4816975f85a37745e939c1
Max Radermacher <max@signal.org>
2025-05-14 12:43:08 -0500

    Don’t inherit from SSKBaseTest

--- SignalServiceKit/tests/Messages/MessagePipelineSupervisorTest.swift
+++ SignalServiceKit/tests/Messages/MessagePipelineSupervisorTest.swift
@@ -7,7 +7,7 @@ import Foundation
 import XCTest
 @testable import SignalServiceKit
 
-class MessagePipelineSupervisorTest: SSKBaseTest {
+class MessagePipelineSupervisorTest: XCTestCase {
 
     var dut: MessagePipelineSupervisor! = nil
 
