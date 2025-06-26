3a8b20d8bb224cccc253eaa626365ec4a9cee2de
Tapmediainc <devops@tapofthink.com>
2025-06-26 18:35:14 +0700

    change config to prod

--- SignalServiceKit/TSConstants.swift
+++ SignalServiceKit/TSConstants.swift
@@ -153,20 +153,20 @@ public struct MrEnclave: Equatable {
 
 private class TSConstantsProduction: TSConstantsProtocol {
 
-    public let mainServiceIdentifiedURL = "https://signal-server-dev.tapofthink.com"
-    public let mainServiceUnidentifiedURL = "https://signal-server-dev.tapofthink.com"
-    public let textSecureCDN0ServerURL = "https://cdn1.tapofthink.com"
+    public let mainServiceIdentifiedURL = "https://chat.tapofthink.com"
+    public let mainServiceUnidentifiedURL = "https://chat.tapofthink.com"
+    public let textSecureCDN0ServerURL = "https://cdn.tapofthink.com"
     public let textSecureCDN2ServerURL = "https://cdn2.tapofthink.com"
-    public let textSecureCDN3ServerURL = "https://cdn3.signal.org"
-    public let storageServiceURL = "https://storage-service-dev.tapofthink.com"
-    public let sfuURL = "https://sfu.voip.signal.org"
-    public let sfuTestURL = "https://sfu.test.voip.signal.org"
-    public let svr2URL = "wss://svr2-dev.tapofthink.com"
+    public let textSecureCDN3ServerURL = "https://cdn3.tapofthink.com"
+    public let storageServiceURL = "https://storage.tapofthink.com"
+    public let sfuURL = "https://sfu.tapofthink.com"
+    public let sfuTestURL = "https://sfu-dev.tapofthink.com"
+    public let svr2URL = "wss://svr2.tapofthink.com"
     public let registrationCaptchaURL = "https://hcaptcha.tapofthink.com/index.html"
     public let challengeCaptchaURL = "https://hcaptcha.tapofthink.com/index.html"
-    public let kUDTrustRoot = "BXu6QIKVz5MA8gstzfOgRQGqyLqOwNKHL6INkv3IHWMF"
+    public let kUDTrustRoot = "BeSnG+rbeaRzY/4zUxm2Ha12ZZITOs4MD5QuDWPbqqgj"
     public let updatesURL = "https://updates.signal.org"
-    public let updates2URL = "https://cdn1.tapofthink.com"
+    public let updates2URL = "https://cdn.tapofthink.com"
 
     public let censorshipFReflectorHost = "reflector-signal.global.ssl.fastly.net"
     public let censorshipGReflectorHost = "reflector-nrgwuv7kwq-uc.a.run.app"
@@ -178,7 +178,7 @@ private class TSConstantsProduction: TSConstantsProtocol {
     public let storageServiceCensorshipPrefix = "storage"
     public let svr2CensorshipPrefix = "svr2"
 
-    public let svr2Enclave = MrEnclave("38e01eff4fe357dc0b0e8ef7a44b4abc5489fbccba3a78780f3872c277f62bf3")
+    public let svr2Enclave = MrEnclave("38f055c0465b379aef56e3ba9318317eacca9432bdd3b3cab887d09eb84b379f")
 
     // An array of previously used enclaves that we should try and restore
     // key material from during registration. These must be ordered from
@@ -205,20 +205,20 @@ private class TSConstantsStaging: TSConstantsProtocol {
 
     public let mainServiceIdentifiedURL = "https://signal-server-dev.tapofthink.com"
     public let mainServiceUnidentifiedURL = "https://signal-server-dev.tapofthink.com"
-    public let textSecureCDN0ServerURL = "https://cdn1.tapofthink.com"
+    public let textSecureCDN0ServerURL = "https://cdn.tapofthink.com"
     public let textSecureCDN2ServerURL = "https://cdn2.tapofthink.com"
-    public let textSecureCDN3ServerURL = "https://cdn3-staging.signal.org"
-    public let storageServiceURL = "https://storage-service-dev.tapofthink.com"
-    public let sfuURL = "https://sfu.staging.voip.signal.org"
+    public let textSecureCDN3ServerURL = "https://cdn3.tapofthink.com"
+    public let storageServiceURL = "https://storage-dev.tapofthink.com"
+    public let sfuURL = "https://sfu-dev.tapofthink.com"
     public let svr2URL = "wss://svr2-dev.tapofthink.com"
     public let registrationCaptchaURL = "https://hcaptcha.tapofthink.com/index.html"
     public let challengeCaptchaURL = "https://hcaptcha.tapofthink.com/index.html"
     // There's no separate test SFU for staging.
-    public let sfuTestURL = "https://sfu.test.voip.signal.org"
-    public let kUDTrustRoot = "BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx"
+    public let sfuTestURL = "https://sfu-dev.tapofthink.com"
+    public let kUDTrustRoot = "BeSnG+rbeaRzY/4zUxm2Ha12ZZITOs4MD5QuDWPbqqgj"
     // There's no separate updates endpoint for staging.
     public let updatesURL = "https://updates.signal.org"
-    public let updates2URL = "https://cdn1.tapofthink.com"
+    public let updates2URL = "https://cdn.tapofthink.com"
 
     public let censorshipFReflectorHost = "reflector-staging-signal.global.ssl.fastly.net"
     public let censorshipGReflectorHost = "reflector-nrgwuv7kwq-uc.a.run.app"
@@ -244,11 +244,11 @@ private class TSConstantsStaging: TSConstantsProtocol {
     /// We *might* need to clear credentials (or perform some other migration)
     /// when this value changes, depending on how it's changing. If you do need
     /// to perform a migration, check out `ZkParamsMigrator`.
-    public let serverPublicParams = [UInt8](Data(base64Encoded: "ABSY21VckQcbSXVNCGRYJcfWHiAMZmpTtTELcDmxgdFbtp/bWsSxZdMKzfCp8rvIs8ocCU3B37fT3r4Mi5qAemeGeR2X+/YmOGR5ofui7tD5mDQfstAI9i+4WpMtIe8KC3wU5w3Inq3uNWVmoGtpKndsNfwJrCg0Hd9zmObhypUnSkfYn2ooMOOnBpfdanRtrvetZUayDMSC5iSRcXKpdlukrpzzsCIvEwjwQlJYVPOQPj4V0F4UXXBdHSLK05uoPBCQG8G9rYIGedYsClJXnbrgGYG3eMTG5hnx4X4ntARBgELuMWWUEEfSK0mjXg+/2lPmWcTZWR9nkqgQQP0tbzuiPm74H2wMO4u1Wafe+UwyIlIT9L7KLS19Aw8r4sPrXZSSsOZ6s7M1+rTJN0bI5CKY2PX29y5Ok3jSWufIKcgKOnWoP67d5b2du2ZVJjpjfibNIHbT/cegy/sBLoFwtHogVYUewANUAXIaMPyCLRArsKhfJ5wBtTminG/PAvuBdJ70Z/bXVPf8TVsR292zQ65xwvWTejROW6AZX6aqucUjlENAErBme1YHmOSpU6tr6doJ66dPzVAWIanmO/5mgjNEDeK7DDqQdB1xd03HT2Qs2TxY3kCK8aAb/0iM0HQiXjxZ9HIgYhbtvGEnDKW5ILSUydqH/KBhW4Pb0jZWnqN/YgbWDKeJxnDbYcUob5ZY5Lt5ZCMKuaGUvCJRrCtuugSMaqjowCGRempsDdJEt+cMaalhZ6gczklJB/IbdwENW9KeVFPoFNFzhxWUIS5ML9riVYhAtE6JE5jX0xiHNVIIPthb458cfA8daR0nYfYAUKogQArm0iBezOO+mPk5vCNWI+wwkyFCqNDXz/qxl1gAntuCJtSfq9OC3NkdhQlgYQ==")!)
+    public let serverPublicParams = [UInt8](Data(base64Encoded: "ABAnr3zs35ijDRYRv9oVbKeBBTqf8tq8ZduF32VkpJYMgImd+68d8P7kDk6lcmLcRr/APdO4MkBK9N2e1STqshLCnmh4rjHdc8U7q61f+B2INSpV39KMTNmXsLg8bXTtVHZNJvTKs7lPFfcPnWsev+cdmjdl9AghmVR0V8z5xnRqaqZ8O3XRyPWmFa8z5gkFW2rEDtO3AP2Ah73y/0fa32fuaSD/rEWscXWh2L/KbY/1z0JWy5Ru1LLL8psExN8+Fg41swo1xb64faW6K4Pit1RrO+IS5tAC+q2HWYpZkIYXTmD1zPKHh0RAasFXGVlTKEU/yWTz7oEzbV2CM0/jZz4WhAqT68kbBFeqN6qUphaIrAIMH7sapbkqp5tJFzKZa9Zy8bu21ZV+5abJde5txkVUOOXl3EU2GFed5HN4t+8NMnqJxXN7kI//zWKTeHrkSancOPu3s+dRg8XPw/66BQAqTQi2MqDn3hMbPefFafjEGzJalU+qwZ+8OjFZNmkqT1a/0Bch2VIUoD/lhaoy6UzQgzv3cOrT7KovtDPnQcEJnpAizRezI9JSVtpjIGeYLoFjdmhbp2l/NgajgQaMxTM290NrYO/L3G7Fm9E7J52o6dZeL4Jfq+Uh2wkpsRWVfCbx6kxsyBZR2515p/vmabZvFNEWMAZ+9l1HolWMte4/0Lvd6LPq0U9Y9yf3LRc8qVemrBAM0V+VK+4SlZAdR2IUya5N4/NfHkN+BWB0IZA6oDYVxS1dvSmeh/Keiq5XBLhAofW2tFCOw4o6Obfb4t+Fw/7f7WSjSbXVJVoe9aBn4gQsRkJuJcwXpM7xOKbV9BJ2NYM0WlwTp2kzq0zVxQICDHKWWvCljXttsiAaFovphDbXPK1kNqEc8UrwZTNEBA==\")!)
 
-    public let callLinkPublicParams = Data(base64Encoded: "AHILOIrFPXX9laLbalbA9+L1CXpSbM/bTJXZGZiuyK1JaI6dK5FHHWL6tWxmHKYAZTSYmElmJ5z2A5YcirjO/yfoemE03FItyaf8W1fE4p14hzb5qnrmfXUSiAIVrhaXVwIwSzH6RL/+EO8jFIjJ/YfExfJ8aBl48CKHgu1+A6kWynhttonvWWx6h7924mIzW0Czj2ROuh4LwQyZypex4GuOPW8sgIT21KNZaafgg+KbV7XM1x1tF3XA17B4uGUaDbDw2O+nR1+U5p6qHPzmJ7ggFjSN6Utu+35dS1sS0P9N")!
+    public let callLinkPublicParams = Data(base64Encoded: "AGLk00y9k8ZdD8rONpj9MCVm0bwPyIo3tfa7Zi59kippDkRVFlaT+192wyVKNIUZmI8/x3a2G4Byc1zRUlPGDEoeUcnnRvF5kYM1/izgssNd0riRsscd8sS2fkv/khY1aSQZHXGED+kyfgr78XT7yl3gsgsuPd/Gz8nJ2dPcZSsx3N8Ckqdy8+ArrOGJFPqnpW77+sVRSuKExjUjKsQodwXo0VF7sphQttqOdtOHJZNk/1EfH5SLtvrBJB0Vy9uBK8Y2rNOZd4JkTlrvnw5a+wNaBHFYZOz6LyiyD0oxghIn")!
 
-    public let backupServerPublicParams = Data(base64Encoded: "AHYrGb9IfugAAJiPKp+mdXUx+OL9zBolPYHYQz6GI1gWjpEu5me3zVNSvmYY4zWboZHif+HG1sDHSuvwFd0QszSwuSF4X4kRP3fJREdTZ5MCR0n55zUppTwfHRW2S4sdQ0JGz7YDQIJCufYSKh0pGNEHL6hv79Agrdnr4momr3oXdnkpVBIp3HWAQ6IbXQVSG18X36GaicI1vdT0UFmTwU2KTneluC2eyL9c5ff8PcmiS+YcLzh0OKYQXB5ZfQ06d6DiINvDQLy75zcfUOniLAj0lGJiHxGczin/RXisKSR8")!
+    public let backupServerPublicParams = Data(base64Encoded: "AGLk00y9k8ZdD8rONpj9MCVm0bwPyIo3tfa7Zi59kippDkRVFlaT+192wyVKNIUZmI8/x3a2G4Byc1zRUlPGDEoeUcnnRvF5kYM1/izgssNd0riRsscd8sS2fkv/khY1aSQZHXGED+kyfgr78XT7yl3gsgsuPd/Gz8nJ2dPcZSsx3N8Ckqdy8+ArrOGJFPqnpW77+sVRSuKExjUjKsQodwXo0VF7sphQttqOdtOHJZNk/1EfH5SLtvrBJB0Vy9uBK8Y2rNOZd4JkTlrvnw5a+wNaBHFYZOz6LyiyD0oxghIn")!
 
 }
 
