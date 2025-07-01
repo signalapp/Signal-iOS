f0c84da2332760c45f5e3d1c51f83c8126bd1b2a
Max Radermacher <max@signal.org>
2025-07-01 14:11:36 -0500

    Add “What to Test” notes for Xcode 26

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,6 +8,8 @@ cd ..
 
 if [ "${CI_WORKFLOW-}" = "Nightly (Xcode 26)" ]; then
     : "Skipping version check for Xcode 26"
+    mkdir TestFlight
+    echo "Xcode 26 build" > TestFlight/WhatToTest.en-US.txt
 else
     Scripts/check_xcode_version.py
 fi
