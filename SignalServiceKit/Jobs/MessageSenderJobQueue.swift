a79d45e3716741a868a51faea19a3326ef522580
Max Radermacher <max@signal.org>
2025-07-23 12:29:17 -0500

    Remove unused variable

--- SignalServiceKit/Jobs/MessageSenderJobQueue.swift
+++ SignalServiceKit/Jobs/MessageSenderJobQueue.swift
@@ -331,7 +331,6 @@ public class MessageSenderJobQueue {
     }
 
     public func setUp() {
-        let chatConnectionManager = DependenciesBridge.shared.chatConnectionManager
         let jobRecordFinder = JobRecordFinderImpl<MessageSenderJobRecord>(db: DependenciesBridge.shared.db)
         Task {
             if CurrentAppContext().isMainApp {
