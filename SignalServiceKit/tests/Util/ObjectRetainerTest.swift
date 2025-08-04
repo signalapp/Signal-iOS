7b550508303105b4d7bd8f9fbd6d7dc56c55b979
Sasha Weiss <sasha@signal.org>
2025-08-04 15:06:16 -0700

    Add already-passing test for ObjectRetainer

---
+++ SignalServiceKit/tests/Util/ObjectRetainerTest.swift
@@ -0,0 +1,38 @@
+//
+// Copyright 2025 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+import Testing
+
+@testable import SignalServiceKit
+
+@Suite
+struct ObjectRetainerTest {
+    private class Retainer {}
+    private class Retained {}
+
+    @Test
+    func retainerRetainsObjectUntilRetainerReleased() {
+        var retainer: Retainer? = Retainer()
+        weak var retained: Retained?
+
+        do {
+            let _retained = Retained()
+            retained = _retained
+
+            ObjectRetainer.retainObject(
+                retained!,
+                forLifetimeOf: retainer!
+            )
+        }
+
+        #expect(retainer != nil)
+        #expect(retained != nil)
+
+        retainer = nil
+
+        #expect(retainer == nil)
+        #expect(retained == nil)
+    }
+}
