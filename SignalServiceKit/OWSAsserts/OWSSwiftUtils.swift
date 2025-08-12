b4696c181b199247e55cef5303f46561ec1f5a67
Sasha Weiss <sasha@signal.org>
2025-08-11 18:27:30 -0700

    Make owsFail more descriptive

--- SignalServiceKit/OWSAsserts/OWSSwiftUtils.swift
+++ SignalServiceKit/OWSAsserts/OWSSwiftUtils.swift
@@ -67,7 +67,43 @@ public func owsFail(
     logger.error(Thread.callStackSymbols.joined(separator: "\n"))
     owsFailDebug(logMessage, logger: logger, file: file, function: function, line: line)
     logger.flush()
-    fatalError(logMessage)
+
+    __crashDescriptively(logMessage)
+}
+
+/// Crash, injecting as much of the given log message as possible into the crash
+/// report itself. Useful in cases where we can get an `.ips` file, but no logs.
+public func __crashDescriptively(_ logMessageParam: String) -> Never {
+    var logMessage = ScrubbingLogFormatter().redactMessage(logMessageParam)
+
+    // Strip non-ASCII, to make the byte-range math below safe.
+    logMessage = logMessage.filter { $0.isASCII }
+    // Strip whitespace, to make the message as info-dense as possible.
+    logMessage = logMessage.filter { !$0.isWhitespace }
+
+    // Extract the first 63 bytes to use as the thread name, which is the max
+    // accepted. More than this will result in the name being set to "".
+    let threadNameSegment = String(logMessage.prefix(63))
+    logMessage = String(logMessage.dropFirst(63))
+
+    // Extract the next 127 bytes to use as the label for a DispatchQueue, which
+    // is the max accepted. More than this will be truncated.
+    let dispatchQueueNameSegment = String(logMessage.prefix(127))
+    logMessage = String(logMessage.dropFirst(127))
+
+    if let threadNameSegment = threadNameSegment.nilIfEmpty {
+        Thread.current.name = threadNameSegment
+
+        if let dispatchQueueNameSegment = dispatchQueueNameSegment.nilIfEmpty {
+            DispatchQueue(label: dispatchQueueNameSegment).sync {
+                fatalError(logMessageParam)
+            }
+        }
+    }
+
+    // Catch here in case we didn't have a long enough log message as to merit
+    // crashing above in a dedicated DispatchQueue.
+    fatalError(logMessageParam)
 }
 
 public func failIfThrows<T>(
