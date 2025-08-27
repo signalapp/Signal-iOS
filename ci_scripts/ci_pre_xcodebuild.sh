b5305318bd33d49ae986a2ac25a633cd2e1f1876
Max Radermacher <max@signal.org>
2025-08-27 18:08:47 -0500

    Switch to Xcode 26.0

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,7 +2,5 @@
 
 set -eux
 
-if [ "${CI_WORKFLOW-}" = "Nightly (Xcode 26)" ]; then
-    : "Downloading Metal Toolchain for Xcode 26"
-    xcodebuild -downloadComponent MetalToolchain
-fi
+: "Downloading Metal Toolchain"
+xcodebuild -downloadComponent MetalToolchain
