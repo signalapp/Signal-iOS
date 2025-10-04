96eed3bced9766e1775c74af538e348ca02bf8fd
Max Radermacher <max@signal.org>
2025-10-03 19:40:24 -0500

    Relax Xcode version checks in GitHub Actions

--- Scripts/check_xcode_version.py
+++ Scripts/check_xcode_version.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import subprocess
 
 
@@ -14,9 +15,22 @@ def get_expected_version():
         return file.read().rstrip()
 
 
+def without_patch_version(value):
+    components = value.split(".")
+    return ".".join(components[:2])
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--relaxed", action="store_true", help="ignore patch version when comparing"
+    )
+    ns = parser.parse_args()
     actual_version = get_actual_version()
     expected_version = get_expected_version()
+    if ns.relaxed:
+        actual_version = without_patch_version(actual_version)
+        expected_version = without_patch_version(expected_version)
     if actual_version != expected_version:
         print(
             f"You’re using {actual_version} but you should be using {expected_version}."
