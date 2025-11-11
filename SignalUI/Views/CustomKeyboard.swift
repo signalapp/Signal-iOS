659c3d78f06e0a2cf969981eff1f3f15e2ad3d30
Igor Solomennikov <igor@signal.org>
2025-11-11 12:19:16 -0800

    Improve animations when presenting attachment and stickers keyboards.

--- SignalUI/Views/CustomKeyboard.swift
+++ SignalUI/Views/CustomKeyboard.swift
@@ -29,13 +29,10 @@ open class CustomKeyboard: UIInputView {
     open func wasDismissed() {}
 
     open override func willMove(toSuperview newSuperview: UIView?) {
-        DispatchQueue.main.async { [weak self] in
-            guard let self else { return }
-            if newSuperview != nil {
-                self.willPresent()
-            } else {
-                self.willDismiss()
-            }
+        if newSuperview != nil {
+            self.willPresent()
+        } else {
+            self.willDismiss()
         }
     }
 
