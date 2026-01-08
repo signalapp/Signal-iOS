4332e1d28a5cef649124781f87c5f68f30ea2d4b
Serhii Bykov <esphynox@gmail.com>
2026-01-08 21:10:40 +0100

    MimeTypeUtil - update audio/mpeg mime type to correct file extension

--- SignalServiceKit/Util/MimeTypeUtil.swift
+++ SignalServiceKit/Util/MimeTypeUtil.swift
@@ -947,7 +947,7 @@ public enum MimeTypeUtil {
         "audio/midi": "midi",
         "audio/mod": "mod",
         "audio/mp4": "m4a",
-        "audio/mpeg": "mpg",
+        "audio/mpeg": "mp3",
         "audio/mpeg3": "mp3",
         "audio/ogg": "oga",
         "audio/s3m": "s3m",
