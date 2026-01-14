d162a86316e579757992ed1553a36b3c594c9cfc
Elaine <138257830+elaine-signal@users.noreply.github.com>
2026-01-14 15:35:36 -0500

    Update call quality survey flag and correct toast corners

--- SignalUI/Views/Toast.swift
+++ SignalUI/Views/Toast.swift
@@ -208,11 +208,7 @@ class ToastView: UIView {
         label = UILabel()
         super.init(frame: frame)
 
-        if #available(iOS 26, *) {
-            self.cornerConfiguration = .capsule()
-        } else {
-            self.layer.cornerRadius = 12
-        }
+        self.layer.cornerRadius = 12
         self.clipsToBounds = true
         self.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
 
