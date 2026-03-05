fccc0dcbcff188deae5194c17b03bc989bcb0eea
Pete Walters <pete@signal.org>
2026-03-05 15:55:27 -0600

    Revert test changes

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,11 +2,5 @@
 
 set -eux
 
-: "Checking Metal Toolchain"
-xcodebuild -showComponent metalToolchain
-
 : "Downloading Metal Toolchain"
-xcodebuild -downloadComponent metalToolchain -exportPath ./build_assets/
-
-: "Installing Metal Toolchain"
-xcodebuild -importComponent metalToolchain -importPath ./build_assets/*.exportedBundle
+xcodebuild -downloadComponent metalToolchain
