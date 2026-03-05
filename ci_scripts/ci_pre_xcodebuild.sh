4b238853be2faaccf2e02dd1cd39bc984cb515de
Pete Walters <pete@signal.org>
2026-03-05 11:20:48 -0600

    Check the Metal version before downloading

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,5 +5,8 @@ set -eux
 : "Checking Metal Toolchain"
 xcodebuild -showComponent metalToolchain
 
+: "Checking Metal version"
+xcrun metal --version
+
 : "Downloading Metal Toolchain"
 xcodebuild -downloadComponent MetalToolchain
