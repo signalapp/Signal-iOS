2412acead49c9251aa8d8b7ba3ea590751bf3dd6
Max Radermacher <max@signal.org>
2026-03-10 07:33:44 -0700

    Build v7 UUIDs with safe types

--- SignalServiceKit/Util/UUIDv7.swift
+++ SignalServiceKit/Util/UUIDv7.swift
@@ -18,45 +18,39 @@ extension UUID {
     ///
     /// [RFC]: https://www.rfc-editor.org/rfc/rfc9562.html#name-uuid-version-7
     static func v7(timestamp: UInt64) -> UUID {
-        var uuidBytes = Data(repeating: 0, count: 16)
+        return UUID(uuid: (
+            // Assign the lower six bytes of the timestamp (the bytes that change
+            // the most) to the first six bytes of the UUID, in big-endian order
+            // (most-significant byte first).
 
-        // Assign the lower six bytes of the timestamp (the bytes that change
-        // the most) to the first six bytes of the UUID, in big-endian order
-        // (most-significant byte first).
+            // Assign timestamp to the first 6 bytes (big-endian)
+            // For each of the first six bytes of the
+            UInt8((timestamp >> 40) & 0xFF),
+            UInt8((timestamp >> 32) & 0xFF),
+            UInt8((timestamp >> 24) & 0xFF),
+            UInt8((timestamp >> 16) & 0xFF),
+            UInt8((timestamp >> 8) & 0xFF),
+            UInt8(timestamp & 0xFF),
 
-        // Assign timestamp to the first 6 bytes (big-endian)
-        // For each of the first six bytes of the
-        uuidBytes[0] = UInt8((timestamp >> 40) & 0xFF)
-        uuidBytes[1] = UInt8((timestamp >> 32) & 0xFF)
-        uuidBytes[2] = UInt8((timestamp >> 24) & 0xFF)
-        uuidBytes[3] = UInt8((timestamp >> 16) & 0xFF)
-        uuidBytes[4] = UInt8((timestamp >> 8) & 0xFF)
-        uuidBytes[5] = UInt8(timestamp & 0xFF)
+            // Set the next four bits to 0b0111, the required "version" for UUIDv7.
+            // Set the rest of that byte to random.
+            ((0b0111 as UInt8) << 4) | UInt8.random(in: 0...((1 << 4) - 1)),
+            // Add a full byte of random.
+            UInt8.random(in: 0...0xFF),
 
-        // Set the next four bits to 0b0111, the required "version" for UUIDv7.
-        // Set the rest of that byte to random.
-        let versionBits = UInt8(0b0111)
-        let fourRandomBits = UInt8.random(in: 0...((1 << 4) - 1))
-        uuidBytes[6] = UInt8((versionBits << 4) | fourRandomBits)
+            // Set the next two bits to 0b10, the required "variant" for UUIDv7. Set
+            // the rest of that byte to random.
+            ((0b10 as UInt8) << 6) | UInt8.random(in: 0...((1 << 6) - 1)),
 
-        // Add a full byte of random.
-        uuidBytes[7] = UInt8.random(in: 0...0xFF)
-
-        // Set the next two bits to 0b10, the required "variant" for UUIDv7. Set
-        // the rest of that byte to random.
-        let variantBits = UInt8(0b10)
-        let sixRandomBits = UInt8.random(in: 0...((1 << 6) - 1))
-        uuidBytes[8] = UInt8((variantBits << 6) | sixRandomBits)
-
-        // Set the remaining bytes to random.
-        for i in 9..<16 {
-            uuidBytes[i] = UInt8.random(in: 0...0xFF)
-        }
-
-        // Finally, construct a UUID from the bytes.
-        return uuidBytes.withUnsafeBytes { bytes in
-            UUID(uuid: bytes.load(as: uuid_t.self))
-        }
+            // Set the remaining bytes to random.
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+            UInt8.random(in: 0...0xFF),
+        ))
     }
 }
 
