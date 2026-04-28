01f9fbad850ca73eaf38a7e5d761d0b430ac2bba
Max Radermacher <max@signal.org>
2026-04-28 11:37:51 -0500

    Add unit tests for Safety Number fingerprints

--- SignalServiceKit/tests/Security/OWSFingerprintTest.swift
+++ SignalServiceKit/tests/Security/OWSFingerprintTest.swift
@@ -4,80 +4,100 @@
 //
 
 import LibSignalClient
-import SignalServiceKit
-import XCTest
+import Testing
+@testable import SignalServiceKit
 
-final class OWSFingerprintTest: XCTestCase {
+struct OWSFingerprintTest {
+    private let aliceAci = Aci.constantForTesting("00000000-0000-4000-8000-0000000000a1")
+    private let bobAci = Aci.constantForTesting("00000000-0000-4000-8000-0000000000b1")
 
-    func testDisplayableTextInsertsSpaces() {
-        let aliceAci = Aci.constantForTesting("00000000-0000-4000-8000-0000000000a1")
-        let bobAci = Aci.constantForTesting("00000000-0000-4000-8000-0000000000b1")
+    private let aliceIdentity = IdentityKey(
+        publicKey: try! PrivateKey([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 64]).publicKey,
+    )
+    private let bobIdentity = IdentityKey(
+        publicKey: try! PrivateKey([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 64]).publicKey,
+    )
 
-        let aliceIdentityKey = IdentityKeyPair.generate().identityKey
-        let bobIdentityKey = IdentityKeyPair.generate().identityKey
-
+    @Test
+    func testDisplayableText() {
         let aliceToBobFingerprint = OWSFingerprint(
             myAci: aliceAci,
             theirAci: bobAci,
-            myAciIdentityKey: aliceIdentityKey,
-            theirAciIdentityKey: bobIdentityKey,
+            myAciIdentityKey: aliceIdentity,
+            theirAciIdentityKey: bobIdentity,
             hashIterations: 2,
         )
         let displayableText = aliceToBobFingerprint.displayableText
 
-        let expectedGroupCount = 12
-        let expectedGroupSize = 5
-
-        let expectedResultLength = (expectedGroupCount * expectedGroupSize) + (expectedGroupCount - 1)
-        XCTAssertEqual(displayableText.count, expectedResultLength)
-
-        let components = displayableText.components(separatedBy: .whitespacesAndNewlines)
-        XCTAssertEqual(components.count, expectedGroupCount)
-        for component in components {
-            XCTAssertEqual(component.count, expectedGroupSize)
-            let match = component.range(of: #"^\d*$"#, options: .regularExpression)
-            XCTAssertNotNil(match, "\(component) is not all digits")
-        }
+        #expect(displayableText == "79869 05861 85368 01620\n29524 19262 97840 44452\n97466 75030 05834 47853")
     }
 
-    func testTextMatchesReciprocally() {
-        let aliceAci = Aci.randomForTesting()
-        let bobAci = Aci.randomForTesting()
-        let charlieAci = Aci.randomForTesting()
-
-        let aliceIdentityKey = IdentityKeyPair.generate().identityKey
-        let bobIdentityKey = IdentityKeyPair.generate().identityKey
-        let charlieIdentityKey = IdentityKeyPair.generate().identityKey
+    @Test
+    func testTextMatchesReciprocally() throws {
+        let charlieAci = Aci.constantForTesting("00000000-0000-4000-8000-0000000000c1")
+        let charlieKey = try PrivateKey([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 64])
+        let charlieIdentity = IdentityKey(publicKey: charlieKey.publicKey)
 
         let aliceToBobFingerprint = OWSFingerprint(
             myAci: aliceAci,
             theirAci: bobAci,
-            myAciIdentityKey: aliceIdentityKey,
-            theirAciIdentityKey: bobIdentityKey,
+            myAciIdentityKey: aliceIdentity,
+            theirAciIdentityKey: bobIdentity,
             hashIterations: 2,
         )
         let bobToAliceFingerprint = OWSFingerprint(
             myAci: bobAci,
             theirAci: aliceAci,
-            myAciIdentityKey: bobIdentityKey,
-            theirAciIdentityKey: aliceIdentityKey,
+            myAciIdentityKey: bobIdentity,
+            theirAciIdentityKey: aliceIdentity,
             hashIterations: 2,
         )
-        XCTAssertEqual(
-            aliceToBobFingerprint.displayableText,
-            bobToAliceFingerprint.displayableText,
-        )
+        #expect(aliceToBobFingerprint.displayableText == bobToAliceFingerprint.displayableText)
 
         let charlieToAliceFingerprint = OWSFingerprint(
             myAci: charlieAci,
             theirAci: aliceAci,
-            myAciIdentityKey: charlieIdentityKey,
-            theirAciIdentityKey: aliceIdentityKey,
+            myAciIdentityKey: charlieIdentity,
+            theirAciIdentityKey: aliceIdentity,
             hashIterations: 2,
         )
-        XCTAssertNotEqual(
-            aliceToBobFingerprint.displayableText,
-            charlieToAliceFingerprint.displayableText,
+        #expect(aliceToBobFingerprint.displayableText != charlieToAliceFingerprint.displayableText)
+    }
+
+    @Test
+    func testCombinedFingerprints() throws {
+        let bobCombinedFingerprints = try Textsecure_CombinedFingerprints(
+            serializedBytes: Data(base64Encoded: "CAISIgogh2isJ/CLN4n95KkusJUazEq6p9YOBrWV6kvVk02NQSgaIgog828QHN1CX/GHRdrB7LJYTr6zxXQqYfv4dKqE57n+0IU=")!,
         )
+        let aliceToBobFingerprint = OWSFingerprint(
+            myAci: aliceAci,
+            theirAci: bobAci,
+            myAciIdentityKey: aliceIdentity,
+            theirAciIdentityKey: bobIdentity,
+            hashIterations: 2,
+        )
+        try aliceToBobFingerprint.checkAgainst(combinedFingerprints: bobCombinedFingerprints)
+    }
+
+    @Test(arguments: [
+        (OWSFingerprint.MatchError.weHaveOldVersion, "CAMSIgogh2isJ/CLN4n95KkusJUazEq6p9YOBrWV6kvVk02NQSgaIgog828QHN1CX/GHRdrB7LJYTr6zxXQqYfv4dKqE57n+0IU="),
+        (OWSFingerprint.MatchError.theyHaveOldVersion, "CAESIgogh2isJ/CLN4n95KkusJUazEq6p9YOBrWV6kvVk02NQSgaIgog828QHN1CX/GHRdrB7LJYTr6zxXQqYfv4dKqE57n+0IU="),
+        (OWSFingerprint.MatchError.weHaveWrongKeyForThem, "CAISIgogi2isJ/CLN4n95KkusJUazEq6p9YOBrWV6kvVk02NQSgaIgog828QHN1CX/GHRdrB7LJYTr6zxXQqYfv4dKqE57n+0IU="),
+        (OWSFingerprint.MatchError.theyHaveWrongKeyForUs, "CAISIgogh2isJ/CLN4n95KkusJUazEq6p9YOBrWV6kvVk02NQSgaIgog828QHN1CX/GHRdrB7LJYTr6zxXQqYfv4dKqE57n+0JU="),
+    ])
+    func testCombinedFingerprintsFailures(testCase: (matchError: OWSFingerprint.MatchError, bobEncodedFingerprints: String)) {
+        let bobCombinedFingerprints = try! Textsecure_CombinedFingerprints(
+            serializedBytes: Data(base64Encoded: testCase.bobEncodedFingerprints)!,
+        )
+        let aliceToBobFingerprint = OWSFingerprint(
+            myAci: aliceAci,
+            theirAci: bobAci,
+            myAciIdentityKey: aliceIdentity,
+            theirAciIdentityKey: bobIdentity,
+            hashIterations: 2,
+        )
+        #expect(throws: testCase.matchError) {
+            try aliceToBobFingerprint.checkAgainst(combinedFingerprints: bobCombinedFingerprints)
+        }
     }
 }
