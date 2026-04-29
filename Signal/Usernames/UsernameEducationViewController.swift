4f0c2c3debae53af9806e98aef87d0fdea4d0f26
Igor Solomennikov <igor@signal.org>
2026-04-29 14:58:12 -0700

    Fix colors and simplify layout in username education screen.
    • Use a plain OWSViewController instead of a table view controller.
    • Update helper text to use "headline" and "body" fonts for helper text - looks better that way.

--- Signal/Usernames/UsernameEducationViewController.swift
+++ Signal/Usernames/UsernameEducationViewController.swift
@@ -8,66 +8,99 @@ import SafariServices
 import SignalServiceKit
 import SignalUI
 
-class UsernameEducationViewController: OWSTableViewController2 {
-
-    private enum Constants {
-        static let headerHeight: CGFloat = 44.0
-        static let itemIconSize: CGFloat = 48.0
-        static let itemMargin: CGFloat = 20.0
-
-        static let pillSize: CGSize = .init(width: 36, height: 5)
-        static let pillTopMargin: CGFloat = 12.0
-    }
+class UsernameEducationViewController: OWSViewController {
 
     /// Completion called once the user taps 'Continue' in the education prompt
     var continueCompletion: (() -> Void)?
 
     var prefersNavigationBarHidden: Bool { true }
 
-    // MARK: Lifecycle
-
-    override init() {
-        super.init()
-
-        topHeader = pillHeader
-        bottomFooter = footerView
-    }
+    // MARK: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = Theme.tableView2BackgroundColor
-        rebuildTableContents()
-        setColorsForCurrentTheme()
-        tableView.alwaysBounceVertical = false
-    }
 
-    override func themeDidChange() {
-        super.themeDidChange()
-        setColorsForCurrentTheme()
-        rebuildTableContents()
-    }
+        view.backgroundColor = .Signal.background
 
-    // MARK: Views
+        // Pill handle.
+        let handleSize = CGSize(width: 52, height: 5) // Match InteractiveSheetViewController
+        let handleView = PillView()
+        handleView.backgroundColor = .Signal.primaryFill // Match InteractiveSheetViewController
+        handleView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(handleView)
+        NSLayoutConstraint.activate([
+            handleView.widthAnchor.constraint(equalToConstant: handleSize.width),
+            handleView.heightAnchor.constraint(equalToConstant: handleSize.height),
+            handleView.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            handleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
 
-    private lazy var pillHeader: UIView = {
-        let headerView = UIView()
+        // Main content view.
+        let stackView = addStaticContentStackView(
+            arrangedSubviews: [],
+            isScrollable: true,
+        )
+        stackView.spacing = 40 // space between helper rows
 
-        headerView.addSubview(pillView)
-        headerView.autoSetDimension(.height, toSize: Constants.headerHeight)
+        // Spacer at the top.
+        let topSpacer = UIView.transparentSpacer()
+        topSpacer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topSpacer.heightAnchor.constraint(equalToConstant: 40), // 44 dp total with stackView's spacing€
+        ])
+        stackView.addArrangedSubview(topSpacer)
 
-        pillView.autoPinEdge(toSuperviewEdge: .top, withInset: Constants.pillTopMargin)
-        pillView.autoHCenterInSuperview()
+        // Title.
+        let titleLabel = UILabel.titleLabelForRegistration(text: OWSLocalizedString(
+            "USERNAME_EDUCATION_TITLE",
+            comment: "Title to set up signal username",
+        ))
+        titleLabel.adjustsFontForContentSizeCategory = true
+        stackView.addArrangedSubview(titleLabel)
 
-        return headerView
-    }()
+        // Helper text.
+        stackView.addArrangedSubview(createHelperTextRow(
+            iconName: "phone-48-color",
+            title: OWSLocalizedString(
+                "USERNAME_EDUCATION_PRIVACY_TITLE",
+                comment: "Title for phone number privacy section of the username education sheet",
+            ),
+            description: OWSLocalizedString(
+                "USERNAME_EDUCATION_PRIVACY_DESCRIPTION",
+                comment: "Description of phone number privacy on the username education sheet",
+            ),
+        ))
 
-    private lazy var pillView: PillView = {
-        let pillView = PillView()
-        pillView.autoSetDimensions(to: Constants.pillSize)
-        return pillView
-    }()
+        stackView.addArrangedSubview(createHelperTextRow(
+            iconName: "usernames-48-color",
+            title: OWSLocalizedString(
+                "USERNAME_EDUCATION_USERNAME_TITLE",
+                comment: "Title for usernames section on the username education sheet",
+            ),
+            description: OWSLocalizedString(
+                "USERNAME_EDUCATION_USERNAME_DESCRIPTION",
+                comment: "Description of usernames on the username education sheet",
+            ),
+        ))
 
-    private lazy var footerView: UIView = {
+        stackView.addArrangedSubview(createHelperTextRow(
+            iconName: "qr-codes-48-color",
+            title: OWSLocalizedString(
+                "USERNAME_EDUCATION_LINK_TITLE",
+                comment: "Title for the username links and QR codes section on the username education sheet",
+            ),
+            description: OWSLocalizedString(
+                "USERNAME_EDUCATION_LINK_DESCRIPTION",
+                comment: "Description of username links and QR codes on the username education sheet",
+            ),
+        ))
+
+        // Vertical spacer.
+        let verticalSpacer = UIView.transparentSpacer()
+        stackView.addArrangedSubview(verticalSpacer)
+        stackView.setCustomSpacing(0, after: verticalSpacer) // avoid double space: above and below spacer view
+
+        // Buttons at the bottom.
         let continueButton = UIButton(
             configuration: .largePrimary(title: OWSLocalizedString(
                 "USERNAME_EDUCATION_SET_UP_BUTTON",
@@ -84,154 +117,58 @@ class UsernameEducationViewController: OWSTableViewController2 {
                 self?.didTapDismiss()
             },
         )
-
-        let stackView = UIStackView.verticalButtonStack(
-            buttons: [continueButton, dismissButton],
-            isFullWidthButtons: true,
-        )
-
-        let container = UIView()
-        container.preservesSuperviewLayoutMargins = true
-        container.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: container.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: container.layoutMarginsGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: container.layoutMarginsGuide.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: container.bottomAnchor),
-        ])
-        return container
-    }()
-
-    // MARK: TableView config
-
-    private func rebuildTableContents() {
-        let contents = OWSTableContents()
-
-        let headerSection = OWSTableSection()
-        headerSection.hasBackground = false
-        headerSection.add(createTableHeader())
-
-        let section = OWSTableSection()
-        section.hasBackground = false
-
-        section.add(createTableItem(
-            iconName: "phone-48-color",
-            title: OWSLocalizedString(
-                "USERNAME_EDUCATION_PRIVACY_TITLE",
-                comment: "Title for phone number privacy section of the username education sheet",
-            ),
-            description: OWSLocalizedString(
-                "USERNAME_EDUCATION_PRIVACY_DESCRIPTION",
-                comment: "Description of phone number privacy on the username education sheet",
-            ),
-        ))
-
-        section.add(createTableItem(
-            iconName: "usernames-48-color",
-            title: OWSLocalizedString(
-                "USERNAME_EDUCATION_USERNAME_TITLE",
-                comment: "Title for usernames section on the username education sheet",
-            ),
-            description: OWSLocalizedString(
-                "USERNAME_EDUCATION_USERNAME_DESCRIPTION",
-                comment: "Description of usernames on the username education sheet",
-            ),
-        ))
-
-        section.add(createTableItem(
-            iconName: "qr-codes-48-color",
-            title: OWSLocalizedString(
-                "USERNAME_EDUCATION_LINK_TITLE",
-                comment: "Title for the username links and QR codes section on the username education sheet",
-            ),
-            description: OWSLocalizedString(
-                "USERNAME_EDUCATION_LINK_DESCRIPTION",
-                comment: "Description of username links and QR codes on the username education sheet",
-            ),
-            isLastItem: true,
-        ))
-
-        contents.add(sections: [
-            headerSection,
-            section,
-        ])
-
-        self.contents = contents
+        stackView.addArrangedSubview(UIStackView.verticalButtonStack(buttons: [continueButton, dismissButton]))
     }
 
-    private func createTableHeader() -> OWSTableItem {
-        return OWSTableItem {
-            let cell = OWSTableItem.newCell()
-
-            let headerView = Self.HeaderView()
-            cell.addSubview(headerView)
-
-            headerView.autoPinEdgesToSuperviewMargins()
-            return cell
-        }
-    }
-
-    private func createTableItem(
+    private func createHelperTextRow(
         iconName: String,
         title: String,
         description: String,
-        isLastItem: Bool = false,
-    ) -> OWSTableItem {
-        return OWSTableItem {
-            let cell = OWSTableItem.newCell()
-            cell.selectionStyle = .none
+    ) -> UIView {
+        let iconView = UIImageView(image: UIImage(named: iconName))
+        iconView.contentMode = .scaleAspectFit
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            iconView.widthAnchor.constraint(equalTo: iconView.heightAnchor),
+            iconView.widthAnchor.constraint(equalToConstant: 48),
+        ])
 
-            let stackView = UIStackView()
-            stackView.spacing = Constants.itemMargin
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = .dynamicTypeHeadline
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .natural
+        titleLabel.lineBreakMode = .byWordWrapping
+        titleLabel.textColor = .Signal.label
 
-            cell.addSubview(stackView)
+        let bodyLabel = UILabel()
+        bodyLabel.text = description
+        bodyLabel.font = .dynamicTypeBodyClamped
+        bodyLabel.adjustsFontForContentSizeCategory = true
+        bodyLabel.numberOfLines = 0
+        bodyLabel.textAlignment = .natural
+        bodyLabel.lineBreakMode = .byWordWrapping
+        bodyLabel.textColor = .Signal.secondaryLabel
 
-            stackView.autoPinLeadingToSuperviewMargin(withInset: Constants.itemMargin)
-            stackView.autoPinTrailingToSuperviewMargin(withInset: Constants.itemMargin)
-            stackView.autoPinEdge(toSuperviewEdge: .top, withInset: 0)
-            stackView.autoPinEdge(
-                toSuperviewEdge: .bottom,
-                withInset: isLastItem ? 0 : Constants.itemMargin * 2,
-            )
-            stackView.alignment = .top
+        let textStack = UIStackView(
+            arrangedSubviews: [
+                titleLabel,
+                bodyLabel,
+            ],
+        )
+        textStack.axis = .vertical
+        textStack.spacing = 4
 
-            let iconView = UIImageView(image: UIImage(named: iconName))
-            iconView.contentMode = .scaleAspectFit
-            iconView.autoPinToSquareAspectRatio()
-            iconView.autoSetDimension(.height, toSize: Constants.itemIconSize)
+        let stackView = UIStackView(arrangedSubviews: [iconView, textStack])
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.preservesSuperviewLayoutMargins = true
+        stackView.directionalLayoutMargins.leading = 20
+        stackView.directionalLayoutMargins.trailing = 20
+        stackView.spacing = 20
+        stackView.alignment = .top
 
-            stackView.addArrangedSubview(iconView)
-
-            let titleLabel = UILabel()
-            titleLabel.text = title
-            titleLabel.font = UIFont.dynamicTypeBody
-            titleLabel.numberOfLines = 0
-            titleLabel.textAlignment = .left
-            titleLabel.lineBreakMode = .byWordWrapping
-            titleLabel.textColor = Theme.primaryTextColor
-
-            let bodyLabel = UILabel()
-            bodyLabel.text = description
-            bodyLabel.font = UIFont.dynamicTypeSubheadlineClamped
-            bodyLabel.numberOfLines = 0
-            bodyLabel.textAlignment = .left
-            bodyLabel.lineBreakMode = .byWordWrapping
-            bodyLabel.textColor = Theme.secondaryTextAndIconColor
-
-            let textStack = UIStackView(
-                arrangedSubviews: [
-                    titleLabel,
-                    bodyLabel,
-                ],
-            )
-            textStack.axis = .vertical
-            textStack.spacing = 4
-
-            stackView.addArrangedSubview(textStack)
-
-            return cell
-        }
+        return stackView
     }
 
     // MARK: Actions
@@ -245,55 +182,15 @@ class UsernameEducationViewController: OWSTableViewController2 {
     private func didTapDismiss() {
         dismiss(animated: true)
     }
-
-    // MARK: Theme
-
-    private func setColorsForCurrentTheme() {
-        pillView.backgroundColor = Theme.isDarkThemeEnabled ? .ows_gray80 : .ows_gray20
-    }
 }
 
-extension UsernameEducationViewController {
-    class HeaderView: UIView {
+// MARK: -
 
-        // MARK: Init
+#if DEBUG
 
-        init() {
-            super.init(frame: .zero)
-
-            addSubview(usernameTitleLabel)
-            usernameTitleLabel.autoPinEdgesToSuperviewEdges()
-
-            updateFontsForCurrentPreferredContentSize()
-            setColorsForCurrentTheme()
-        }
-
-        @available(*, unavailable, message: "Use other constructor")
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-
-        // MARK: Views
-
-        private lazy var usernameTitleLabel: UILabel = {
-            let label = UILabel()
-            label.text = OWSLocalizedString(
-                "USERNAME_EDUCATION_TITLE",
-                comment: "Title to set up signal username",
-            )
-            label.numberOfLines = 0
-            label.textAlignment = .center
-            return label
-        }()
-
-        // MARK: - Style views
-
-        func updateFontsForCurrentPreferredContentSize() {
-            usernameTitleLabel.font = .dynamicTypeTitle1Clamped.semibold()
-        }
-
-        func setColorsForCurrentTheme() {
-            usernameTitleLabel.textColor = Theme.primaryTextColor
-        }
-    }
+@available(iOS 17, *)
+#Preview {
+    return UsernameEducationViewController()
 }
+
+#endif
