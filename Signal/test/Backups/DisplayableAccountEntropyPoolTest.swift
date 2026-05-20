abc2e213c726e39c6116a21b83b415690afc7196
Sasha Weiss <sasha@signal.org>
2026-05-20 10:32:56 -0700

    Revert "Display AEPs with 0/O, accept 0/O/=/#"

--- Signal/test/Backups/DisplayableAccountEntropyPoolTest.swift
+++ Signal/test/Backups/DisplayableAccountEntropyPoolTest.swift
@@ -7,7 +7,6 @@ import SignalServiceKit
 import Testing
 @testable import Signal
 
-@Suite(.disabled("Waiting for TODO at DisplayableAccountEntropyPool.swift:34"))
 struct DisplayableAccountEntropyPoolTest {
     private static let aepLength = AccountEntropyPool.Constants.byteLength
 
